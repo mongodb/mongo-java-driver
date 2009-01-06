c4f3f526c40face1f437cbc05200ca4621244d79
Eliot Horowitz <eliot@10gen.com>
2009-01-06 17:02:26 -0500

    some nicer getters ordered keySet() for commands

--- src/main/ed/db/BasicDBObject.java
+++ src/main/ed/db/BasicDBObject.java
@@ -4,6 +4,8 @@ package ed.db;
 
 import java.util.*;
 
+import ed.util.*;
+
 public class BasicDBObject extends HashMap<String,Object> implements DBObject {
     
     public BasicDBObject(){
@@ -30,5 +32,46 @@ public class BasicDBObject extends HashMap<String,Object> implements DBObject {
         return super.get( (Object)key );
     }
 
+    public int getInt( String key ){
+        return ((Number)get( key )).intValue();
+    }
+
+    public int getInt( String key , int def ){
+        Object foo = get( key );
+        if ( foo == null )
+            return def;
+        return ((Number)foo).intValue();
+    }
+    
+    public String getString( String key ){
+        Object foo = get( key );
+        if ( foo == null )
+            return null;
+        return foo.toString();
+    }
+
+    public Object put( String key , Object val ){
+        _keys.add( key );
+        return super.put( key , val );
+    }
+
+    public Set<String> keySet(){
+        assert( _keys.size() == size() );
+        return _keys;
+    }
+    
+    public String toString(){
+        StringBuilder buf = new StringBuilder();
+        buf.append( "{ " );
+        for ( String s : _keys ){
+            if ( buf.length() > 2 )
+                buf.append( ", " );
+            buf.append( s ).append( ":" ).append( get( s ) ).append( " " );
+        }
+        buf.append( "}" );
+        return buf.toString();
+    }
+
+    final Set<String> _keys = new OrderedSet<String>();
     final boolean _isPartialObject;
 }
