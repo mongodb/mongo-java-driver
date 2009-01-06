4912f5cc606c3e159a0967283b1f0304573293cc
Eliot Horowitz <eliot@10gen.com>
2009-01-06 17:22:04 -0500

    playing with reflection api

---
+++ src/main/ed/db/ReflectionDBObject.java
@@ -0,0 +1,168 @@
+// ReflectionDBObject.java
+
+package ed.db;
+
+import java.util.*;
+import java.lang.reflect.*;
+
+import ed.util.*;
+
+public abstract class ReflectionDBObject implements DBObject {
+    
+    public Object get( String key ){
+        return getWrapper().get( this , key );
+    }
+
+    public Set<String> keySet(){
+        return getWrapper().keySet();
+    }
+
+    public boolean containsKey( String s ){
+        return getWrapper().containsKey( s );
+    }
+
+    public Object put( String key , Object v ){
+        return getWrapper().set( this , key , v );
+    }
+
+    public ObjectId get_id(){
+        return _id;
+    }
+
+    public void set_id( ObjectId id ){
+        _id = id;
+    }
+
+    public boolean isPartialObject(){
+        return false;
+    }
+
+    public Object removeField( String key ){
+        throw new RuntimeException( "can't remove from a ReflectionDBObject" );
+    }
+
+    JavaWrapper getWrapper(){
+        if ( _wrapper != null )
+            return _wrapper;
+
+        _wrapper = getWrapper( this.getClass() );
+        return _wrapper;
+    }
+
+    JavaWrapper _wrapper;
+    ObjectId _id;
+
+    public static class JavaWrapper {
+        JavaWrapper( Class c ){
+            _class = c;
+            _name = c.getName();
+
+            _fields = new TreeMap<String,FieldInfo>();
+            for ( Method m : c.getMethods() ){
+                if ( ! ( m.getName().startsWith( "get" ) || m.getName().startsWith( "set" ) ) )
+                    continue;
+                
+                String name = m.getName().substring(3);
+                if ( name.length() == 0 || IGNORE_FIELDS.contains( name ) )
+                    continue;
+
+                FieldInfo fi = _fields.get( name );
+                if ( fi == null ){
+                    fi = new FieldInfo( name );
+                    _fields.put( name , fi );
+                }
+                
+                if ( m.getName().startsWith( "get" ) )
+                    fi._getter = m;
+                else
+                    fi._setter = m;
+            }
+
+            Set<String> names = new HashSet<String>( _fields.keySet() );
+            for ( String name : names )
+                if ( ! _fields.get( name ).ok() )
+                    _fields.remove( name );
+            
+            _keys = Collections.unmodifiableSet( _fields.keySet() );
+        }
+
+        public Set<String> keySet(){
+            return _keys;
+        }
+
+        public boolean containsKey( String key ){
+            return _keys.contains( key );
+        }
+
+        public Object get( ReflectionDBObject t , String name ){
+            FieldInfo i = _fields.get( name );
+            if ( i == null )
+                return null;
+            try {
+                return i._getter.invoke( t );
+            }
+            catch ( Exception e ){
+                throw new RuntimeException( "could not invoke getter for [" + name + "] on [" + _name + "]" , e );
+            }
+        }
+
+        public Object set( ReflectionDBObject t , String name , Object val ){
+            if ( IGNORE_SETS.contains( name ) )
+                return null;
+            FieldInfo i = _fields.get( name );
+            if ( i == null )
+                throw new IllegalArgumentException( "no field [" + name + "] on [" + _name + "]" );
+            try {
+                return i._setter.invoke( t , val );
+            }
+            catch ( Exception e ){
+                throw new RuntimeException( "could not invoke setter for [" + name + "] on [" + _name + "]" , e );
+            }
+        }
+        
+        final Class _class;
+        final String _name;
+        final Map<String,FieldInfo> _fields;
+        final Set<String> _keys;
+    }
+    
+    static class FieldInfo {
+        FieldInfo( String name ){
+            _name = name;
+        }
+
+        boolean ok(){
+            return 
+                _getter != null &&
+                _setter != null;
+        }
+        
+        final String _name;
+        Method _getter;
+        Method _setter;
+    }
+
+    public static JavaWrapper getWrapper( Class c ){
+        JavaWrapper w = _wrappers.get( c );
+        if ( w == null ){
+            w = new JavaWrapper( c );
+            _wrappers.put( c , w );
+        }
+        return w;
+    }
+    
+    private static final Map<Class,JavaWrapper> _wrappers = Collections.synchronizedMap( new HashMap<Class,JavaWrapper>() );
+    private static final Set<String> IGNORE_FIELDS = new HashSet<String>();
+    static {
+        IGNORE_FIELDS.add( "Int" );
+    }
+
+    private static final Set<String> IGNORE_SETS = new HashSet<String>();
+    static {
+        IGNORE_SETS.add( "_save" );
+        IGNORE_SETS.add( "_update" );
+        IGNORE_SETS.add( "_ns" );
+    }
+
+    
+}
---
+++ src/test/ed/db/ReflectionTest.java
@@ -0,0 +1,62 @@
+// ReflectionTest.java
+
+package ed.db;
+
+import java.io.*;
+import java.util.*;
+import java.util.regex.*;
+
+import org.testng.annotations.Test;
+
+import ed.*;
+
+public class ReflectionTest extends TestCase {
+    
+    public static class Person extends ReflectionDBObject {
+        
+        public Person(){
+
+        }
+
+        Person( String name ){
+            _name = name;
+        }
+
+        public String getName(){
+            return _name;
+        }
+
+        public void setName(String name){
+            _name = name;
+        }
+
+        String _name;
+    }
+
+    public ReflectionTest()
+        throws IOException {
+        _db = new Mongo( "127.0.0.1" , "reflectiontest" );        
+    }
+    
+
+    @Test
+    public void test1(){
+        DBCollection c = _db.getCollection( "persen.test1" );
+        c.drop();
+        //c.setConstructor( Person.getWrapper( Person.class ).getConstructor() );
+
+        Person p = new Person( "eliot" );
+        c.save( p );
+
+        DBObject out = c.findOne();
+        assertEquals( "eliot" , out.get( "Name" ) );
+    }
+
+    final Mongo _db;
+    
+    public static void main( String args[] )
+        throws Exception {
+        (new ReflectionTest()).runConsole();
+    }
+
+}
