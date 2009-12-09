e809abb1e7d6c1386fc85121be23dd3a1f4ee071
Eliot Horowitz <eliot@10gen.com>
2009-12-09 13:48:29 -0500

    clean up examples

--- examples/QuickTour.java
+++ examples/QuickTour.java
@@ -12,47 +12,30 @@ public class QuickTour {
 
     public static void main(String[] args) throws Exception {
 
-        /*
-         *   connect to the local database server for the 'mydb' database
-         */
+        // connect to the local database server
         Mongo m = new Mongo();
 
+        // get handle to "mydb"
         DB db = m.getDB( "mydb" );
     
-        /*
-         *  Authenticate - optional
-         */
+        // Authenticate - optional
         // boolean auth = db.authenticate("foo", "bar");
 
 
-        /*
-         *  get a list of the collections in this database and print them out
-         */
+        // get a list of the collections in this database and print them out
         Set<String> colls = db.getCollectionNames();
-
         for (String s : colls) {
             System.out.println(s);
         }
 
-        /*
-         *  get a collection object to work with
-         */
-
-        System.out.println(" --- ");
-
+        // get a collection object to work with
         DBCollection coll = db.getCollection("testCollection");
-
-        /*
-         *   drop all the data in it
-         */
-
+        
+        // drop all the data in it
         coll.drop();
 
 
-        /*
-         *  make a document and insert it
-         */
-
+        // make a document and insert it
         BasicDBObject doc = new BasicDBObject();
 
         doc.put("name", "MongoDB");
@@ -68,135 +51,71 @@ public class QuickTour {
 
         coll.insert(doc);
 
-        /*
-         *  get it (since it's the only one in there since we dropped the rest earlier on)
-         */
+        // get it (since it's the only one in there since we dropped the rest earlier on)
         DBObject myDoc = coll.findOne();
-
         System.out.println(myDoc);
 
-
-        /*
-         * now, lets add lots of little documents to the collection so we can explore queries and cursors
-         */
-
-        System.out.println(" --- ");
-
+        // now, lets add lots of little documents to the collection so we can explore queries and cursors
         for (int i=0; i < 100; i++) {
             coll.insert(new BasicDBObject().append("i", i));
         }
+        System.out.println("total # of documents after inserting 100 small ones (should be 101) " + coll.getCount());
 
-        System.out.println(coll.getCount());
-
-        /*
-         *  lets get all the documents in the collection and print them out
-         */
-
-        System.out.println(" --- ");
-
+        //  lets get all the documents in the collection and print them out
         DBCursor cur = coll.find();
-
         while(cur.hasNext()) {
             System.out.println(cur.next());
         }
 
-        /*
-         *  now use a query to get 1 document out
-         */
-
-        System.out.println(" --- ");
-        
+        //  now use a query to get 1 document out
         BasicDBObject query = new BasicDBObject();
-
         query.put("i", 71);
-
         cur = coll.find(query);
 
         while(cur.hasNext()) {
             System.out.println(cur.next());
         }
 
-        /*
-         *  now use a query to get a larger set
-         */
-
-        System.out.println(" --- ");
-
+        //  now use a range query to get a larger subset
         query = new BasicDBObject();
-
         query.put("i", new BasicDBObject("$gt", 50));  // i.e. find all where i > 50
-
         cur = coll.find(query);
 
         while(cur.hasNext()) {
             System.out.println(cur.next());
         }
 
-        System.out.println(" --- ");
-
+        // range query with multiple contstraings
         query = new BasicDBObject();
-
         query.put("i", new BasicDBObject("$gt", 20).append("$lte", 30));  // i.e.   20 < i <= 30
-
         cur = coll.find(query);
 
         while(cur.hasNext()) {
             System.out.println(cur.next());
         }
 
-        /*
-         *  create an index on the "i" field
-         */
-
-       coll.createIndex(new BasicDBObject("i", 1));  // create index on "i", ascending
+        // create an index on the "i" field
+        coll.createIndex(new BasicDBObject("i", 1));  // create index on "i", ascending
 
 
-        /*
-         *   list the indexes on the collection
-         */
-
+        //  list the indexes on the collection
         List<DBObject> list = coll.getIndexInfo();
-
         for (DBObject o : list) {
             System.out.println(o);
         }
 
-        /*
-         *  See if the last operation had an error
-         */
+        // See if the last operation had an error
         System.out.println("Last error : " + db.getLastError());
 
-        /*
-         *  see if any previous operation had an error
-         */
+        // see if any previous operation had an error
         System.out.println("Previous error : " + db.getPreviousError());
 
-        /*
-         *  force an error
-         */
-
+        // force an error
         db.forceError();
 
-        /*
-         *  See if the last operation had an error
-         */
-        System.out.println("Last error : " + db.getLastError());
-
-        /*
-         *  see if any previous operation had an error
-         */
-        System.out.println("Previous error : " + db.getPreviousError());
-
-        /*
-         *  See if the last operation had an error
-         */
+        // See if the last operation had an error
         System.out.println("Last error : " + db.getLastError());
 
         db.resetError();
-
-        /*
-         *  see if any previous operation had an error
-         */
-        System.out.println("Previous error : " + db.getPreviousError());
     }
 }
--- examples/QuickTourAdmin.java
+++ examples/QuickTourAdmin.java
@@ -6,41 +6,29 @@ public class QuickTourAdmin {
     
     public static void main(String[] args) throws Exception {
 
-        /*
-         *   connect to the local database server 
-         */
+        // connect to the local database server 
         Mongo m = new Mongo();
 
-        /*
-         *  Authenticate - optional
-         */
+        // Authenticate - optional
         // boolean auth = db.authenticate("foo", "bar");
-
+        
+        // get db names
         for (String s : m.getDatabaseNames()) {
             System.out.println(s);
         }
 
 
-        /*
-         *   get a db
-         */
-
+        // get a db
         DB db = m.getDB("com_mongodb_MongoAdmin");
 
-        /*
-         * do an insert so that the db will really be created.  Calling getDB() doesn't really take any
-         * action with the server 
-         */
+        // do an insert so that the db will really be created.  Calling getDB() doesn't really take any
+        // action with the server 
         db.getCollection("testcollection").insert(new BasicDBObject("i",1));
-        
         for (String s : m.getDatabaseNames()) {
             System.out.println(s);
         }
 
-        /*
-         *   drop a database
-         */
-
+        // drop a database
         m.dropDatabase("com_mongodb_MongoAdmin");
 
         for (String s : m.getDatabaseNames()) {
