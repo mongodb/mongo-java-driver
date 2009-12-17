0094e91a4b17da2ba3fb0d342238fd04fbe89971
Eliot Horowitz <eliot@10gen.com>
2009-12-17 10:09:29 -0500

    push sources and javadoc jars

--- mavenPush.py
+++ mavenPush.py
@@ -23,20 +23,21 @@ dir = root + "/org/mongodb/mongo-java-driver/" + version
 if not os.path.exists( dir ):
     os.makedirs( dir )
 
-root = dir + "/mongo-java-driver-" + version + "."
+root = dir + "/mongo-java-driver-" + version
 
-shutil.copy2( "mongo.jar" , root + "jar" )
+for x in [ "" , "-sources" , "-javadoc" ]:
+    shutil.copy2( "mongo%s.jar" % x , root + x + ".jar" )
 
 pom = open( "maven.xml" , "r" ).read()
 pom = pom.replace( "$VERSION" , version )
 
-out = open( root + "pom" , 'w' )
+out = open( root + ".pom" , 'w' )
 out.write( pom )
 out.close()
 
-p = subprocess.Popen( [ "sha1sum" , root + "jar" ] , stdout=subprocess.PIPE ).communicate()
+p = subprocess.Popen( [ "sha1sum" , root + ".jar" ] , stdout=subprocess.PIPE ).communicate()
 sha1 = p[0].split( ' ' )[0]
-out = open( root + "jar.sha1" , 'w' )
+out = open( root + ".jar.sha1" , 'w' )
 out.write( sha1 )
 out.close()
 
