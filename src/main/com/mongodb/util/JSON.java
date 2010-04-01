6045f5b17a0cedcb375ad802957d66a2935e7cb0
Eliot Horowitz <eliot@10gen.com>
2010-03-31 20:27:55 -0400

    handle arrays in JSON

--- src/main/com/mongodb/util/JSON.java
+++ src/main/com/mongodb/util/JSON.java
@@ -82,6 +82,21 @@ public class JSON {
             return;
         }
 
+        if ( o.getClass().isArray() ){
+            Object[] arr = (Object[])o;
+
+            buf.append( "[ " );
+            
+            for ( int i=0; i<arr.length; i++) {
+                if ( i > 0 ) buf.append( " , " );
+                serialize( arr[i] , buf );
+            }
+            
+            buf.append( "]" );
+            return;
+        }
+
+
         if ( o instanceof ObjectId) {
             string(buf, o.toString());
             return;
