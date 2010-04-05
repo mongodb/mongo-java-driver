0b83bd82f67d3d400bd345b32d7d7a9c8716305f
Eliot Horowitz <eliot@10gen.com>
2010-04-05 13:42:37 -0400

    ByteBuffer md5

--- src/main/com/mongodb/util/Util.java
+++ src/main/com/mongodb/util/Util.java
@@ -16,6 +16,7 @@
  */
 package com.mongodb.util;
 
+import java.nio.*;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
@@ -59,5 +60,13 @@ public class Util {
         } catch (NoSuchAlgorithmException e) {
             throw new RuntimeException("Error - this implementation of Java doesn't support MD5.");
         }
+    }
+
+    public static String hexMD5( ByteBuffer buf , int offset , int len ){
+        byte b[] = new byte[len];
+        for ( int i=0; i<len; i++ )
+            b[i] = buf.get( offset + i );
+        
+        return hexMD5( b );
     }
 }
