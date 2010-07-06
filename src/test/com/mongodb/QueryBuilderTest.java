e9572676e9129ba3c45c648991bf8b9aed1e27f2
Eliot Horowitz <eliot@10gen.com>
2010-07-06 11:16:36 -0400

    tabs -> spaces

--- src/test/com/mongodb/QueryBuilderTest.java
+++ src/test/com/mongodb/QueryBuilderTest.java
@@ -30,274 +30,274 @@ import com.mongodb.util.TestCase;
  *
  */
 public class QueryBuilderTest extends TestCase {
-	private static TestDB _testDB;
+    private static TestDB _testDB;
 	
-	@BeforeClass
+    @BeforeClass
 	public static void setup() {
-		_testDB = new TestDB("queryBuilderTest");
-	}
+        _testDB = new TestDB("queryBuilderTest");
+    }
 	
-	@Test
+    @Test
 	public void greaterThanTest() {
-		String key = "x";
-		DBCollection collection = _testDB.getCollection("gt-test");
-		saveTestDocument(collection, key, 0);
+        String key = "x";
+        DBCollection collection = _testDB.getCollection("gt-test");
+        saveTestDocument(collection, key, 0);
 		
-		DBObject queryTrue = QueryBuilder.start(key).greaterThan(-1).get();
-		assertTrue(testQuery(collection, queryTrue));
+        DBObject queryTrue = QueryBuilder.start(key).greaterThan(-1).get();
+        assertTrue(testQuery(collection, queryTrue));
 		
-		DBObject queryFalse = QueryBuilder.start(key).greaterThan(0).get();
-		assertFalse(testQuery(collection, queryFalse));
-	}
+        DBObject queryFalse = QueryBuilder.start(key).greaterThan(0).get();
+        assertFalse(testQuery(collection, queryFalse));
+    }
 	
-	@Test
+    @Test
 	public void greaterThanEqualsTest() {
-		String key = "x";
-		DBCollection collection = _testDB.getCollection("gte-test");
-		saveTestDocument(collection, key, 0);
+        String key = "x";
+        DBCollection collection = _testDB.getCollection("gte-test");
+        saveTestDocument(collection, key, 0);
 		
-		DBObject queryTrue = QueryBuilder.start(key).greaterThanEquals(0).get();
-		assertTrue(testQuery(collection, queryTrue));
+        DBObject queryTrue = QueryBuilder.start(key).greaterThanEquals(0).get();
+        assertTrue(testQuery(collection, queryTrue));
 		
-		DBObject queryTrue2 = QueryBuilder.start(key).greaterThanEquals(-1).get();
-		assertTrue(testQuery(collection, queryTrue2));
+        DBObject queryTrue2 = QueryBuilder.start(key).greaterThanEquals(-1).get();
+        assertTrue(testQuery(collection, queryTrue2));
 		
-		DBObject queryFalse = QueryBuilder.start(key).greaterThanEquals(1).get();
-		assertFalse(testQuery(collection, queryFalse));
+        DBObject queryFalse = QueryBuilder.start(key).greaterThanEquals(1).get();
+        assertFalse(testQuery(collection, queryFalse));
 
-	}
+    }
 	
-	@Test
+    @Test
 	public void lessThanTest() {
-		String key = "x";
-		DBCollection collection = _testDB.getCollection("lt-test");
-		saveTestDocument(collection, key, 0);
+        String key = "x";
+        DBCollection collection = _testDB.getCollection("lt-test");
+        saveTestDocument(collection, key, 0);
 		
-		DBObject queryTrue = QueryBuilder.start(key).lessThan(1).get();
-		assertTrue(testQuery(collection, queryTrue));
+        DBObject queryTrue = QueryBuilder.start(key).lessThan(1).get();
+        assertTrue(testQuery(collection, queryTrue));
 		
-		DBObject queryFalse = QueryBuilder.start(key).lessThan(0).get();
-		assertFalse(testQuery(collection, queryFalse));
+        DBObject queryFalse = QueryBuilder.start(key).lessThan(0).get();
+        assertFalse(testQuery(collection, queryFalse));
 
-	}
+    }
 	
-	@Test
+    @Test
 	public void lessThanEqualsTest() {
-		String key = "x";
-		DBCollection collection = _testDB.getCollection("lte-test");
-		saveTestDocument(collection, key, 0);
+        String key = "x";
+        DBCollection collection = _testDB.getCollection("lte-test");
+        saveTestDocument(collection, key, 0);
 		
-		DBObject queryTrue = QueryBuilder.start(key).lessThanEquals(1).get();
-		assertTrue(testQuery(collection, queryTrue));
+        DBObject queryTrue = QueryBuilder.start(key).lessThanEquals(1).get();
+        assertTrue(testQuery(collection, queryTrue));
 		
-		DBObject queryTrue2 = QueryBuilder.start(key).lessThanEquals(0).get();
-		assertTrue(testQuery(collection, queryTrue2));
+        DBObject queryTrue2 = QueryBuilder.start(key).lessThanEquals(0).get();
+        assertTrue(testQuery(collection, queryTrue2));
 		
-		DBObject queryFalse = QueryBuilder.start(key).lessThanEquals(-1).get();
-		assertFalse(testQuery(collection, queryFalse));
-	}
+        DBObject queryFalse = QueryBuilder.start(key).lessThanEquals(-1).get();
+        assertFalse(testQuery(collection, queryFalse));
+    }
 	
-	@Test
+    @Test
 	public void isTest() {
-		String key = "x";
-		DBCollection collection = _testDB.getCollection("is-test");
-		saveTestDocument(collection, key, "test");
+        String key = "x";
+        DBCollection collection = _testDB.getCollection("is-test");
+        saveTestDocument(collection, key, "test");
 		
-		DBObject queryTrue = QueryBuilder.start(key).is("test").get();
-		assertTrue(testQuery(collection, queryTrue));
+        DBObject queryTrue = QueryBuilder.start(key).is("test").get();
+        assertTrue(testQuery(collection, queryTrue));
 		
-		DBObject queryFalse = QueryBuilder.start(key).is("test1").get();
-		assertFalse(testQuery(collection, queryFalse));
-	}
+        DBObject queryFalse = QueryBuilder.start(key).is("test1").get();
+        assertFalse(testQuery(collection, queryFalse));
+    }
 	
-	@Test
+    @Test
 	public void notEqualsTest() {
-		String key = "x";
-		DBCollection collection = _testDB.getCollection("ne-test");
-		saveTestDocument(collection, key, "test");
+        String key = "x";
+        DBCollection collection = _testDB.getCollection("ne-test");
+        saveTestDocument(collection, key, "test");
 		
-		DBObject queryTrue = QueryBuilder.start(key).notEquals("test1").get();
-		assertTrue(testQuery(collection, queryTrue));
+        DBObject queryTrue = QueryBuilder.start(key).notEquals("test1").get();
+        assertTrue(testQuery(collection, queryTrue));
 		
-		DBObject queryFalse = QueryBuilder.start(key).notEquals("test").get();
-		assertFalse(testQuery(collection, queryFalse));
+        DBObject queryFalse = QueryBuilder.start(key).notEquals("test").get();
+        assertFalse(testQuery(collection, queryFalse));
 
-	}
+    }
 	
-	@Test    
+    @Test    
 	public void inTest() {
-		String key = "x";
-		DBCollection collection = _testDB.getCollection("in-test");
-		saveTestDocument(collection, key, 1);
+        String key = "x";
+        DBCollection collection = _testDB.getCollection("in-test");
+        saveTestDocument(collection, key, 1);
 		
-		DBObject queryTrue = QueryBuilder.start(key).in(Arrays.asList(1, 2, 3)).get();
-		assertTrue(testQuery(collection, queryTrue));
+        DBObject queryTrue = QueryBuilder.start(key).in(Arrays.asList(1, 2, 3)).get();
+        assertTrue(testQuery(collection, queryTrue));
 		
-		DBObject queryFalse = QueryBuilder.start(key).in(Arrays.asList(2, 3, 4)).get();
-		assertFalse(testQuery(collection, queryFalse));
-	}
+        DBObject queryFalse = QueryBuilder.start(key).in(Arrays.asList(2, 3, 4)).get();
+        assertFalse(testQuery(collection, queryFalse));
+    }
 	
-	@Test
+    @Test
 	public void notInTest() {
-		String key = "x";
-		DBCollection collection = _testDB.getCollection("nin-test");
-		saveTestDocument(collection, key, 1);
+        String key = "x";
+        DBCollection collection = _testDB.getCollection("nin-test");
+        saveTestDocument(collection, key, 1);
 
-		DBObject queryTrue = QueryBuilder.start(key).notIn(Arrays.asList(2, 3, 4)).get();
-		assertTrue(testQuery(collection, queryTrue));
+        DBObject queryTrue = QueryBuilder.start(key).notIn(Arrays.asList(2, 3, 4)).get();
+        assertTrue(testQuery(collection, queryTrue));
 		
-		DBObject queryFalse = QueryBuilder.start(key).notIn(Arrays.asList(1, 2, 3)).get();
-		assertFalse(testQuery(collection, queryFalse));
-	}
+        DBObject queryFalse = QueryBuilder.start(key).notIn(Arrays.asList(1, 2, 3)).get();
+        assertFalse(testQuery(collection, queryFalse));
+    }
 	
-	@Test
+    @Test
 	public void modTest() {
-		String key = "x";
-		DBCollection collection = _testDB.getCollection("mod-test");
-		saveTestDocument(collection, key, 9);
+        String key = "x";
+        DBCollection collection = _testDB.getCollection("mod-test");
+        saveTestDocument(collection, key, 9);
 		
-		DBObject queryTrue = QueryBuilder.start(key).mod(Arrays.asList(2, 1)).get();
-		assertTrue(testQuery(collection, queryTrue));
+        DBObject queryTrue = QueryBuilder.start(key).mod(Arrays.asList(2, 1)).get();
+        assertTrue(testQuery(collection, queryTrue));
 		
-		DBObject queryFalse = QueryBuilder.start(key).mod(Arrays.asList(2, 0)).get();
-		assertFalse(testQuery(collection, queryFalse));
-	}	
+        DBObject queryFalse = QueryBuilder.start(key).mod(Arrays.asList(2, 0)).get();
+        assertFalse(testQuery(collection, queryFalse));
+    }	
 	
-	@Test
+    @Test
 	public void allTest() {
-		String key = "x";
-		DBCollection collection = _testDB.getCollection("all-test");
-		saveTestDocument(collection, key, Arrays.asList(1, 2, 3));
+        String key = "x";
+        DBCollection collection = _testDB.getCollection("all-test");
+        saveTestDocument(collection, key, Arrays.asList(1, 2, 3));
 		
-		DBObject query = QueryBuilder.start(key).all(Arrays.asList(1, 2, 3)).get();
-		assertTrue(testQuery(collection, query));
+        DBObject query = QueryBuilder.start(key).all(Arrays.asList(1, 2, 3)).get();
+        assertTrue(testQuery(collection, query));
 		
-		DBObject queryFalse = QueryBuilder.start(key).all(Arrays.asList(2, 3, 4)).get();
-		assertFalse(testQuery(collection, queryFalse));
-	}
+        DBObject queryFalse = QueryBuilder.start(key).all(Arrays.asList(2, 3, 4)).get();
+        assertFalse(testQuery(collection, queryFalse));
+    }
 	
-	@Test
+    @Test
 	public void sizeTest() {
-		String key = "x";
-		DBCollection collection = _testDB.getCollection("size-test");
-		saveTestDocument(collection, key, Arrays.asList(1, 2, 3));
+        String key = "x";
+        DBCollection collection = _testDB.getCollection("size-test");
+        saveTestDocument(collection, key, Arrays.asList(1, 2, 3));
 		
-		DBObject queryTrue = QueryBuilder.start(key).size(3).get();
-		assertTrue(testQuery(collection, queryTrue));
+        DBObject queryTrue = QueryBuilder.start(key).size(3).get();
+        assertTrue(testQuery(collection, queryTrue));
 		
-		DBObject queryFalse = QueryBuilder.start(key).size(4).get();
-		assertFalse(testQuery(collection, queryFalse));
+        DBObject queryFalse = QueryBuilder.start(key).size(4).get();
+        assertFalse(testQuery(collection, queryFalse));
 		
-		DBObject queryFalse2 = QueryBuilder.start(key).size(2).get();
-		assertFalse(testQuery(collection, queryFalse2));
-	}
+        DBObject queryFalse2 = QueryBuilder.start(key).size(2).get();
+        assertFalse(testQuery(collection, queryFalse2));
+    }
 
-	@Test
+    @Test
 	public void existsTest() {
-		String key = "x";
-		DBCollection collection = _testDB.getCollection("exists-test");
-		saveTestDocument(collection, key, "test");
+        String key = "x";
+        DBCollection collection = _testDB.getCollection("exists-test");
+        saveTestDocument(collection, key, "test");
 		
-		DBObject queryTrue = QueryBuilder.start(key).exists(true).get();
-		assertTrue(testQuery(collection, queryTrue));
+        DBObject queryTrue = QueryBuilder.start(key).exists(true).get();
+        assertTrue(testQuery(collection, queryTrue));
 		
-		DBObject queryFalse = QueryBuilder.start(key).exists(false).get();
-		assertFalse(testQuery(collection, queryFalse));
-	}
+        DBObject queryFalse = QueryBuilder.start(key).exists(false).get();
+        assertFalse(testQuery(collection, queryFalse));
+    }
 
-	@Test
+    @Test
 	public void regexTest() {
-		String key = "x";
-		DBCollection collection = _testDB.getCollection("regex-test");
-		saveTestDocument(collection, key, "test");
+        String key = "x";
+        DBCollection collection = _testDB.getCollection("regex-test");
+        saveTestDocument(collection, key, "test");
 		
-		DBObject queryTrue = QueryBuilder.start(key).regex(Pattern.compile("\\w*")).get();
-		assertTrue(testQuery(collection, queryTrue));
-	}
+        DBObject queryTrue = QueryBuilder.start(key).regex(Pattern.compile("\\w*")).get();
+        assertTrue(testQuery(collection, queryTrue));
+    }
 	
-	@Test
+    @Test
 	public void rangeChainTest() {
-		String key = "x";
-		DBCollection collection = _testDB.getCollection("range-test");
-		saveTestDocument(collection, key, 2);
+        String key = "x";
+        DBCollection collection = _testDB.getCollection("range-test");
+        saveTestDocument(collection, key, 2);
 		
-		DBObject queryTrue = QueryBuilder.start(key).greaterThan(0).lessThan(3).get();
-		assertTrue(testQuery(collection, queryTrue));
-	}
+        DBObject queryTrue = QueryBuilder.start(key).greaterThan(0).lessThan(3).get();
+        assertTrue(testQuery(collection, queryTrue));
+    }
 	
-	@Test
+    @Test
 	public void compoundChainTest() {
-		String key = "x";
-		String key2 = "y";
-		String value = key;
-		DBCollection collection = _testDB.getCollection("compound-test");
-		DBObject testDocument = new BasicDBObject();
-		testDocument.put(key, value);
-		testDocument.put(key2, 9);
-		collection.save(testDocument);
+        String key = "x";
+        String key2 = "y";
+        String value = key;
+        DBCollection collection = _testDB.getCollection("compound-test");
+        DBObject testDocument = new BasicDBObject();
+        testDocument.put(key, value);
+        testDocument.put(key2, 9);
+        collection.save(testDocument);
 		
-		DBObject queryTrue = QueryBuilder.start(key).is(value).and(key2).mod(Arrays.asList(2,1)).get();
-		assertTrue(testQuery(collection, queryTrue));
-	}
+        DBObject queryTrue = QueryBuilder.start(key).is(value).and(key2).mod(Arrays.asList(2,1)).get();
+        assertTrue(testQuery(collection, queryTrue));
+    }
 	
-	@Test
+    @Test
 	public void arrayChainTest() {
-		String key = "x";
-		DBCollection collection = _testDB.getCollection("array-test");
-		saveTestDocument(collection, key, Arrays.asList(1, 2, 3));
+        String key = "x";
+        DBCollection collection = _testDB.getCollection("array-test");
+        saveTestDocument(collection, key, Arrays.asList(1, 2, 3));
 		
-		DBObject queryTrue = QueryBuilder.start(key).all(Arrays.asList(1,2,3)).size(3).get();
-		assertTrue(testQuery(collection, queryTrue));
-	}
+        DBObject queryTrue = QueryBuilder.start(key).all(Arrays.asList(1,2,3)).size(3).get();
+        assertTrue(testQuery(collection, queryTrue));
+    }
 	
-	@Test
+    @Test
 	public void failureTest() {
-		boolean thrown = false;
-		try {
-			QueryBuilder.start("x").get();
-		} catch(QueryBuilderException e) {
-			thrown = true;
-		}
-		assertTrue(thrown);
+        boolean thrown = false;
+        try {
+            QueryBuilder.start("x").get();
+        } catch(QueryBuilderException e) {
+            thrown = true;
+        }
+        assertTrue(thrown);
 		
-		boolean thrown2 = false;
-		try {
-			QueryBuilder.start("x").exists(true).and("y").get();
-		} catch(QueryBuilderException e) {
-			thrown2 = true;
-		}
-		assertTrue(thrown2);
+        boolean thrown2 = false;
+        try {
+            QueryBuilder.start("x").exists(true).and("y").get();
+        } catch(QueryBuilderException e) {
+            thrown2 = true;
+        }
+        assertTrue(thrown2);
 		
-		boolean thrown3 = false;
-		try {
-			QueryBuilder.start("x").and("y").get();
-		} catch(QueryBuilderException e) {
-			thrown3 = true;
-		}
-		assertTrue(thrown3);
-	}
+        boolean thrown3 = false;
+        try {
+            QueryBuilder.start("x").and("y").get();
+        } catch(QueryBuilderException e) {
+            thrown3 = true;
+        }
+        assertTrue(thrown3);
+    }
 		
-	@AfterClass
+    @AfterClass
 	public static void tearDown() {
-		_testDB.cleanup();
-	}
+        _testDB.cleanup();
+    }
 	
-	/**
-	 * Convenience method that
-	 * creates a new MongoDB Document with a key-value pair and saves it inside the specified collection
-	 * @param collection Collection to save the new document to
-	 * @param key key of the field to be inserted to the new document
-	 * @param value value of the field to be inserted to the new document
-	 */
-	private void saveTestDocument(DBCollection collection, String key, Object value) {
-		DBObject testDocument = new BasicDBObject();
-		testDocument.put(key, value);
-		collection.save(testDocument);
-	}
+    /**
+     * Convenience method that
+     * creates a new MongoDB Document with a key-value pair and saves it inside the specified collection
+     * @param collection Collection to save the new document to
+     * @param key key of the field to be inserted to the new document
+     * @param value value of the field to be inserted to the new document
+     */
+    private void saveTestDocument(DBCollection collection, String key, Object value) {
+        DBObject testDocument = new BasicDBObject();
+        testDocument.put(key, value);
+        collection.save(testDocument);
+    }
 	
-	private boolean testQuery(DBCollection collection, DBObject query) {
-		DBCursor cursor = collection.find(query);
-		return cursor.hasNext();
-	}
+    private boolean testQuery(DBCollection collection, DBObject query) {
+        DBCursor cursor = collection.find(query);
+        return cursor.hasNext();
+    }
 		
 }
