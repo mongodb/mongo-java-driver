562eb88fed1f849c4a2b82e3baca9a0c7461ef74
Eliot Horowitz <eliot@10gen.com>
2010-09-24 17:15:13 -0400

    some prep work for getting bson.jar into maven JAVA-152

--- mavenPush.py
+++ mavenPush.py
@@ -28,27 +28,29 @@ if p[0].find( "SUCCESSFUL" ) < 0:
     print( p[1] )
     raise( "build failed" )
 
-dir = root + "/org/mongodb/mongo-java-driver/" + version
-if not os.path.exists( dir ):
-    os.makedirs( dir )
+def go( shortName , longName ):
+    dir = root + "/org/mongodb/" + longName + "/" + version
+    if not os.path.exists( dir ):
+        os.makedirs( dir )
 
-root = dir + "/mongo-java-driver-" + version
+    fileRoot = dir + "/" + longName + "-" + version
 
-for x in [ "" , "-sources" , "-javadoc" ]:
-    shutil.copy2( "mongo%s.jar" % x , root + x + ".jar" )
+    for x in [ "" , "-sources" , "-javadoc" ]:
+        shutil.copy2( shortName + x + ".jar" , fileRoot + x + ".jar" )
 
-pom = open( "maven.xml" , "r" ).read()
-pom = pom.replace( "$VERSION" , version )
+    pom = open( "maven-" + shortName + ".xml" , "r" ).read()
+    pom = pom.replace( "$VERSION" , version )
 
-out = open( root + ".pom" , 'w' )
-out.write( pom )
-out.close()
+    out = open( fileRoot + ".pom" , 'w' )
+    out.write( pom )
+    out.close()
 
-p = subprocess.Popen( [ "sha1sum" , root + ".jar" ] , stdout=subprocess.PIPE ).communicate()
-sha1 = p[0].split( ' ' )[0]
-out = open( root + ".jar.sha1" , 'w' )
-out.write( sha1 )
-out.close()
+    p = subprocess.Popen( [ "sha1sum" , fileRoot + ".jar" ] , stdout=subprocess.PIPE ).communicate()
+    sha1 = p[0].split( ' ' )[0]
+    out = open( fileRoot + ".jar.sha1" , 'w' )
+    out.write( sha1 )
+    out.close()
 
 
-
+go( "mongo" , "mongo-java-driver" )
+go( "bson" , "bson" )
