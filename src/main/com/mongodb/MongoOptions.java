48992fb8f27019b279793480809fc7e8428bfb19
Ryan <rgnitz@gmail.com>
2010-12-17 12:24:43 -0500

    fixed javadoc comment (@link)

--- src/main/com/mongodb/MongoOptions.java
+++ src/main/com/mongodb/MongoOptions.java
@@ -63,7 +63,7 @@ public class MongoOptions {
     public int connectTimeout;
 
     /**
-      The socket timeout; this value is passed to {@link java.net.Socket.setSoTimeout}.  0 is default and infinite
+      The socket timeout; this value is passed to {@link java.net.Socket#setSoTimeout(int)}.  0 is default and infinite
      */
     public int socketTimeout;
     
