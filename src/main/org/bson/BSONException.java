4d12e3a10b24d6076f53f42f2e6a1c6e331d0f9a
Ryan <rgnitz@gmail.com>
2011-02-12 21:28:00 -0500

    changed error code to Integer (was string).

--- src/main/org/bson/BSONException.java
+++ src/main/org/bson/BSONException.java
@@ -34,7 +34,7 @@ public class BSONException extends RuntimeException {
      * @param errorCode The error code.
      * @param msg The error message.
      */
-    public BSONException( final String errorCode, final String msg ) {
+    public BSONException( final int errorCode, final String msg ) {
         super( msg );
         _errorCode = errorCode;
     }
@@ -52,7 +52,7 @@ public class BSONException extends RuntimeException {
      * @param msg The error message.
      * @param t The throwable cause.
      */
-    public BSONException( final String errorCode, final String msg, final Throwable t ) {
+    public BSONException( final int errorCode, final String msg, final Throwable t ) {
         super( msg,  t );
         _errorCode = errorCode;
     }
@@ -61,7 +61,7 @@ public class BSONException extends RuntimeException {
      * Returns the error code.
      * @return The error code.
      */
-    public String getErrorCode() { return _errorCode; }
+    public Integer getErrorCode() { return _errorCode; }
 
     /**
      * Returns true if the error code is set (i.e., not null).
@@ -70,6 +70,6 @@ public class BSONException extends RuntimeException {
         return (_errorCode != null) ? true : false; 
     }
 
-    private String _errorCode = null;
+    private Integer _errorCode = null;
 }
 
