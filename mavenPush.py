09392028897123bd8939f59cca1c469150d1bb93
Scott Hernandez <scotthernandez@gmail.com>
2011-05-27 16:06:41 -0700

    JAVA-278: create maven-metadata.xml files

--- mavenPush.py
+++ mavenPush.py
@@ -2,10 +2,13 @@
 
 # http://repo1.maven.org/maven2/org/mongodb/mongo-java-driver/
 
+from xml.dom.minidom import parseString
+import datetime
 import os
 import sys
 import shutil
 import subprocess
+import time
 
 if len( sys.argv ) == 1:
     print "Usage: mavenPush.py VERSION [PUBDIR]"
@@ -27,6 +30,23 @@ if p[0].find( "SUCCESSFUL" ) < 0:
     print( p[0] )
     print( p[1] )
     raise( "build failed" )
+
+def build_metadata_xml(path, groupid, artifactid):
+    xml = '<metadata>'
+    xml += '<groupId>org.%s</groupId>' % (groupid,)
+    xml += '<artifactId>%s</artifactId>' % (artifactid,)
+    xml += '<versioning><versions>'
+    print( "listing versions in %s" % (path,) )
+    entries = os.listdir(path)
+    for entry in entries:
+        if os.path.isdir(os.path.join(path, entry)):
+            xml += '<version>%s</version>' % (entry,)
+    xml += '</versions>'
+    xml += '<lastUpdated>%s</lastUpdated>' % (int(time.time()*1000),)
+    xml += '</versioning></metadata>'
+
+    doc = parseString(xml)
+    return doc.documentElement.toprettyxml()
 
 def go( pkgName, shortName , longName ):
     dir = root + pkgName + longName + "/" + version
@@ -50,7 +70,10 @@ def go( pkgName, shortName , longName ):
     out = open( fileRoot + ".jar.sha1" , 'w' )
     out.write( sha1 )
     out.close()
-
+    
+    out = open( os.path.join(os.path.split(dir)[0], "maven-metadata.xml") , 'w' )
+    out.write( build_metadata_xml(os.path.split(dir)[0], shortName, longName) )
+    out.close()
 
 go( "/org/mongodb/" , "mongo" , "mongo-java-driver" )
 go( "/org/bson/" , "bson" , "bson" )
