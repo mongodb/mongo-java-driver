2894ab4fc761d7324d364132615bf695d8c4b009
Brendan W. McAdams <brendan@10gen.com>
2011-08-09 14:23:14 -0400

    Fixes JAVA-411, findOne fails when fields are specified but no document is found ( NullPointerException )
    * Test for nullness on DBObject prior to setting PartialObject when fields are set

--- src/main/com/mongodb/DBCollection.java
+++ src/main/com/mongodb/DBCollection.java
@@ -564,7 +564,7 @@ public abstract class DBCollection {
     public final DBObject findOne( DBObject o, DBObject fields ) {
         Iterator<DBObject> i = __find( o , fields , 0 , -1 , 0, getOptions() );
         DBObject obj = (i == null ? null : i.next());
-        if ( fields != null && fields.keySet().size() > 0 ){
+        if ( obj != null && ( fields != null && fields.keySet().size() > 0 ) ){
             obj.markAsPartialObject();
         }
         return obj;
