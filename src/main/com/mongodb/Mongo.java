10026dcc06da2ac2a64ff95457507f98d73d22c1
agirbal <antoine@10gen.com>
2011-08-08 17:13:50 -0700

    trunk back to 2.7

--- src/main/com/mongodb/Mongo.java
+++ src/main/com/mongodb/Mongo.java
@@ -106,7 +106,7 @@ public class Mongo {
     /**
      *
      */
-    public static final int MINOR_VERSION = 6;
+    public static final int MINOR_VERSION = 7;
 
     static int cleanerIntervalMS;
     static {
