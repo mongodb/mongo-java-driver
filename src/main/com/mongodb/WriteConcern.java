86a38c89badf12ffc4540d0a20cdd53025de261d
Brendan W. McAdams <brendan@10gen.com>
2011-09-05 16:07:23 +0100

    JAVA-425 - Support MongoDB Server 2.0 getLastError Changes (j, w=string/number) * Migrated WriteConcern to support String or Int arguments against w; call getWValue or getWString for non-int values. * Added "J" flag to WriteConcern

--- src/main/com/mongodb/WriteConcern.java
+++ src/main/com/mongodb/WriteConcern.java
@@ -1,7 +1,7 @@
 // WriteConcern.java
 
 /**
- *      Copyright (C) 2008 10gen Inc.
+ *      Copyright (C) 2008-2011 10gen Inc.
  *
  *   Licensed under the Apache License, Version 2.0 (the "License");
  *   you may not use this file except in compliance with the License.
@@ -54,41 +54,21 @@ public class WriteConcern {
     
     /** Exceptions are raised for network issues, and server errors; waits on a server for the write operation */
     public final static WriteConcern SAFE = new WriteConcern(1);
-    
-    /** Exceptions are raised for network issues, and server errors and the write operation waits for the server to flush the data to disk*/
+
+    /** Exceptions are raised for network issues, and server errors; waits on a majority of servers for the write operation */
+    public final static WriteConcern MAJORITY_SAFE = new MajorityWriteConcern();
+
+    /** Exceptions are raised for network issues, and server errors; the write operation waits for the server to flush the data to disk*/
     public final static WriteConcern FSYNC_SAFE = new WriteConcern(true);
+
+    /** Exceptions are raised for network issues, and server errors; the write operation waits for the server to group commit to the journal file on disk*/
+    public final static WriteConcern JOURNAL_SAFE = new WriteConcern( 1, 0, false, true );
 
     /** Exceptions are raised for network issues, and server errors; waits for at least 2 servers for the write operation*/
     public final static WriteConcern REPLICAS_SAFE = new WriteConcern(2);
     
     // map of the constants from above for use by fromString
     private static Map<String, WriteConcern> _namedConcerns = null;
-
-    /**
-     * Gets the WriteConcern constants by name: NONE, NORMAL, SAFE, FSYNC_SAFE,
-     * REPLICA_SAFE. (matching is done case insensitively)
-     * @param name 
-     * @return
-     */
-    public static WriteConcern valueOf(String name) {
-        if (_namedConcerns == null) {
-            HashMap<String, WriteConcern> newMap = new HashMap<String, WriteConcern>( 8 , 1 );
-            for (Field f : WriteConcern.class.getFields())
-                if (Modifier.isStatic( f.getModifiers() ) && f.getType().equals( WriteConcern.class )) {
-                    try {
-                        newMap.put( f.getName().toLowerCase(), (WriteConcern) f.get( null ) );
-                    } catch (Exception e) {
-                        throw new RuntimeException( e );
-                    }
-                }
-            
-            // Thought about doing a synchronize but this seems just as safe and
-            // I don't care about race conditions.
-            _namedConcerns = newMap;
-        }
-        
-        return _namedConcerns.get( name.toLowerCase() );
-    }
 
     /**
      * Default constructor keeping all options as default
@@ -138,18 +118,76 @@ public class WriteConcern {
      * @param fsync whether or not to fsync
      */
     public WriteConcern( int w , int wtimeout , boolean fsync ){
-        _w = w;
+        this(w, wtimeout, fsync, false);
+    }
+
+    /**
+     * Creates a WriteConcern object.
+     * <p>Specifies the number of servers to wait for on the write operation, and exception raising behavior </p>
+     *	<p> w represents the number of servers:
+     * 		<ul>
+     * 			<li>{@code w=-1} None, no checking is done</li>
+     * 			<li>{@code w=0} None, network socket errors raised</li>
+     * 			<li>{@code w=1} Checks server for errors as well as network socket errors raised</li>
+     * 			<li>{@code w>1} Checks servers (w) for errors as well as network socket errors raised</li>
+     * 		</ul>
+     * 	</p>
+     * @param w number of writes
+     * @param wtimeout timeout for write operation
+     * @param fsync whether or not to fsync
+     * @param j whether writes should wait for a journaling group commit
+     */
+    public WriteConcern( int w , int wtimeout , boolean fsync, boolean j ){
+        _wValue = w;
         _wtimeout = wtimeout;
         _fsync = fsync;
-        
+        _j = j;
+
         _command = new BasicDBObject( "getlasterror" , 1 );
-        if ( _w > 0 ){
-            _command.put( "w" , _w );
-            _command.put( "wtimeout" , wtimeout );
-        }
+
+        _command.put( "w" , _wValue );
+        _command.put( "wtimeout" , wtimeout );
 
         if ( _fsync )
             _command.put( "fsync" , true );
+
+        if ( _j )
+            _command.put( "j", true );
+
+    }
+
+    /**
+     * Creates a WriteConcern object.
+     * <p>Specifies the number of servers to wait for on the write operation, and exception raising behavior </p>
+     *	<p> w represents the number of servers:
+     * 		<ul>
+     * 			<li>{@code w=-1} None, no checking is done</li>
+     * 			<li>{@code w=0} None, network socket errors raised</li>
+     * 			<li>{@code w=1} Checks server for errors as well as network socket errors raised</li>
+     * 			<li>{@code w>1} Checks servers (w) for errors as well as network socket errors raised</li>
+     * 		</ul>
+     * 	</p>
+     * @param w number of writes
+     * @param wtimeout timeout for write operation
+     * @param fsync whether or not to fsync
+     * @param j whether writes should wait for a journaling group commit
+     */
+    public WriteConcern( String w , int wtimeout , boolean fsync, boolean j ){
+        _wValue = w;
+        _wtimeout = wtimeout;
+        _fsync = fsync;
+        _j = j;
+
+        _command = new BasicDBObject( "getlasterror" , 1 );
+
+        _command.put( "w" , _wValue );
+        _command.put( "wtimeout" , wtimeout );
+
+        if ( _fsync )
+            _command.put( "fsync" , true );
+
+        if ( _j )
+            _command.put( "j", true );
     }
 
     /**
@@ -162,11 +200,23 @@ public class WriteConcern {
 
     /**
      * Gets the number of servers to write to
+     * if W is not a string value, returns -999.
+     *
+     * You should migrate to using getWValue (returns Object)
+     * or getWString (String)
      * @return
      */
     public int getW(){
-        return _w;
+        if (_wValue instanceof Integer)
+            return (Integer) _wValue;
+        else
+            return -999;
     }
+
+    public String getWString(){
+        return _wValue.toString();
+    }
+
 
     /**
      * Gets the write timeout (in milliseconds)
@@ -177,7 +227,7 @@ public class WriteConcern {
     }
 
     /**
-     * Returns whether files are synced to disk
+     * Returns whether writes wait for files to be synced to disk
      * @return
      */
     public boolean fsync(){
@@ -185,11 +235,20 @@ public class WriteConcern {
     }
 
     /**
+     * Returns whether writes will await a group commit to the
+     * journal.
+     * @return boolean
+     */
+    public boolean j(){
+        return _j;
+    }
+
+    /**
      * Returns whether network error may be raised (w >= 0)
      * @return
      */
     public boolean raiseNetworkErrors(){
-        return _w >= 0;
+        return (Integer) _wValue >= 0;
     }
 
     /**
@@ -197,7 +256,33 @@ public class WriteConcern {
      * @return
      */
     public boolean callGetLastError(){
-        return _w > 0;
+        return (Integer) _wValue  > 0;
+    }
+
+    /**
+     * Gets the WriteConcern constants by name: NONE, NORMAL, SAFE, FSYNC_SAFE,
+     * REPLICA_SAFE. (matching is done case insensitively)
+     * @param name
+     * @return
+     */
+    public static WriteConcern valueOf(String name) {
+        if (_namedConcerns == null) {
+            HashMap<String, WriteConcern> newMap = new HashMap<String, WriteConcern>( 8 , 1 );
+            for (Field f : WriteConcern.class.getFields())
+                if (Modifier.isStatic( f.getModifiers() ) && f.getType().equals( WriteConcern.class )) {
+                    try {
+                        newMap.put( f.getName().toLowerCase(), (WriteConcern) f.get( null ) );
+                    } catch (Exception e) {
+                        throw new RuntimeException( e );
+                    }
+                }
+
+            // Thought about doing a synchronize but this seems just as safe and
+            // I don't care about race conditions.
+            _namedConcerns = newMap;
+        }
+
+        return _namedConcerns.get( name.toLowerCase() );
     }
 
     @Override
@@ -212,17 +297,34 @@ public class WriteConcern {
 
         WriteConcern that = (WriteConcern) o;
 
-        if ( _fsync != that._fsync ) return false;
-        if ( _w != that._w ) return false;
-        if ( _wtimeout != that._wtimeout ) return false;
+        return _fsync == that._fsync && _wValue == that._wValue && _wtimeout == that._wtimeout && _j == that._j;
 
-        return true;
     }
 
-    final int _w;
+    /**
+     * Gets the number of servers to write to
+     * @return
+     */
+    public Object getWValue(){
+        return _wValue;
+    }
+
+    final Object _wValue;
     final int _wtimeout;
     final boolean _fsync;
+    final boolean _j;
 
     final BasicDBObject _command;
 
+    public static class MajorityWriteConcern extends WriteConcern {
+
+        public MajorityWriteConcern( ) {
+            super( "majority", 0, false, false );
+        }
+
+        public MajorityWriteConcern( int wtimeout, boolean fsync, boolean j ){
+            super( "majority", wtimeout, fsync, j );
+        }
+
+    }
 }
