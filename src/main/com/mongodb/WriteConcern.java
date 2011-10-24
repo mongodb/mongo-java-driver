3a8549e88f21e4fa7e0e11d4f2b396259ea403a5
Brendan W. McAdams <brendan@10gen.com>
2011-10-24 23:09:38 +0100

    Fix dangling compile issues w/ tags; correct logic on getLastError defaults

--- src/main/com/mongodb/WriteConcern.java
+++ src/main/com/mongodb/WriteConcern.java
@@ -242,7 +242,8 @@ public class WriteConcern {
     public BasicDBObject getCommand(){
         BasicDBObject _command = new BasicDBObject( "getlasterror" , 1 );
 
-        if ( _wValue instanceof Integer && ( (Integer) _wValue > 0) ){
+        if ( _wValue instanceof Integer && ( (Integer) _wValue > 0) || 
+            ( _wValue instanceof String && _wValue != null ) ){
             _command.put( "w" , _wValue );
             _command.put( "wtimeout" , _wtimeout );
         }
--- src/test/com/mongodb/ReplSetTest.java
+++ src/test/com/mongodb/ReplSetTest.java
@@ -85,8 +85,7 @@ public class ReplSetTest {
         while ( true ){
             _sleep();
             try {
-                DBObject x = c.findOne(new BasicDBObject( "_id", 17 ), null,
-                                       ReadPreference.withTags( new BasicDBObject("dc", "proximacentauri") ));
+                DBObject x = c.findOne(new BasicDBObject( "_id", 17 ), null);
                 System.out.println( x );
                 Integer n = (Integer) x.get( "x" );
                 if (n != null && n >= 150 )
