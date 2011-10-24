defc4a7897d1b83eb7fab1e38a05cdbcbade880b
Scott Hernandez <scotthernandez@gmail.com>
2011-10-24 17:41:17 +1100

    JAVA-444: query before insert on index creation.

--- src/main/com/mongodb/DBApiLayer.java
+++ src/main/com/mongodb/DBApiLayer.java
@@ -359,7 +359,10 @@ public class DBApiLayer extends DB {
                 full.put( k , options.get( k ) );
             full.put( "key" , keys );
 
-            DBApiLayer.this.doGetCollection( "system.indexes" ).insert( new DBObject[]{ full } , false , WriteConcern.SAFE, encoder );
+            DBCollection idxs = DBApiLayer.this.doGetCollection( "system.indexes" );
+            //query first, maybe we should do an update w/upsert? -- need to test performance and lock behavior
+            if ( idxs.findOne( full ) == null )
+                idxs.insert( new DBObject[] { full }, WriteConcern.SAFE, encoder );
         }
 
         final String _fullNameSpace;
