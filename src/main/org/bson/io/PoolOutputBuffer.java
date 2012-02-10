df5fbe029122711255afe65c927435b0632d303b
Ryan <rgnitz@gmail.com>
2012-02-10 10:00:03 -0500

    added constants and made some vars final

--- src/main/org/bson/io/PoolOutputBuffer.java
+++ src/main/org/bson/io/PoolOutputBuffer.java
@@ -26,7 +26,7 @@ import java.io.*;
 import java.util.*;
 
 public class PoolOutputBuffer extends OutputBuffer {
-    
+
     public static final int BUF_SIZE = 1024 * 16;
 
     public PoolOutputBuffer(){
@@ -45,20 +45,20 @@ public class PoolOutputBuffer extends OutputBuffer {
     public int getPosition(){
         return _cur.pos();
     }
-    
+
     public void setPosition( int position ){
         _cur.reset( position );
     }
-    
+
     public void seekEnd(){
         _cur.reset( _end );
     }
-    
+
     public void seekStart(){
         _cur.reset();
     }
 
-    
+
     public int size(){
         return _end.pos();
     }
@@ -84,9 +84,9 @@ public class PoolOutputBuffer extends OutputBuffer {
         bs[_cur.getAndInc()] = (byte)(b&0xFF);
         _afterWrite();
     }
-    
+
     void _afterWrite(){
-        
+
         if ( _cur.pos() < _end.pos() ){
             // we're in the middle of the total space
             // just need to make sure we're not at the end of a buffer
@@ -94,17 +94,17 @@ public class PoolOutputBuffer extends OutputBuffer {
                 _cur.nextBuffer();
             return;
         }
-        
+
         _end.reset( _cur );
-        
+
         if ( _end.y < BUF_SIZE )
             return;
-        
+
         _fromPool.add( _extra.get() );
         _end.nextBuffer();
         _cur.reset( _end );
     }
-    
+
     byte[] _cur(){
         return _get( _cur.x );
     }
@@ -115,21 +115,21 @@ public class PoolOutputBuffer extends OutputBuffer {
         return _fromPool.get(z);
     }
 
-    public int pipe( OutputStream out )
+    public int pipe( final OutputStream out )
         throws IOException {
-        
+
         if ( out == null )
             throw new NullPointerException( "out is null" );
 
         int total = 0;
-        
+
         for ( int i=-1; i<_fromPool.size(); i++ ){
-            byte[] b = _get( i );
-            int amt = _end.len( i );
+            final byte[] b = _get( i );
+            final int amt = _end.len( i );
             out.write( b , 0 , amt );
             total += amt;
         }
-        
+
         return total;
     }
 
@@ -137,12 +137,12 @@ public class PoolOutputBuffer extends OutputBuffer {
         Position(){
             reset();
         }
-        
+
         void reset(){
             x = -1;
             y = 0;
-        } 
-        
+        }
+
         void reset( Position other ){
             x = other.x;
             y = other.y;
@@ -152,7 +152,7 @@ public class PoolOutputBuffer extends OutputBuffer {
             x = ( pos / BUF_SIZE ) - 1;
             y = pos % BUF_SIZE;
         }
-        
+
         int pos(){
             return ( ( x + 1 ) * BUF_SIZE ) + y;
         }
@@ -166,7 +166,7 @@ public class PoolOutputBuffer extends OutputBuffer {
             if ( y > BUF_SIZE )
                 throw new IllegalArgumentException( "something is wrong" );
         }
-        
+
         void nextBuffer(){
             if ( y != BUF_SIZE )
                 throw new IllegalArgumentException( "broken" );
@@ -187,24 +187,28 @@ public class PoolOutputBuffer extends OutputBuffer {
         int x; // which buffer -1 == _mine
         int y; // position in buffer
     }
-    
+
     public String asAscii(){
         if ( _fromPool.size() > 0 )
             return super.asString();
-        
+
         final int m = size();
-        char c[] = m < _chars.length ? _chars : new char[m];
+        final char c[] = m < _chars.length ? _chars : new char[m];
+
         for ( int i=0; i<m; i++ )
             c[i] = (char)_mine[i];
+
         return new String( c , 0  , m );
     }
 
     public String asString( String encoding )
         throws UnsupportedEncodingException {
+
+
         if ( _fromPool.size() > 0 )
             return super.asString( encoding );
 
-        if ( encoding.equals( "UTF8" ) || encoding.equals( "UTF-8" ) ){
+        if ( encoding.equals( DEFAULT_ENCODING_1 ) || encoding.equals( DEFAULT_ENCODING_2) ){
             try {
                 return _encoding.decode( _mine , 0 , size() );
             }
@@ -214,19 +218,22 @@ public class PoolOutputBuffer extends OutputBuffer {
         }
         return new String( _mine , 0 , size() , encoding );
     }
-    
-    
+
+
     final byte[] _mine = new byte[BUF_SIZE];
     final char[] _chars = new char[BUF_SIZE];
     final List<byte[]> _fromPool = new ArrayList<byte[]>();
     final UTF8Encoding _encoding = new UTF8Encoding();
 
+    private static final String DEFAULT_ENCODING_1 = "UTF-8";
+    private static final String DEFAULT_ENCODING_2 = "UTF8";
+
     private final Position _cur = new Position();
     private final Position _end = new Position();
-    
-    private static org.bson.util.SimplePool<byte[]> _extra = 
+
+    private static org.bson.util.SimplePool<byte[]> _extra =
         new org.bson.util.SimplePool<byte[]>( ( 1024 * 1024 * 10 ) / BUF_SIZE ){
-        
+
         protected byte[] createNew(){
             return new byte[BUF_SIZE];
         }
