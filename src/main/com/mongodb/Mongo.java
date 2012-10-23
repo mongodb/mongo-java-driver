bfbcc0a3714ef781cf2a0cd52d45a0d9e79e3163
Jeff Yemin <jeff.yemin@10gen.com>
2012-10-23 13:24:49 -0400

    Updating version to 2.9.3-SNAPSHOT

--- src/main/com/mongodb/Mongo.java
+++ src/main/com/mongodb/Mongo.java
@@ -84,7 +84,7 @@ public class Mongo {
     @Deprecated
     public static final int MINOR_VERSION = 9;
 
-    private static final String FULL_VERSION = "2.9.2";
+    private static final String FULL_VERSION = "2.9.3-SNAPSHOT";
 
     static int cleanerIntervalMS;
     static {
