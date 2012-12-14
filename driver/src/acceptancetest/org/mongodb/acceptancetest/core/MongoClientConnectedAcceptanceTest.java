9edda9951cb8ec768a39d90c9cd6ff520220f94a
trishagee <trishagee@github.com>
2012-12-14 15:16:34 +0000

    Added the first acceptance test

---
+++ driver/src/acceptancetest/org/mongodb/acceptancetest/core/MongoClientConnectedAcceptanceTest.java
@@ -0,0 +1,76 @@
+/*
+ * Copyright (c) 2008 - 2012 10gen, Inc. <http://10gen.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.mongodb.acceptancetest.core;
+
+import org.bson.types.Document;
+import org.junit.Before;
+import org.junit.Ignore;
+import org.junit.Test;
+import org.mongodb.CommandDocument;
+import org.mongodb.MongoClient;
+import org.mongodb.ServerAddress;
+import org.mongodb.impl.SingleServerMongoClient;
+import org.mongodb.operation.MongoCommandOperation;
+import org.mongodb.serialization.PrimitiveSerializers;
+import org.mongodb.serialization.serializers.DocumentSerializer;
+
+import java.net.UnknownHostException;
+
+import static org.hamcrest.CoreMatchers.not;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class MongoClientConnectedAcceptanceTest {
+
+    private static final String SERVER_NAME = "localhost";
+    private static final int PORT = 27017;
+    private ServerAddress serverAddress;
+
+    @Before
+    public void setUp() throws UnknownHostException {
+        serverAddress = new ServerAddress(SERVER_NAME, PORT);
+    }
+
+    @Test
+    public void shouldBeConnectedToMongoAsSoonAsNewSingleServerMongoClientIsCreated() {
+        final MongoClient mongoClient = new SingleServerMongoClient(serverAddress);
+
+        final MongoCommandOperation pingCommand = new MongoCommandOperation(new CommandDocument("ping", 1));
+        final DocumentSerializer basicSerializer = new DocumentSerializer(PrimitiveSerializers.createDefault());
+        final Document pingResult = mongoClient.getOperations().executeCommand("anyDatabase",
+                                                                               pingCommand,
+                                                                               basicSerializer);
+
+        assertThat((Double) pingResult.get("ok"), is(1.0));
+    }
+
+    @Test
+    @Ignore("Not working at the moment because SingleServerMongoClient hasn't implemented close")
+    public void shouldDisconnectFromServerWhenRequested() {
+        final MongoClient mongoClient = new SingleServerMongoClient(serverAddress);
+
+        mongoClient.close();
+
+        final MongoCommandOperation pingCommand = new MongoCommandOperation(new CommandDocument("ping", 1));
+        final DocumentSerializer basicSerializer = new DocumentSerializer(PrimitiveSerializers.createDefault());
+        final Document pingResult = mongoClient.getOperations().executeCommand("anyDatabase",
+                                                                               pingCommand,
+                                                                               basicSerializer);
+
+        assertThat((Double) pingResult.get("ok"), not(1.0));
+    }
+}
