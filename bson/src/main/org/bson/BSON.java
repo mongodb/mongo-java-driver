f3c00410f5dff40370b670fa672726a39f93b6f0
Jeff Yemin <jeff.yemin@10gen.com>
2012-12-18 10:23:56 -0500

    Simplified BSON class

--- bson/src/main/org/bson/BSON.java
+++ bson/src/main/org/bson/BSON.java
@@ -18,14 +18,8 @@
 
 package org.bson;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.logging.Logger;
-import java.util.regex.Pattern;
-
+// TODO: the basics should become an enum for new code.  And this whole class should then move to driver-compat
 public class BSON {
-
-    static final Logger LOGGER = Logger.getLogger("org.bson.BSON");
 
     // ---- basics ----
 
@@ -64,111 +58,17 @@ public class BSON {
     public static final byte B_BINARY = 2;
     public static final byte B_UUID = 3;
 
-    // ---- regular expression handling ----
+    public static int toInt( Object o ){
+        if ( o == null )
+            throw new NullPointerException( "can't be null" );
 
-    /**
-     * Converts a string of regular expression flags from the database in Java regular expression flags.
-     *
-     * @param flags flags from database
-     * @return the Java flags
-     */
-    public static int regexFlags(String flags) {
-        int fint = 0;
-        if (flags == null || flags.length() == 0) {
-            return fint;
-        }
+        if ( o instanceof Number )
+            return ((Number)o).intValue();
 
-        flags = flags.toLowerCase();
+        if ( o instanceof Boolean )
+            return ((Boolean)o) ? 1 : 0;
 
-        for (int i = 0; i < flags.length(); i++) {
-            final RegexFlag flag = RegexFlag.getByCharacter(flags.charAt(i));
-            if (flag != null) {
-                fint |= flag.javaFlag;
-                if (flag.unsupported != null) {
-                    _warnUnsupportedRegex(flag.unsupported);
-                }
-            }
-            else {
-                throw new IllegalArgumentException("unrecognized flag [" + flags.charAt(i) + "] " + (int) flags.charAt(i));
-            }
-        }
-        return fint;
+        throw new IllegalArgumentException( "can't convert: " + o.getClass().getName() + " to int" );
     }
 
-    public static int regexFlag(final char c) {
-        final RegexFlag flag = RegexFlag.getByCharacter(c);
-        if (flag == null) {
-            throw new IllegalArgumentException("unrecognized flag [" + c + "]");
-        }
-
-        if (flag.unsupported != null) {
-            _warnUnsupportedRegex(flag.unsupported);
-            return 0;
-        }
-
-        return flag.javaFlag;
-    }
-
-    /**
-     * Converts Java regular expression flags into a string of flags for the database
-     *
-     * @param flags Java flags
-     * @return the flags for the database
-     */
-    public static String regexFlags(int flags) {
-        final StringBuilder buf = new StringBuilder();
-
-        for (final RegexFlag flag : RegexFlag.values()) {
-            if ((flags & flag.javaFlag) > 0) {
-                buf.append(flag.flagChar);
-                flags -= flag.javaFlag;
-            }
-        }
-
-        if (flags > 0) {
-            throw new IllegalArgumentException("some flags could not be recognized.");
-        }
-
-        return buf.toString();
-    }
-
-    private static enum RegexFlag {
-        CANON_EQ(Pattern.CANON_EQ, 'c', "Pattern.CANON_EQ"),
-        UNIX_LINES(Pattern.UNIX_LINES, 'd', "Pattern.UNIX_LINES"),
-        GLOBAL(GLOBAL_FLAG, 'g', null),
-        CASE_INSENSITIVE(Pattern.CASE_INSENSITIVE, 'i', null),
-        MULTILINE(Pattern.MULTILINE, 'm', null),
-        DOTALL(Pattern.DOTALL, 's', "Pattern.DOTALL"),
-        LITERAL(Pattern.LITERAL, 't', "Pattern.LITERAL"),
-        UNICODE_CASE(Pattern.UNICODE_CASE, 'u', "Pattern.UNICODE_CASE"),
-        COMMENTS(Pattern.COMMENTS, 'x', null);
-
-        private static final Map<Character, RegexFlag> byCharacter = new HashMap<Character, RegexFlag>();
-
-        static {
-            for (final RegexFlag flag : values()) {
-                byCharacter.put(flag.flagChar, flag);
-            }
-        }
-
-        public static RegexFlag getByCharacter(final char ch) {
-            return byCharacter.get(ch);
-        }
-
-        public final int javaFlag;
-        public final char flagChar;
-        public final String unsupported;
-
-        RegexFlag(final int f, final char ch, final String u) {
-            javaFlag = f;
-            flagChar = ch;
-            unsupported = u;
-        }
-    }
-
-    private static void _warnUnsupportedRegex(final String flag) {
-        LOGGER.info("flag " + flag + " not supported by db.");
-    }
-
-    private static final int GLOBAL_FLAG = 256;
 }
