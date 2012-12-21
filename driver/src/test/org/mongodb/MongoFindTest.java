e8f56639ddd8bcf126aa92d5a6caab9726f1c81d
Jeff Yemin <jeff.yemin@10gen.com>
2012-12-21 17:47:49 -0500

    Added MongoFindTest

---
+++ driver/src/test/org/mongodb/MongoFindTest.java
@@ -0,0 +1,39 @@
+/*
+ * Copyright (c) 2008 - 2012 10gen, Inc. <http://10gen.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package org.mongodb;
+
+import org.bson.types.Document;
+import org.junit.Test;
+import org.mongodb.operation.MongoFind;
+import org.mongodb.operation.MongoInsert;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
+public class MongoFindTest extends MongoClientTestBase {
+    @Test
+    public void shouldThrowQueryFailureException() {
+        collection.insert(new MongoInsert<Document>(new Document("loc", new double[] {0, 0})));
+        try {
+            collection.findOne(new MongoFind(new QueryFilterDocument("loc", new Document("$near", new double[] {0, 0}))));
+            fail("Should be a query failure since there is no 2d index");
+        } catch (MongoQueryFailureException e) {
+            assertEquals(13038, e.getCode());
+        }
+    }
+}
