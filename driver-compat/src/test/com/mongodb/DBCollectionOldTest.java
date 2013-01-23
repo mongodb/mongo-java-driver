274c7afce75f6fcca4fc176079bd5f8d4d004284
Jeff Yemin <jeff.yemin@10gen.com>
2013-01-23 13:51:01 -0500

    Removed import *

--- driver-compat/src/test/com/mongodb/DBCollectionOldTest.java
+++ driver-compat/src/test/com/mongodb/DBCollectionOldTest.java
@@ -22,7 +22,10 @@ import org.junit.Test;
 
 import java.util.List;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.fail;
 
 public class DBCollectionOldTest extends MongoClientTestBase {
     @Test
