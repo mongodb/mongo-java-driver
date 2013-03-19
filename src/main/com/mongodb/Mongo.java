ffc3d136b90620dfb9440d628e2bede584cc3a69
Jeff Yemin <jeff.yemin@10gen.com>
2013-03-19 13:10:02 -0400

    Setting next release to 2.12.0-SNAPSHOT

--- src/main/com/mongodb/Mongo.java
+++ src/main/com/mongodb/Mongo.java
@@ -89,9 +89,9 @@ public class Mongo {
      * @deprecated Replaced by <code>Mongo.getMinorVersion()</code>
      */
     @Deprecated
-    public static final int MINOR_VERSION = 11;
+    public static final int MINOR_VERSION = 12;
 
-    private static final String FULL_VERSION = "2.11.0";
+    private static final String FULL_VERSION = "2.12.0-SNAPSHOT";
 
     static int cleanerIntervalMS;
 
