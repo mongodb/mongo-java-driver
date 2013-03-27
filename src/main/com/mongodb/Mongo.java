4b0c6127a4181e059e9c19ebfdfa034305888d05
Jeff Yemin <jeff.yemin@10gen.com>
2013-03-27 13:49:52 -0400

    Preparing 2.11.1-SNAPSHOT

--- src/main/com/mongodb/Mongo.java
+++ src/main/com/mongodb/Mongo.java
@@ -91,7 +91,7 @@ public class Mongo {
     @Deprecated
     public static final int MINOR_VERSION = 11;
 
-    private static final String FULL_VERSION = "2.11.0";
+    private static final String FULL_VERSION = "2.11.1-SNAPSHOT";
 
     static int cleanerIntervalMS;
 
