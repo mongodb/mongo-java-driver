647b37a237792b777cc646815e7ad33d3d3123e5
Uladzimir Mihura <trnl.me@gmail.com>
2013-04-07 22:23:42 +0300

    Added Clirr 0.7 as local lib. Updated Clirr plugin.

--- buildSrc/src/main/groovy/org/gradle/api/plugins/clirr/ClirrPlugin.groovy
+++ buildSrc/src/main/groovy/org/gradle/api/plugins/clirr/ClirrPlugin.groovy
@@ -28,61 +28,85 @@ import org.gradle.api.reporting.ReportingExtension
 
 class ClirrPlugin implements Plugin<Project> {
 
+    Configuration clirrConfiguration
+    Configuration baseConfiguration
+    ClirrPluginExtension extension
+
+    @Override
     void apply(Project project) {
-        project.apply(plugin: JavaBasePlugin)
-        project.apply(plugin: ReportingBasePlugin)
+        createConfigurations(project)
+        createExtension(project)
+        addClirrTask(project)
+    }
 
-        def reportingExtension = project.extensions.getByType(ReportingExtension)
-
-        def extension = project.extensions.create("clirr", ClirrPluginExtension)
-
-        Configuration clirrConfiguration = project.configurations.add("clirr")
-        Dependency clirrDependency = project.dependencies.create('net.sf.clirr:clirr-core:0.6')
-        clirrConfiguration.dependencies.add(clirrDependency)
-
-        extension.conventionMapping.with {
-            map('reportsDir') { reportingExtension.file("clirr") }
-            map('baseline') { "$project.group:$project.name:(,$project.version)".toString() }
-            map('formats') { ['plain'] }
-            map('failOnBinWarning') { false }
-            map('failOnBinError') { true }
-            map('failOnSrcWarning') { false }
-            map('failOnSrcError') { true }
+    void createConfigurations(Project project) {
+        clirrConfiguration = project.configurations.add('clirr').with {
+            visible = false
+            transitive = true
+            description = "The Clirr libraries to be used for this project."
+            return (Configuration) delegate
         }
 
+        baseConfiguration = project.configurations.add('base').with {
+            visible = false
+            transitive = false
+            return (Configuration) delegate
+        }
+    }
 
-        def jarTask = project.tasks.getByName("jar");
+    void createExtension(Project project) {
+        project.apply(plugin: ReportingBasePlugin)
 
-        def clirrTask = project.task('clirr', dependsOn: jarTask) << {
+        extension = project.extensions.create('clirr', ClirrPluginExtension).with {
+            reportsDir = new File("${project.reporting.baseDir.absolutePath}/clirr")
+            baseline = "$project.group:$project.name:(,$project.version)".toString()
+            formats = ['plain']
+            failOnBinWarning = false
+            failOnBinError = true
+            failOnSrcWarning = false
+            failOnSrcError = true
+            return (ClirrPluginExtension) delegate
+        }
+    }
 
-            Configuration configuration = project.configurations.add("baseline").setTransitive(false)
-            configuration.dependencies.add(project.dependencies.create(extension.baseline))
+    void addClirrTask(Project project) {
+        project.apply(plugin: JavaBasePlugin)
 
-            File jar = configuration.resolve().find { it.name.endsWith('.jar') }
+        def jarTask = project.tasks.getByName("jar")
 
-            ant.taskdef(resource: 'clirrtask.properties', classpath: project.configurations.clirr.asPath)
+        def clirrTask = project.task('clirr', dependsOn: jarTask) {
+            doFirst {
+                project.dependencies {
+                    clirr project.parent.files('buildSrc/lib/clirr-core-0.7-SNAPSHOT.jar')
+                    clirr 'asm:asm-all:2.2'
+                    base "${extension.baseline}"
+                }
 
-            extension.reportsDir.mkdirs()
+                ant.taskdef(
+                        resource: 'clirrtask.properties',
+                        classpath: clirrConfiguration.asPath
+                )
 
-            inputs.file jar
-            outputs.dir extension.reportsDir
+                extension.reportsDir.mkdirs()
 
-            ant.clirr(
-                    failOnBinWarning: extension.failOnBinWarning,
-                    failOnBinError: extension.failOnBinError,
-                    failOnSrcWarning: extension.failOnSrcWarning,
-                    failOnSrcError: extension.failOnSrcError
-            ) {
-                origfiles(file: jar.getPath())
-                newfiles(dir: jarTask.destinationDir, includes: jarTask.archiveName)
-                extension.formats.each { format ->
-                    formatter(type: format, outfile: "$extension.reportsDir/report.${format == 'xml' ? 'xml' : 'txt'}")
+                File baseJar = baseConfiguration.resolve().find { it.name.endsWith('.jar') }
+
+                ant.clirr(
+                        failOnBinWarning: extension.failOnBinWarning,
+                        failOnBinError: extension.failOnBinError,
+                        failOnSrcWarning: extension.failOnSrcWarning,
+                        failOnSrcError: extension.failOnSrcError
+                ) {
+                    origfiles(file: baseJar.getPath())
+                    newfiles(dir: jarTask.destinationDir, includes: jarTask.archiveName)
+                    extension.formats.each { format ->
+                        formatter(type: format, outfile: "$extension.reportsDir/report.${format == 'xml' ? 'xml' : 'txt'}")
+                    }
                 }
             }
         }
 
         project.tasks.getByName("check").dependsOn(clirrTask)
-
     }
 
 }
