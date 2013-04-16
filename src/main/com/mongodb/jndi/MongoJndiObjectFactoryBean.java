17d35069482d2dc3ecc0b38badea4f04283be1b4
juanlmelo@gmail.com <juanlmelo@gmail.com>
2013-04-16 06:15:07 -0700

    Added MongoJndiObjectFactoryBean

---
+++ src/main/com/mongodb/jndi/MongoJndiObjectFactoryBean.java
@@ -0,0 +1,71 @@
+//MongoJndiObjectFactoryBean.java
+/**
+ *      Copyright (C) 2013 10gen Inc.
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ *   
+ *   Author:
+ *   Juan Luis Melo
+ *   
+ */
+
+
+package com.mongodb.jndi;
+
+import java.util.Enumeration;
+import java.util.Hashtable;
+
+import javax.naming.Context;
+import javax.naming.Name;
+import javax.naming.RefAddr;
+import javax.naming.Reference;
+import javax.naming.spi.ObjectFactory;
+
+import com.mongodb.MongoClient;
+import com.mongodb.MongoClientURI;
+import com.mongodb.MongoException;
+
+/**
+ * 
+ * Mongo factory bean to help configuring Mongodb using Jndi.
+ * 
+ * @author jmelo
+ *
+ */
+public class MongoJndiObjectFactoryBean implements ObjectFactory {
+
+	private static final String MONGO_CLIENT_URI = "mongoClientURI";
+
+
+	public Object getObjectInstance(Object obj, Name name, Context nameCtx, Hashtable<?, ?> environment) throws Exception {
+
+		String mongoURI = null;
+		Enumeration<RefAddr> props = ((Reference) obj).getAll();
+		
+		while (props.hasMoreElements()) {
+			RefAddr addr = (RefAddr) props.nextElement();
+			if (addr != null) {
+				if (MONGO_CLIENT_URI.equals(addr.getType())) {
+					mongoURI = (String) addr.getContent();
+				}
+			}
+		}
+		
+		if (mongoURI == null || mongoURI.isEmpty()) {
+			throw new MongoException(MONGO_CLIENT_URI + " resource property is empty");
+		}
+		
+		return new MongoClient(new MongoClientURI(mongoURI));
+	}
+
+}
