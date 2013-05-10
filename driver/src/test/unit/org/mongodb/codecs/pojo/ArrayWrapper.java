d1998d417cbc5377f69e009114e617dafe72d845
Trisha Gee <trisha.gee@10gen.com>
2013-05-10 17:02:51 +0100

    Suppressing FindBugs errors that not bugs in this case.

--- driver/src/test/unit/org/mongodb/codecs/pojo/ArrayWrapper.java
+++ driver/src/test/unit/org/mongodb/codecs/pojo/ArrayWrapper.java
@@ -1,12 +1,16 @@
 package org.mongodb.codecs.pojo;
 
 public class ArrayWrapper {
+    //set via reflection in encoder/decoder tests
+    @SuppressWarnings({"FieldCanBeLocal", "UnusedDeclaration"})
     private int[] ints;
 
     public ArrayWrapper(final int[] ints) {
         this.ints = ints;
     }
 
+    //required for decoding
+    @SuppressWarnings("UnusedDeclaration")
     public ArrayWrapper() {
     }
 }
