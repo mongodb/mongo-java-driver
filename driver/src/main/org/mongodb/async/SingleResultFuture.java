56a57ca07689bb6a8b7c6c0ccb586ad6c003d90d
Jeff Yemin <jeff.yemin@10gen.com>
2013-05-15 15:27:53 -0400

    No longer wrapping MongoException in ExecutionException.

--- driver/src/main/org/mongodb/async/SingleResultFuture.java
+++ driver/src/main/org/mongodb/async/SingleResultFuture.java
@@ -4,7 +4,7 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *   http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
@@ -12,7 +12,6 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *
  */
 
 package org.mongodb.async;
@@ -117,7 +116,7 @@ public class SingleResultFuture<T> implements MongoFuture<T> {
         }
 
         if (exception != null) {
-            throw new ExecutionException(exception);
+            throw exception;
         }
         return result;
     }
--- driver/src/test/unit/org/mongodb/impl/SingleResultFutureTest.java
+++ driver/src/test/unit/org/mongodb/impl/SingleResultFutureTest.java
@@ -4,7 +4,7 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *   http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
@@ -12,7 +12,6 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *
  */
 
 package org.mongodb.impl;
@@ -77,9 +76,9 @@ public class SingleResultFutureTest {
         try {
             future.get();
             fail();
-        } catch (ExecutionException e) {
+        } catch (MongoException e) {
             assertTrue(future.isDone());
-            assertEquals(mongoException, e.getCause());
+            assertEquals(mongoException, e);
         }
     }
 
