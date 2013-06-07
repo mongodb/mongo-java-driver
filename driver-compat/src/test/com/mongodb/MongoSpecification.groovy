fcc500d70b5b1f7b30cfabbe28d10aae4ffaf5f9
Trisha Gee <trisha.gee@10gen.com>
2013-06-07 10:03:39 +0100

    Converted the two tests that use JMock in driver-compat to Spock tests.

---
+++ driver-compat/src/test/com/mongodb/MongoSpecification.groovy
@@ -0,0 +1,65 @@
+/*
+ * Copyright (c) 2008 - 2013 10gen, Inc. <http://10gen.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.mongodb
+
+import org.mongodb.connection.Cluster
+import org.mongodb.connection.ClusterConnectionMode
+import org.mongodb.connection.ClusterDescription
+import org.mongodb.connection.ClusterType
+import spock.lang.Specification
+
+class MongoSpecification extends Specification {
+    private ClusterDescription clusterDescription = Mock();
+    private Cluster cluster = Mock();
+
+    private Mongo mongo= new Mongo(cluster, MongoClientOptions.builder().build());
+
+     public void 'should return replica set status if cluster type replica set and mode discovering'() {
+        //TODO: this is not really the correct way to go about this
+        //we should have a test builder for the cluster if we have to mock the behaviour to get it to do what we want
+        //currently we're getting null pointers for description
+        setup:
+        cluster.getDescription() >> clusterDescription
+        clusterDescription.getType() >> ClusterType.ReplicaSet
+        clusterDescription.getMode() >> ClusterConnectionMode.Discovering
+
+        expect:
+        mongo.getReplicaSetStatus() != null;
+    }
+
+
+     public void 'should return null if cluster type not replica'() {
+        setup:
+        cluster.getDescription() >> clusterDescription
+        clusterDescription.getType() >> ClusterType.Sharded
+        clusterDescription.getMode() >> ClusterConnectionMode.Discovering
+
+        expect:
+        mongo.getReplicaSetStatus() == null;
+    }
+
+     public void 'should return null if cluster mode not discovering'() {
+        setup:
+        cluster.getDescription() >> clusterDescription
+        clusterDescription.getType() >> ClusterType.ReplicaSet
+        clusterDescription.getMode() >> ClusterConnectionMode.Direct
+
+        expect:
+        mongo.getReplicaSetStatus() == null;
+    }
+
+}
