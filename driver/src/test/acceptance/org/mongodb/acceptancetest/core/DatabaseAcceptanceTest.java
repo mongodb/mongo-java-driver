f585620e513f3c9593a316571ddf69a8859367e0
Uladzimir Mihura <trnl.me@gmail.com>
2013-08-21 13:08:31 +0300

    fix(driver) provide settings with ssl=true for new connection

--- driver/src/test/acceptance/org/mongodb/acceptancetest/core/DatabaseAcceptanceTest.java
+++ driver/src/test/acceptance/org/mongodb/acceptancetest/core/DatabaseAcceptanceTest.java
@@ -210,7 +210,7 @@ public class DatabaseAcceptanceTest extends DatabaseTestCase {
             database.tools().addUser(credential.getUserName(), credential.getPassword(), true);
             database.tools().removeUser(credential.getUserName());
             try {
-                MongoClient client = MongoClients.create(getPrimary(), Arrays.asList(credential), MongoClientOptions.builder().build());
+                MongoClient client = MongoClients.create(getPrimary(), Arrays.asList(credential), getOptions());
                 client.getDatabase("test").getCollection("test").find().getOne();
             } catch (MongoSecurityException e) {
                 // all good.  using this style to make sure that it's not the addUser call that is throwing.  of course, could move
