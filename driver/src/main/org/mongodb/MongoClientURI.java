bdba95b2a4411fd845ab7778ed0541228e1f9a07
Jeff Yemin <jeff.yemin@10gen.com>
2013-09-30 12:48:06 -0400

    Updated MongoClientURI docs to include auth mechanism for X.509.

--- driver/src/main/org/mongodb/MongoClientURI.java
+++ driver/src/main/org/mongodb/MongoClientURI.java
@@ -138,9 +138,9 @@ import static org.mongodb.AuthenticationMechanism.PLAIN;
  * </ul>
  * <p>Authentication configuration:</p>
  * <ul>
- * <li>{@code authMechanism=MONGO-CR|GSSAPI|PLAIN}: The authentication mechanism to use if a credential was supplied.
- * The default is MONGODB-CR, which is the native MongoDB Challenge Response mechanism.  For the GSSAPI mechanism, no password is accepted,
- * only the username.
+ * <li>{@code authMechanism=MONGO-CR|GSSAPI|PLAIN|MONGODB-X509}: The authentication mechanism to use if a credential was supplied.
+ * The default is MONGODB-CR, which is the native MongoDB Challenge Response mechanism.  For the GSSAPI and MONGODB-X509 mechanisms,
+ * no password is accepted, only the username.
  * </li>
  * <li>{@code authSource=string}: The source of the authentication credentials.  This is typically the database that
  * the credentials have been created.  The value defaults to the database specified in the path portion of the URI.
