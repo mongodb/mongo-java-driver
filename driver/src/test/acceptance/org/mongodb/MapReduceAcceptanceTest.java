3f35c675aaa9aed114a308652ea69c1ad6dc5762
Trisha Gee <trisha.gee@10gen.com>
2013-10-11 18:13:53 +0200

    Turned the single MapReduce test into several acceptance tests.  Needs more coverage but this is a start.

---
+++ driver/src/test/acceptance/org/mongodb/MapReduceAcceptanceTest.java
@@ -0,0 +1,199 @@
+/*
+ * Copyright (c) 2008 - 2013 10gen, Inc. <http://10gen.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.mongodb;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import static org.hamcrest.CoreMatchers.hasItem;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.Matchers.contains;
+import static org.junit.Assert.assertThat;
+import static org.mongodb.Sort.ascending;
+
+public class MapReduceAcceptanceTest extends DatabaseTestCase {
+    public void setUp() {
+        super.setUp();
+        collection.save(new Document("_id", 0).append("labels", new String[]{"a", "b"}));
+        collection.save(new Document("_id", 1).append("labels", new String[]{"a", "b"}));
+
+        collection.save(new Document("_id", 2).append("labels", new String[]{"b", "c"}));
+        collection.save(new Document("_id", 3).append("labels", new String[]{"c", "d"}));
+        collection.save(new Document("_id", 4).append("labels", new String[]{"a", "b"}));
+        collection.save(new Document("_id", 5).append("labels", new String[]{"b", "c"}));
+        collection.save(new Document("_id", 6).append("labels", new String[]{"b", "c"}));
+        collection.save(new Document("_id", 7).append("labels", new String[]{"a", "b"}));
+
+        collection.save(new Document("_id", 8).append("labels", new String[]{"b", "c"}));
+        collection.save(new Document("_id", 9).append("labels", new String[]{"c", "d"}));
+    }
+
+    @Test
+    public void shouldReturnCountOfAllArrayValuesUsingSimpleMapReduce() {
+        // perform Map Reduce on all data
+        MongoIterable<Document> results = collection.find()
+                                                    .mapReduce("  function(){ "
+                                                               + "  for ( var i=0; i < this.labels.length; i++ ){ "
+                                                               + "    emit( this.labels[i] , 1 ); "
+                                                               + "  }"
+                                                               + "}",
+
+                                                               "  function(key,values){ "
+                                                               + "  var sum=0; "
+                                                               + "  for( var i=0; i < values.length; i++ ) "
+                                                               + "    sum += values[i]; "
+                                                               + "  return sum;"
+                                                               + "}");
+
+        List<Document> resultList = results.into(new ArrayList<Document>());
+        assertThat("There are four distinct labels, a b c d", resultList.size(), is(4));
+
+        assertThat("There are four 'a's in the data", resultList, hasItem(new Document("_id", "a").append("value", 4.0)));
+        assertThat("There are eight 'b's in the data", resultList, hasItem(new Document("_id", "b").append("value", 8.0)));
+        assertThat("There are six 'c's in the data", resultList, hasItem(new Document("_id", "c").append("value", 6.0)));
+        assertThat("There are two 'd's in the data", resultList, hasItem(new Document("_id", "d").append("value", 2.0)));
+    }
+
+    @Test
+    public void shouldPerformMapReduceOnALimitedSetOfData() {
+        MongoIterable<Document> results = collection.find(new Document("_id", new Document("$gt", 0))) //find all IDs greater than zero
+                                              .sort(ascending("_id"))   // sort by ID
+                                              .skip(1)                  // skip the first in the results
+                                              .limit(6)                 // limit to 6 of the remaining results
+                                              .mapReduce("  function(){ "
+                                                         + "  for ( var i=0; i < this.labels.length; i++ ){ "
+                                                         + "    emit( this.labels[i] , 1 ); "
+                                                         + "  }"
+                                                         + "}",
+
+                                                         "  function(key,values){ "
+                                                         + "  var sum=0; "
+                                                         + "  for( var i=0; i < values.length; i++ ) "
+                                                         + "    sum += values[i]; "
+                                                         + "  return sum;"
+                                                         + "}");
+        // will perform Map Reduce on _ids 2-7
+
+        List<Document> resultList = results.into(new ArrayList<Document>());
+        assertThat("There are four distinct labels, a b c d", resultList.size(), is(4));
+
+        assertThat("There are two 'a's in the data", resultList, hasItem(new Document("_id", "a").append("value", 2.0)));
+        assertThat("There are five 'b's in the data", resultList, hasItem(new Document("_id", "b").append("value", 5.0)));
+        assertThat("There are four 'c's in the data", resultList, hasItem(new Document("_id", "c").append("value", 4.0)));
+        assertThat("There is one 'd's in the data", resultList, hasItem(new Document("_id", "d").append("value", 1.0)));
+    }
+
+    @Test
+    public void shouldMapIterableOfDocumentsIntoAnObjectOfYourChoice() {
+        MongoIterable<Document> results = collection.find(new Document("_id", new Document("$gt", 0))) //find all IDs greater than zero
+                                              .sort(ascending("_id"))   // sort by ID
+                                              .skip(1)                  // skip the first in the results
+                                              .limit(6)                 // limit to 6 of the remaining results
+                                              .mapReduce("  function(){ "
+                                                         + "  for ( var i=0; i < this.labels.length; i++ ){ "
+                                                         + "    emit( this.labels[i] , 1 ); "
+                                                         + "  }"
+                                                         + "}",
+
+                                                         "  function(key,values){ "
+                                                         + "  var sum=0; "
+                                                         + "  for( var i=0; i < values.length; i++ ) "
+                                                         + "    sum += values[i]; "
+                                                         + "  return sum;"
+                                                         + "}");
+        // will perform Map Reduce on _ids 2-7
+
+        //transforms Documents into LabelCount objects
+        List<LabelCount> labelCounts = results.map(new Function<Document, LabelCount>() {
+            @Override
+            public LabelCount apply(final Document document) {
+                return new LabelCount(document.getString("_id"), document.getDouble("value"));
+            }
+        })
+                                              .into(new ArrayList<LabelCount>());
+        assertThat("Transformed results should still be the same size as original results", labelCounts.size(), is(4));
+
+        Collections.sort(labelCounts);
+        assertThat("Should be LabelCount ordered by count ascending", labelCounts, contains(new LabelCount("d", 1),
+                                                                                            new LabelCount("a", 2),
+                                                                                            new LabelCount("c", 4),
+                                                                                            new LabelCount("b", 5)));
+    }
+
+    static class LabelCount implements Comparable<LabelCount> {
+        private final String tag;
+        private final double count;
+
+        LabelCount(final String tag, final double count) {
+            this.tag = tag;
+            this.count = count;
+        }
+
+        @SuppressWarnings("RedundantIfStatement")
+        @Override
+        public boolean equals(final Object o) {
+            if (this == o) {
+                return true;
+            }
+            if (o == null || getClass() != o.getClass()) {
+                return false;
+            }
+
+            LabelCount labelCount = (LabelCount) o;
+
+            if (Double.compare(labelCount.count, count) != 0) {
+                return false;
+            }
+            if (!tag.equals(labelCount.tag)) {
+                return false;
+            }
+
+            return true;
+        }
+
+        @Override
+        public int hashCode() {
+            int result;
+            long temp;
+            result = tag.hashCode();
+            temp = Double.doubleToLongBits(count);
+            result = 31 * result + (int) (temp ^ (temp >>> 32));
+            return result;
+        }
+
+        // So Collections.sort works.  Sorts by count.
+        @Override
+        public int compareTo(final LabelCount o) {
+            int cmp = Double.compare(count, o.count);
+            if (cmp != 0) {
+                return cmp;
+            }
+            return tag.compareTo(o.tag);
+        }
+
+        @Override
+        public String toString() {
+            return "TagCount{"
+                   + "tag='" + tag + '\''
+                   + ", count=" + count
+                   + '}';
+        }
+    }
+}
