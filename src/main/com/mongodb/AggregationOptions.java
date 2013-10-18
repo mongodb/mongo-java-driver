4e4e9cbcaffe4759bffe5ec117ba693b327ec919
Jeff Yemin <jeff.yemin@10gen.com>
2013-10-18 17:03:10 -0400

    Removed extraneous public members of AggregationOptions

--- src/main/com/mongodb/AggregationOptions.java
+++ src/main/com/mongodb/AggregationOptions.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright (c) 2008 - 2013 MongoDB Inc., Inc. <http://mongodb.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.mongodb;
 
 
@@ -21,9 +37,9 @@ public class AggregationOptions {
     }
 
     AggregationOptions(final Builder builder) {
-        this.batchSize = builder.getBatchSize();
-        this.allowDiskUsage = builder.getAllowDiskUsage();
-        this.outputMode = builder.getOutputMode();
+        batchSize = builder.batchSize;
+        allowDiskUsage = builder.allowDiskUsage;
+        outputMode = builder.outputMode;
     }
 
     /**
@@ -75,7 +91,7 @@ public class AggregationOptions {
         private Boolean allowDiskUsage;
         private OutputMode outputMode = OutputMode.INLINE;
 
-        protected Builder() {
+        private Builder() {
         }
 
         public Builder batchSize(final Integer size) {
@@ -91,18 +107,6 @@ public class AggregationOptions {
         public Builder outputMode(final OutputMode mode) {
             outputMode = mode;
             return this;
-        }
-
-        public Boolean getAllowDiskUsage() {
-            return allowDiskUsage;
-        }
-
-        public Integer getBatchSize() {
-            return batchSize;
-        }
-
-        public OutputMode getOutputMode() {
-            return outputMode;
         }
 
         public AggregationOptions build() {
