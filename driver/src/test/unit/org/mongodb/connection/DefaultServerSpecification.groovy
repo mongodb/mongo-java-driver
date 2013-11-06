9beac1bfb4eaa52a3453906f5d5082d162a0daa7
Jeff Yemin <jeff.yemin@10gen.com>
2013-11-06 15:56:54 -0500

    DefaultServer.invalidate needs to invoke the change listeners.

---
+++ driver/src/test/unit/org/mongodb/connection/DefaultServerSpecification.groovy
@@ -0,0 +1,69 @@
+/*
+ * Copyright (c) 2008 - 2013 10gen, Inc. <http://10gen.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+
+
+
+package org.mongodb.connection
+
+import spock.lang.Specification
+
+import java.util.concurrent.Executors
+import java.util.concurrent.ScheduledExecutorService
+
+import static org.mongodb.Fixture.getBufferProvider
+
+class DefaultServerSpecification extends Specification {
+
+    static ScheduledExecutorService scheduledExecutorService
+    DefaultServer server;
+
+    def setupSpec() {
+        scheduledExecutorService = Executors.newScheduledThreadPool(1)
+    }
+
+    def cleanupSpec() {
+        scheduledExecutorService.shutdown()
+    }
+
+    def setup() {
+        server = new DefaultServer(new ServerAddress(), ServerSettings.builder().build(), new TestConnectionProvider(),
+                                   new TestInternalConnectionFactory(), scheduledExecutorService, getBufferProvider())
+    }
+
+    def cleanup() {
+        server.close()
+    }
+
+    def 'invalidate should invoke change listeners'() {
+        given:
+        def stateChanged = false;
+
+        server.addChangeListener(new ChangeListener<ServerDescription>() {
+            @Override
+            void stateChanged(final ChangeEvent<ServerDescription> event) {
+                stateChanged = true;
+            }
+        })
+
+        when:
+        server.invalidate();
+
+        then:
+        stateChanged
+    }
+}
---
+++ driver/src/test/unit/org/mongodb/connection/TestConnectionProvider.java
@@ -0,0 +1,80 @@
+/*
+ * Copyright (c) 2008 - 2013 10gen, Inc. <http://10gen.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.mongodb.connection;
+
+import org.bson.ByteBuf;
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+public class TestConnectionProvider implements ConnectionProvider {
+
+    @Override
+    public Connection get() {
+        return new Connection() {
+            @Override
+            public void sendMessage(final List<ByteBuf> byteBuffers, final int lastRequestId) {
+                throw new UnsupportedOperationException("Not implemented yet!");
+            }
+
+            @Override
+            public ResponseBuffers receiveMessage(final int responseTo) {
+                throw new UnsupportedOperationException("Not implemented yet!");
+            }
+
+            @Override
+            public void sendMessageAsync(final List<ByteBuf> byteBuffers, final int lastRequestId,
+                                         final SingleResultCallback<Void> callback) {
+                throw new UnsupportedOperationException("Not implemented yet!");
+            }
+
+            @Override
+            public void receiveMessageAsync(final int responseTo, final SingleResultCallback<ResponseBuffers> callback) {
+                throw new UnsupportedOperationException("Not implemented yet!");
+            }
+
+            @Override
+            public ServerAddress getServerAddress() {
+                throw new UnsupportedOperationException("Not implemented yet!");
+            }
+
+            @Override
+            public String getId() {
+                throw new UnsupportedOperationException("Not implemented yet!");
+            }
+
+            @Override
+            public void close() {
+                throw new UnsupportedOperationException("Not implemented yet!");
+            }
+
+            @Override
+            public boolean isClosed() {
+                throw new UnsupportedOperationException("Not implemented yet!");
+            }
+        };
+    }
+
+    @Override
+    public Connection get(final long timeout, final TimeUnit timeUnit) {
+        return get();
+    }
+
+    @Override
+    public void close() {
+    }
+}
