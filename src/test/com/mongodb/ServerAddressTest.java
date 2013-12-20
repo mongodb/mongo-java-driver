f906d7e6603b50912ae3f5ba79a690ec5fee53f6
Craig Wilson <craiggwilson@gmail.com>
2013-12-20 10:12:37 -0600

    added missing tests for existing functionality.

---
+++ src/test/com/mongodb/ServerAddressTest.java
@@ -0,0 +1,106 @@
+/*
+ * Copyright (c) 2008 - 2013 MongoDB Inc., Inc. <http://mongodb.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.mongodb;
+
+
+import junit.framework.Assert;
+import org.junit.Test;
+
+import java.net.UnknownHostException;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashSet;
+
+import static com.mongodb.ServerConnectionState.Connected;
+import static com.mongodb.ServerConnectionState.Connecting;
+import static com.mongodb.ServerDescription.MAX_DRIVER_WIRE_VERSION;
+import static com.mongodb.ServerDescription.MIN_DRIVER_WIRE_VERSION;
+import static com.mongodb.ServerType.ReplicaSetPrimary;
+import static com.mongodb.ServerType.Unknown;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+
+public class ServerAddressTest {
+
+    @Test
+         public void testDefault() throws UnknownHostException {
+        ServerAddress subject = new ServerAddress();
+
+        assertEquals(ServerAddress.defaultHost(), subject.getHost());
+        assertEquals(ServerAddress.defaultPort(), subject.getPort());
+    }
+
+    @Test
+    public void testParseIPV4WithoutPort() throws UnknownHostException {
+        ServerAddress subject = new ServerAddress("10.0.0.1");
+
+        assertEquals("10.0.0.1", subject.getHost());
+        assertEquals(ServerAddress.defaultPort(), subject.getPort());
+    }
+
+    @Test
+    public void testParseIPV4WithPort() throws UnknownHostException {
+        ServerAddress subject = new ServerAddress("10.0.0.1:1000");
+
+        assertEquals("10.0.0.1", subject.getHost());
+        assertEquals(1000, subject.getPort());
+    }
+
+    @Test
+         public void testIPV4WithPort() throws UnknownHostException {
+        ServerAddress subject = new ServerAddress("10.0.0.1", 1000);
+
+        assertEquals("10.0.0.1", subject.getHost());
+        assertEquals(1000, subject.getPort());
+    }
+
+    @Test
+    public void testParseDnsNameWithoutPort() throws UnknownHostException {
+        ServerAddress subject = new ServerAddress("somewhere");
+
+        assertEquals("somewhere", subject.getHost());
+        assertEquals(27017, subject.getPort());
+    }
+
+    @Test
+    public void testDnsNameWithPort() throws UnknownHostException {
+        ServerAddress subject = new ServerAddress("somewhere", 1000);
+
+        assertEquals("somewhere", subject.getHost());
+        assertEquals(1000, subject.getPort());
+    }
+
+    @Test
+    public void testParseWithPortWhenDefaultPortIsAlsoSpecified() throws UnknownHostException {
+        ServerAddress subject = new ServerAddress("somewhere:80", 27017);
+
+        assertEquals("somewhere", subject.getHost());
+        assertEquals(80, subject.getPort());
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testParseWithPortWhenEquivalentPortIsAlsoSpecified() throws UnknownHostException {
+        new ServerAddress("somewhere:80", 80);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testParseWithPortWhenNonEquivalentPortIsAlsoSpecified() throws UnknownHostException {
+        new ServerAddress("somewhere:80", 1000);
+    }
+}
