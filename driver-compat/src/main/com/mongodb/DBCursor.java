485ae54585f5c3a8cd65f11459051f5be29160bd
Justin Lee <justin.lee@10gen.com>
2014-01-07 12:48:04 -0500

    documentation updates to reflect upstream fixes

--- driver-compat/src/main/com/mongodb/DBCursor.java
+++ driver-compat/src/main/com/mongodb/DBCursor.java
@@ -254,7 +254,7 @@ public class DBCursor implements Iterator<DBObject>, Iterable<DBObject>, Closeab
     }
 
     /**
-     * Specifies a minimum exclusive upper limit for the index to use in a query.
+     * Specifies an <em>exclusive</em> upper limit for the index to use in a query.
      *
      * @mongodb.driver.manual reference/operator/meta/max/ $max
      * @since 2.12
@@ -266,7 +266,7 @@ public class DBCursor implements Iterator<DBObject>, Iterable<DBObject>, Closeab
     }
     
     /**
-     * Specifies a minimum inclusive lower limit for the index to use in a query. 
+     * Specifies an <em>inclusive</em> lower limit for the index to use in a query. 
      *
      * @mongodb.driver.manual reference/operator/meta/min/ $min
      * @since 2.12
