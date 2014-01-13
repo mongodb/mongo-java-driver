ec6142f0ce8357b801f287fc93a3a328bc2c002b
Trisha Gee <trisha.gee@10gen.com>
2014-01-13 18:09:08 +0100

    Fixed copyright on CopyOnWriteMap

--- driver-compat/src/main/org/bson/util/CopyOnWriteMap.java
+++ driver-compat/src/main/org/bson/util/CopyOnWriteMap.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2008 MongoDB, Inc.
+ * Copyright 2008 Atlassian Pty Ltd
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
