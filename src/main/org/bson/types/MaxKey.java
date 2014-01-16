37b3771e4837245a33bc22cefa3fd848445a7f25
Trisha Gee <trishagee@github.com>
2014-01-16 13:33:53 +0100

    Second lot of copyright changes - added the "to" year.

--- src/main/org/bson/types/MaxKey.java
+++ src/main/org/bson/types/MaxKey.java
@@ -1,6 +1,6 @@
 
 /*
- * Copyright (c) 2008 MongoDB, Inc.
+ * Copyright (c) 2008-2014 MongoDB, Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
