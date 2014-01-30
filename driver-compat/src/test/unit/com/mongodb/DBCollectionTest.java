406c3c55f5e64cbea0c5ffd307a907493e374f19
Justin Lee <justin.lee@10gen.com>
2014-01-30 16:31:38 -0500

    unignore test

--- driver-compat/src/test/unit/com/mongodb/DBCollectionTest.java
+++ driver-compat/src/test/unit/com/mongodb/DBCollectionTest.java
@@ -29,7 +29,6 @@ import org.bson.types.CodeWScope;
 import org.bson.types.MaxKey;
 import org.bson.types.MinKey;
 import org.bson.types.ObjectId;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.mongodb.Document;
 
@@ -220,8 +219,6 @@ public class DBCollectionTest extends DatabaseTestCase {
     }
 
     @Test
-    @Ignore("https://trello.com/c/Naqe9XX2/474-unignore-com-mongodb-dbcollectiontest-testcreateindexastext-when"
-            + "-text-search-is-enable-by-default")
     public void testCreateIndexAsText() {
         assumeTrue(serverVersionAtLeast(asList(2, 4, 0)));
         DBObject index = new BasicDBObject("x", "text");
