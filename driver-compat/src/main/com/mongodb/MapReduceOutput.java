71658618c420bc43141cfcaa49cdd4cf9cd3f32d
Jeff Yemin <jeff.yemin@10gen.com>
2014-02-04 13:51:26 -0500

    Removed deprecated method MapReduceOutput.getCommandResult()

--- driver-compat/src/main/com/mongodb/MapReduceOutput.java
+++ driver-compat/src/main/com/mongodb/MapReduceOutput.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2008-2014 MongoDB, Inc.
+ * Copyright (c) 2008 - 2014 MongoDB, Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -185,10 +185,5 @@ public class MapReduceOutput {
      */
     public int getEmitCount() {
         return mapReduceStatistics.getEmitCount();
-    }
-
-    @Deprecated
-    public DBObject getCommandResult() {
-        throw new MongoException("This method is gone.  Remove this implementation after 2.12 ships.");
     }
 }
