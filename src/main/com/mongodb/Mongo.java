f587533ecdcbbae700c8abea95b29b7f13ec12e0
Jeff Yemin <jeff.yemin@10gen.com>
2014-02-15 11:10:05 -0500

    Bumping version to 2.12.0-rc1

--- src/main/com/mongodb/Mongo.java
+++ src/main/com/mongodb/Mongo.java
@@ -91,7 +91,7 @@ public class Mongo {
     @Deprecated
     public static final int MINOR_VERSION = 12;
 
-    private static final String FULL_VERSION = "2.12.0-rc0";
+    private static final String FULL_VERSION = "2.12.0-rc1";
 
     static int cleanerIntervalMS;
 
