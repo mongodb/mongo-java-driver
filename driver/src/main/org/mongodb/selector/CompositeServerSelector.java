7f9930430cf96f1d9fd2bde9c8b9c783138b73d9
Jeff Yemin <jeff.yemin@10gen.com>
2014-04-09 16:54:23 -0400

    Adding CompositeServerSelector

---
+++ driver/src/main/org/mongodb/selector/CompositeServerSelector.java
@@ -0,0 +1,80 @@
+/*
+ * Copyright (c) 2008-2014 MongoDB, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.mongodb.selector;
+
+import org.mongodb.connection.ClusterDescription;
+import org.mongodb.connection.ServerDescription;
+import org.mongodb.connection.ServerSelector;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.mongodb.assertions.Assertions.notNull;
+
+/**
+ * A server selector that composes a list of server selectors, and selects the servers by iterating through the list from start to
+ * finish, passing the result of the previous into the next, and finally returning the result of the last one.
+ *
+ * @since 3.0
+ */
+public final class CompositeServerSelector implements ServerSelector {
+    private final List<ServerSelector> serverSelectors;
+
+    /**
+     * Constructs a new instance.
+     *
+     * @param serverSelectors the list of composed server selectors
+     */
+    public CompositeServerSelector(final List<ServerSelector> serverSelectors) {
+        notNull("serverSelectores", serverSelectors);
+        if (serverSelectors.isEmpty()) {
+            throw new IllegalArgumentException("Server selectors can not be an empty list");
+        }
+        for (ServerSelector cur : serverSelectors) {
+            if (cur == null) {
+                throw new IllegalArgumentException("Can not have a null server selector in the list of composed selectors");
+            }
+        }
+        this.serverSelectors = new ArrayList<ServerSelector>();
+        for (ServerSelector cur : serverSelectors) {
+            if (cur instanceof CompositeServerSelector) {
+                this.serverSelectors.addAll(((CompositeServerSelector) cur).serverSelectors);
+            } else {
+                this.serverSelectors.add(cur);
+            }
+        }
+    }
+
+    @Override
+    public List<ServerDescription> choose(final ClusterDescription clusterDescription) {
+        ClusterDescription curClusterDescription = clusterDescription;
+        List<ServerDescription> choices = null;
+        for (ServerSelector cur : serverSelectors) {
+            choices = cur.choose(curClusterDescription);
+            curClusterDescription = new ClusterDescription(clusterDescription.getConnectionMode(), clusterDescription.getType(), choices);
+        }
+
+        return choices;
+    }
+
+    @Override
+    public String toString() {
+        return "{"
+               + "serverSelectors=" + serverSelectors
+               + '}';
+    }
+}
---
+++ driver/src/test/unit/org/mongodb/selector/CompositeServerSelectorTest.java
@@ -0,0 +1,90 @@
+/*
+ * Copyright (c) 2008-2014 MongoDB, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.mongodb.selector;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mongodb.connection.ClusterDescription;
+import org.mongodb.connection.ServerAddress;
+import org.mongodb.connection.ServerDescription;
+import org.mongodb.connection.ServerSelector;
+import org.mongodb.operation.ReadPreferenceServerSelector;
+import org.mongodb.session.LatencyMinimizingServerSelector;
+
+import java.net.UnknownHostException;
+import java.util.Arrays;
+
+import static java.util.Arrays.asList;
+import static java.util.concurrent.TimeUnit.MILLISECONDS;
+import static org.junit.Assert.assertEquals;
+import static org.mongodb.ReadPreference.secondary;
+import static org.mongodb.connection.ClusterConnectionMode.MULTIPLE;
+import static org.mongodb.connection.ClusterType.REPLICA_SET;
+import static org.mongodb.connection.ServerConnectionState.CONNECTED;
+import static org.mongodb.connection.ServerType.REPLICA_SET_PRIMARY;
+import static org.mongodb.connection.ServerType.REPLICA_SET_SECONDARY;
+
+public class CompositeServerSelectorTest {
+    private CompositeServerSelector selector;
+    private ServerDescription second;
+    private ServerDescription first;
+    private ServerDescription third;
+
+    @Before
+    public void setUp() throws UnknownHostException {
+        first = ServerDescription.builder()
+                                 .state(CONNECTED)
+                                 .address(new ServerAddress())
+                                 .ok(true)
+                                 .averagePingTime(5, MILLISECONDS)
+                                 .type(REPLICA_SET_PRIMARY)
+                                 .build();
+
+        second = ServerDescription.builder()
+                                  .state(CONNECTED)
+                                  .address(new ServerAddress("localhost:27018"))
+                                  .ok(true)
+                                  .averagePingTime(30, MILLISECONDS)
+                                  .type(REPLICA_SET_SECONDARY)
+                                  .build();
+
+        third = ServerDescription.builder()
+                                 .state(CONNECTED)
+                                 .address(new ServerAddress("localhost:27019"))
+                                 .ok(true)
+                                 .averagePingTime(35, MILLISECONDS)
+                                 .type(REPLICA_SET_SECONDARY)
+                                 .build();
+    }
+
+    @Test
+    public void shouldApplyServerSelectorsInOrder() {
+        selector = new CompositeServerSelector(asList(new ReadPreferenceServerSelector(secondary()),
+                                                      new LatencyMinimizingServerSelector(15, MILLISECONDS)));
+        assertEquals(selector.choose(new ClusterDescription(MULTIPLE, REPLICA_SET, asList(first, second, third))), asList(second, third));
+    }
+
+    @Test
+    public void shouldCollapseNestedComposite() {
+        CompositeServerSelector composedSelector =
+        new CompositeServerSelector(asList(new ReadPreferenceServerSelector(secondary()),
+                                           new LatencyMinimizingServerSelector(15, MILLISECONDS)));
+        selector = new CompositeServerSelector(Arrays.<ServerSelector>asList(composedSelector));
+        assertEquals(selector.choose(new ClusterDescription(MULTIPLE, REPLICA_SET, asList(first, second, third))), asList(second, third));
+
+    }
+}
