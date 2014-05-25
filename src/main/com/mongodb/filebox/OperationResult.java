accada21fbbcc8c39c76f5782c8a5789311a11da
Ramazan Polat <ramazanpolat@gmail.comSystemA@SystemPC>
2014-05-26 02:02:45 +0300

    Add Filebox class

--- src/main/com/mongodb/filebox/OperationResult.java
+++ src/main/com/mongodb/filebox/OperationResult.java
@@ -1,5 +1,29 @@
 package com.mongodb.filebox;
 
-public class OperationResult {
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
 
+import com.mongodb.gridfs.GridFSDBFile;
+
+public class OperationResult {
+	public boolean Ok;
+    public String filename;
+    public Object _id;
+    public Date UploadDate;
+    public long FileSize;
+    public String MD5;
+    public boolean hasResult;
+    public Map metaData;
+    public String ErrorMessage;
+    public boolean hasError;
+    public GridFSDBFile GridfsFile;
+    public List<GridFSDBFile> GridfsFileList;
+    //public Set Tags;
+    //public String Path;
+    
+    public OperationResult()
+    {
+        
+    }
 }
---
+++ src/test/com/mongodb/filebox/FileboxTest.java
@@ -0,0 +1,11 @@
+package com.mongodb.filebox;
+
+import com.mongodb.gridfs.GridFS;
+import com.mongodb.util.TestCase;
+
+public class FileboxTest extends TestCase{
+	public FileboxTest() { 
+		
+	}
+	
+}
