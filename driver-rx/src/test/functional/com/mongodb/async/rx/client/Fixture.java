ffa1ef63f682b52ee72e0eb9057d75bce0888f80
Ross Lawley <ross.lawley@gmail.com>
2014-08-01 11:21:07 +0100

    Fixed rxJava deprecation warnings

--- driver-rx/src/test/functional/com/mongodb/async/rx/client/Fixture.java
+++ driver-rx/src/test/functional/com/mongodb/async/rx/client/Fixture.java
@@ -64,10 +64,10 @@ public final class Fixture {
     }
 
     public static <T> T get(final Observable<T> observable, final long waitTime, final TimeUnit timeUnit) {
-        return observable.timeout(waitTime, timeUnit).toBlockingObservable().last();
+        return observable.timeout(waitTime, timeUnit).toBlocking().last();
     }
 
     public static <T> List<T> getAsList(final Observable<T> observable) {
-        return observable.timeout(5, SECONDS).toList().toBlockingObservable().last();
+        return observable.timeout(5, SECONDS).toList().toBlocking().last();
     }
 }
