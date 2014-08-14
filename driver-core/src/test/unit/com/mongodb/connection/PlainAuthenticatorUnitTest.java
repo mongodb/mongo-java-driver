63ba340ab336be3d48e8378f45371a71f9559e5c
Craig Wilson <craiggwilson@gmail.com>
2014-08-14 11:14:56 -0500

    added unit tests for PlainAuthenticator.

---
+++ driver-core/src/test/unit/com/mongodb/connection/PlainAuthenticatorUnitTest.java
@@ -0,0 +1,63 @@
+/*
+ * Copyright (c) 2008-2014 MongoDB, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.mongodb.connection;
+
+import com.mongodb.MongoCredential;
+import com.mongodb.ServerAddress;
+import com.mongodb.protocol.message.RequestMessage;
+import org.bson.io.InputBuffer;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
+public class PlainAuthenticatorUnitTest {
+    private TestInternalConnection connection;
+    private MongoCredential credential;
+    private PlainAuthenticator subject;
+
+    @Before
+    public void before() {
+        connection = new TestInternalConnection("1", new ServerAddress("localhost", 27017));
+        credential = MongoCredential.createPlainCredential("user", "$external", "pencil".toCharArray());
+        subject = new PlainAuthenticator(this.credential, this.connection);
+    }
+
+    @Test
+    public void testSuccessfulAuthentication() {
+        int currentRequestId = RequestMessage.getCurrentGlobalId();
+        ResponseBuffers reply = MessageHelper.buildSuccessfulReply(
+                currentRequestId,
+                "{conversationId: 1, "
+                        + "done: true, "
+                        + "ok: 1}");
+
+        connection.enqueueReply(reply);
+
+        subject.authenticate();
+
+        List<InputBuffer> sent = connection.getSent();
+        String command = MessageHelper.decodeCommandAsJson(sent.get(0));
+        String expectedCommand = "{ \"saslStart\" : 1, "
+                + "\"mechanism\" : \"PLAIN\", "
+                + "\"payload\" : { \"$binary\" : \"dXNlcgB1c2VyAHBlbmNpbA==\", \"$type\" : \"0\" } }";
+
+        assertEquals(expectedCommand, command);
+    }
+}
