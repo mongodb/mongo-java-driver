f400e958a134305f7ffb00994998538998c3dc34
Trisha Gee <trishagee@github.com>
2014-09-04 14:05:34 +0200

    [JAVA-1391] Javadoc for com.mongodb.connection package (added missing Javadoc).

--- driver-core/src/main/com/mongodb/connection/ReplyHeader.java
+++ driver-core/src/main/com/mongodb/connection/ReplyHeader.java
@@ -21,6 +21,12 @@ import org.bson.io.InputBuffer;
 
 import static java.lang.String.format;
 
+/**
+ * Contains the details of a reply from a MongoDB server.
+ *
+ * @mongodb.driver.manual meta-driver/latest/legacy/mongodb-wire-protocol/#database-response-messages Database Response Messages
+ * @since 3.0
+ */
 public class ReplyHeader {
     /**
      * The length of the reply header in the MongoDB wire protocol.
@@ -39,6 +45,11 @@ public class ReplyHeader {
     private final int startingFrom;
     private final int numberReturned;
 
+    /**
+     * Creates a new {@code ReplyHeader}, populated with values from the {@code headerInputBuffer}.
+     *
+     * @param headerInputBuffer the {@code InputBuffer} containing the reply from the MongoDB server.
+     */
     public ReplyHeader(final InputBuffer headerInputBuffer) {
         messageLength = headerInputBuffer.readInt32();
         requestId = headerInputBuffer.readInt32();
@@ -46,45 +57,103 @@ public class ReplyHeader {
         int opCode = headerInputBuffer.readInt32();
         if (opCode != OP_REPLY_OP_CODE) {
             throw new MongoInternalException(format("The opCode (%d) in the response does not match the expected opCode (%d)",
-                                                    opCode, OP_REPLY_OP_CODE)); }
+                                                    opCode, OP_REPLY_OP_CODE));
+        }
         responseFlags = headerInputBuffer.readInt32();
         cursorId = headerInputBuffer.readInt64();
         startingFrom = headerInputBuffer.readInt32();
         numberReturned = headerInputBuffer.readInt32();
     }
 
+    /**
+     * Gets the total size of the message in bytes. This total includes the 4 bytes that holds the message length.
+     *
+     * @return the total message size, including all of the header
+     */
     public int getMessageLength() {
         return messageLength;
     }
 
+    /**
+     * This is a client or database-generated identifier that uniquely identifies this message. Along with the {@code responseTo} field in
+     * responses, clients can use this to associate query responses with the originating query.
+     *
+     * @return the identifier for this message
+     */
     public int getRequestId() {
         return requestId;
     }
 
+    /**
+     * Along with the requestID field in queries, clients can use this to associate query responses with the originating query.
+     *
+     * @return the request ID from the original request
+     */
     public int getResponseTo() {
         return responseTo;
     }
 
+    /**
+     * Gets additional information about the response:
+     * <ul>
+     *     <li>0 - <i>CursorNotFound</i>: Set when getMore is called but the cursor id is not valid at the server. Returned with zero 
+     *     results.</li>
+     *     <li>1 - <i>QueryFailure</i>: Set when query failed. Results consist of one document containing an “$err” field describing the 
+     *     failure.
+     *     <li>2 - <i>ShardConfigStale</i>: Drivers should ignore this. Only mongos will ever see this set, in which case, 
+     *     it needs to update config from the server.
+     *     <li>3 - <i>AwaitCapable</i>: Set when the server supports the AwaitData Query option. If it doesn't, 
+     *     a client should sleep a little between getMore's of a Tailable cursor. Mongod version 1.6 supports AwaitData and thus always 
+     *     sets AwaitCapable.
+     *     <li>4-31 - <i>Reserved</i>: Ignore
+     * </ul>
+     *
+     * @return bit vector - see details above
+     */
     public int getResponseFlags() {
         return responseFlags;
     }
 
+    /**
+     * Gets the cursor ID that this response is a part of. If there are no more documents to fetch from the server, the cursor ID will be 0.
+     * This cursor ID must be used in any messages used to get more data, and also must be closed by the client when no longer needed.
+     *
+     * @return cursor ID if the client needs to fetch more from the server
+     */
     public long getCursorId() {
         return cursorId;
     }
 
+    /**
+     * Returns the position in the cursor that is the start point of this reply.
+     *
+     * @return where in the cursor this reply is starting
+     */
     public int getStartingFrom() {
         return startingFrom;
     }
 
+    /**
+     * @return number of documents in the reply
+     */
     public int getNumberReturned() {
         return numberReturned;
     }
 
+    /**
+     * Gets whether this query was performed with a cursor ID that was not valid on the server.
+     *
+     * @return true if this reply indicates the request to get more data was performed with a cursor ID that's not valid on the server
+     */
     public boolean isCursorNotFound() {
         return (responseFlags & CURSOR_NOT_FOUND_RESPONSE_FLAG) == CURSOR_NOT_FOUND_RESPONSE_FLAG;
     }
 
+    /**
+     * Gets whether the query failed or not.
+     *
+     * @return true if this reply indicates the query failed.
+     */
     public boolean isQueryFailure() {
         return (responseFlags & QUERY_FAILURE_RESPONSE_FLAG) == QUERY_FAILURE_RESPONSE_FLAG;
     }
