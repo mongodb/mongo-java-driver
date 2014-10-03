2b3e75433aaa26e24f95fc4a740442581cc09214
Santiago M. Mola <smola@stratio.com>
2014-10-03 16:12:08 +0100

    Add equals() and hashCode() to MongoClientURI.

--- src/main/com/mongodb/MongoClientURI.java
+++ src/main/com/mongodb/MongoClientURI.java
@@ -653,4 +653,48 @@ public class MongoClientURI {
         return uri;
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+
+        MongoClientURI that = (MongoClientURI) o;
+
+        if (collection != null ? !collection.equals(that.collection) : that.collection != null) {
+            return false;
+        }
+        if (credentials != null ? !credentials.equals(that.credentials) : that.credentials != null) {
+            return false;
+        }
+        if (database != null ? !database.equals(that.database) : that.database != null) {
+            return false;
+        }
+        if (hosts != null ? !hosts.equals(that.hosts) : that.hosts != null) {
+            return false;
+        }
+        if (options != null ? !options.equals(that.options) : that.options != null) {
+            return false;
+        }
+        if (uri != null ? !uri.equals(that.uri) : that.uri != null) {
+            return false;
+        }
+
+        return true;
+  }
+
+  @Override
+  public int hashCode() {
+      int result = options != null ? options.hashCode() : 0;
+      result = 31 * result + (credentials != null ? credentials.hashCode() : 0);
+      result = 31 * result + (hosts != null ? hosts.hashCode() : 0);
+      result = 31 * result + (database != null ? database.hashCode() : 0);
+      result = 31 * result + (collection != null ? collection.hashCode() : 0);
+      result = 31 * result + (uri != null ? uri.hashCode() : 0);
+      return result;
+  }
+
 }
