af69a7948160482469babaee56e484a5c5e7e462
Jeff Yemin <jeff.yemin@10gen.com>
2014-10-27 12:32:18 -0400

    Fixed documentation of return value for BSONObject.put to indicate that it returns the previous value associated with the given key.
      JAVA-1502

--- driver/src/main/org/bson/BSONObject.java
+++ driver/src/main/org/bson/BSONObject.java
@@ -30,7 +30,8 @@ public interface BSONObject {
      *
      * @param key Name to set
      * @param v   Corresponding value
-     * @return <tt>v</tt>
+     * @return the previous value associated with <tt>key</tt>, or <tt>null</tt> if there was no mapping for <tt>key</tt>. (A <tt>null</tt>
+     * return can also indicate that the map previously associated <tt>null</tt> with <tt>key</tt>.)
      */
     Object put(String key, Object v);
 
