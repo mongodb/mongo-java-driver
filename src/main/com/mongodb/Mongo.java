e53e35d4cdf28677dede56ca631fd54bcf48627d
Jeff Yemin <jeff.yemin@10gen.com>
2015-01-15 09:31:49 -0500

    Preparing for 2.12.5 release

--- src/main/com/mongodb/Mongo.java
+++ src/main/com/mongodb/Mongo.java
@@ -91,7 +91,7 @@ public class Mongo {
     @Deprecated
     public static final int MINOR_VERSION = 12;
 
-    private static final String FULL_VERSION = "2.12.5-SNAPSHOT";
+    private static final String FULL_VERSION = "2.12.5";
 
     static int cleanerIntervalMS;
 
