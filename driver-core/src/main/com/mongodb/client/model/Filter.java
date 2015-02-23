c3a725923b55cc744180b66b07aea7fad206bb93
Jeff Yemin <jeff.yemin@10gen.com>
2015-02-23 15:01:30 -0500

    Changed parameter name to Filter.render

--- driver-core/src/main/com/mongodb/client/model/Filter.java
+++ driver-core/src/main/com/mongodb/client/model/Filter.java
@@ -7,12 +7,12 @@ import org.bson.codecs.configuration.CodecRegistry;
 
 public abstract class Filter {
 
-    public abstract <TDocument> BsonDocument render(Class<TDocument> collectionClass, CodecRegistry codecRegistry);
+    public abstract <TDocument> BsonDocument render(Class<TDocument> documentClass, CodecRegistry codecRegistry);
 
     public static Filter asFilter(final BsonDocument document) {
         return new Filter() {
             @Override
-            public <C> BsonDocument render(final Class<C> collectionClass, final CodecRegistry codecRegistry) {
+            public <C> BsonDocument render(final Class<C> documentClass, final CodecRegistry codecRegistry) {
                 return document;
             }
         };
@@ -21,7 +21,7 @@ public abstract class Filter {
     public static Filter asFilter(final Document document) {
         return new Filter() {
             @Override
-            public <C> BsonDocument render(final Class<C> collectionClass, final CodecRegistry codecRegistry) {
+            public <C> BsonDocument render(final Class<C> documentClass, final CodecRegistry codecRegistry) {
                 return new BsonDocumentWrapper<Document>(document, codecRegistry.get(Document.class));
             }
         };
@@ -30,7 +30,7 @@ public abstract class Filter {
     public static Filter asFilter(final String json) {
         return new Filter() {
             @Override
-            public <T> BsonDocument render(final Class<T> collectionClass, final CodecRegistry codecRegistry) {
+            public <T> BsonDocument render(final Class<T> documentClass, final CodecRegistry codecRegistry) {
                 return BsonDocument.parse(json);
             }
         };
@@ -39,9 +39,9 @@ public abstract class Filter {
     public static Filter asFilter(final Object document) {
         return new Filter() {
             @Override
-            public <C> BsonDocument render(final Class<C> collectionClass, final CodecRegistry codecRegistry) {
+            public <C> BsonDocument render(final Class<C> documentClass, final CodecRegistry codecRegistry) {
                 if (document instanceof String) {
-                    return asFilter((String) document).render(collectionClass, codecRegistry);
+                    return asFilter((String) document).render(documentClass, codecRegistry);
                 }
                 return BsonDocumentWrapper.asBsonDocument(document, codecRegistry);
             }
