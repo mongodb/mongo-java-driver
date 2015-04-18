dd5b36bfd1aa63d893edfe4f024fd4831bc2d0fb
Jeff Yemin <jeff.yemin@10gen.com>
2015-04-18 07:01:42 -0400

    When selecting a server that satisifies a read preference, select any ok server when cluster connection mode is SINGLE.
     JAVA-1767

--- driver-core/src/main/com/mongodb/selector/ReadPreferenceServerSelector.java
+++ driver-core/src/main/com/mongodb/selector/ReadPreferenceServerSelector.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2008-2014 MongoDB, Inc.
+ * Copyright 2008-2015 MongoDB, Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -17,6 +17,7 @@
 package com.mongodb.selector;
 
 import com.mongodb.ReadPreference;
+import com.mongodb.connection.ClusterConnectionMode;
 import com.mongodb.connection.ClusterDescription;
 import com.mongodb.connection.ServerDescription;
 
@@ -52,6 +53,9 @@ public class ReadPreferenceServerSelector implements ServerSelector {
 
     @Override
     public List<ServerDescription> select(final ClusterDescription clusterDescription) {
+        if (clusterDescription.getConnectionMode() == ClusterConnectionMode.SINGLE) {
+            return clusterDescription.getAny();
+        }
         return readPreference.choose(clusterDescription);
     }
 
---
+++ driver-core/src/test/unit/com/mongodb/selector/ReadPreferenceServerSelectorSpecification.groovy
@@ -0,0 +1,75 @@
+/*
+ * Copyright 2015 MongoDB, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.mongodb.selector
+
+import com.mongodb.ServerAddress
+import com.mongodb.connection.ClusterDescription
+import com.mongodb.connection.ServerDescription
+import com.mongodb.connection.ServerType
+import spock.lang.Specification
+
+import static com.mongodb.ReadPreference.primary
+import static com.mongodb.ReadPreference.secondary
+import static com.mongodb.connection.ClusterConnectionMode.MULTIPLE
+import static com.mongodb.connection.ClusterConnectionMode.SINGLE
+import static com.mongodb.connection.ClusterType.REPLICA_SET
+import static com.mongodb.connection.ServerConnectionState.CONNECTED
+
+class ReadPreferenceServerSelectorSpecification extends Specification {
+
+    def primary = ServerDescription.builder()
+                                   .state(CONNECTED)
+                                   .address(new ServerAddress())
+                                   .ok(true)
+                                   .type(ServerType.REPLICA_SET_PRIMARY)
+                                   .build();
+    def secondary = ServerDescription.builder()
+                                     .state(CONNECTED)
+                                     .address(new ServerAddress('localhost', 27018))
+                                     .ok(true)
+                                     .type(ServerType.REPLICA_SET_SECONDARY)
+                                     .build();
+
+    def 'constructor should throws if read preference is null'() {
+        when:
+        new ReadPreferenceServerSelector(null)
+
+        then:
+        thrown(IllegalArgumentException)
+    }
+
+    def 'should get read preference'() {
+        expect:
+        new ReadPreferenceServerSelector(primary()).readPreference == primary()
+    }
+
+    def 'should override toString'() {
+        expect:
+        new ReadPreferenceServerSelector(primary()).toString() == 'ReadPreferenceServerSelector{readPreference=primary}'
+    }
+
+    def 'should select server that matches read preference when connection mode is multiple'() {
+        expect:
+        new ReadPreferenceServerSelector(primary()).select(new ClusterDescription(MULTIPLE, REPLICA_SET, [primary, secondary])) ==
+        [primary]
+        new ReadPreferenceServerSelector(secondary()).select(new ClusterDescription(MULTIPLE, REPLICA_SET, [primary, secondary])) ==
+        [secondary]
+    }
+
+    def 'should select any ok server when connection mode is single'() {
+        expect:
+        new ReadPreferenceServerSelector(primary()).select(new ClusterDescription(SINGLE, REPLICA_SET, [secondary])) == [secondary]
+    }
+}
