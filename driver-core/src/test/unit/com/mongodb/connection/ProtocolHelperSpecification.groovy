79916d74811b7dc77265db609596447483e191b1
Jeff Yemin <jeff.yemin@10gen.com>
2015-05-18 14:41:48 -0400

    Handling command responses that are missing "ok" and/or "errmsg" fields.
       JAVA-1811

--- driver-core/src/test/unit/com/mongodb/connection/ProtocolHelperSpecification.groovy
+++ driver-core/src/test/unit/com/mongodb/connection/ProtocolHelperSpecification.groovy
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2014-2015 MongoDB, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.mongodb.connection
 
 import com.mongodb.MongoCommandException
@@ -24,6 +40,11 @@ import static com.mongodb.connection.ProtocolHelper.getWriteResult
 import static com.mongodb.connection.ProtocolHelper.isCommandOk
 
 class ProtocolHelperSpecification extends Specification {
+
+    def 'isCommandOk should be false if ok field is missing'() {
+        expect:
+        !isCommandOk(new BsonDocument())
+    }
 
     def 'isCommandOk should be false for numbers that are 0'() {
         expect:
