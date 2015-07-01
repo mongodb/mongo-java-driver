db3b40f57f483954976624f84b70fb27ffe2783a
Ross Lawley <ross.lawley@gmail.com>
2015-07-01 16:00:41 +0100

    Fix IndexesPrimer.java index order
    DOCS-5753

--- driver/src/examples/primer/IndexesPrimer.java
+++ driver/src/examples/primer/IndexesPrimer.java
@@ -41,7 +41,7 @@ public class IndexesPrimer extends PrimerTestCase {
     public void  createCompoundIndex() {
         // @begin: create-compound-index
         // @code: start
-        db.getCollection("restaurants").createIndex(new Document("cuisine", 1).append("address.zipcode", 1));
+        db.getCollection("restaurants").createIndex(new Document("cuisine", 1).append("address.zipcode", -1));
         // @code: end
 
         // @post: The method does not return a result.
