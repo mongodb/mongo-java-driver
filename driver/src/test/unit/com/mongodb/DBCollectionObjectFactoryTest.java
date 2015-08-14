0572b1327f977c6a8d6429f41edaf7d3078fcb92
Jeff Yemin <jeff.yemin@10gen.com>
2015-08-14 14:22:10 -0400

    Class rename refactoring

--- driver/src/test/unit/com/mongodb/DBCollectionObjectFactoryTest.java
+++ driver/src/test/unit/com/mongodb/DBCollectionObjectFactoryTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2008-2014 MongoDB, Inc.
+ * Copyright 2008-2015 MongoDB, Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -25,7 +25,7 @@ import java.util.Collections;
 import static org.hamcrest.CoreMatchers.instanceOf;
 import static org.junit.Assert.assertThat;
 
-public class DBObjectFactoryTest {
+public class DBCollectionObjectFactoryTest {
 
     private DBCollectionObjectFactory factory;
 
