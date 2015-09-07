00842d345586ce3919006a743140d5ca6d4b4014
Ross Lawley <ross.lawley@gmail.com>
2015-09-07 10:09:15 +0100

    Docs - fix headers and menu toggle
    JAVA-1865

--- docs/reference/themes/mongodb/static/js/scripts.js
+++ docs/reference/themes/mongodb/static/js/scripts.js
@@ -1,15 +1,35 @@
 function initializeJS() {
     jQuery('.toggle-nav').click(function () {
-        if (jQuery('#sidebar > .ssidebar').is(":visible") === true) {
-            jQuery('#sidebar > .ssidebar').hide();
-            jQuery('#sidebar > .nav-footer').hide();
-            jQuery("#container").addClass("sidebar-closed");
+        var $container = jQuery('#container'),
+            $sidebar = jQuery('#sidebar'),
+            $ssidebar = jQuery('#sidebar > .ssidebar'),
+            $footer = jQuery('#sidebar > .nav-footer'),
+            isVisible = $ssidebar.is(":visible") && !$container.hasClass("sidebar-closed-winSize");
+
+        if (isVisible) {
+            $container.addClass("sidebar-closed");
+            $ssidebar.hide();
+            $footer.hide();
+            $sidebar.removeClass("reveal");
         } else {
-            jQuery("#container").removeClass("sidebar-closed");
-            jQuery('#sidebar > .ssidebar').show();
-            jQuery('#sidebar > .nav-footer').show();
+            $container.removeClass("sidebar-closed");
+            $container.removeClass("sidebar-closed-winSize");
+            $sidebar.addClass("reveal");
+            $ssidebar.show();
+            $footer.show();
         }
     });
+
+    var resizer = function() {
+        var $container = jQuery('#container');
+        if (jQuery(window).width() <= 1093) {
+            $container.addClass("sidebar-closed-winSize");
+        } else {
+            $container.removeClass("sidebar-closed-winSize");
+        }
+    };
+    jQuery(window).resize(resizer);
+    resizer();
 };
 
 jQuery(document).ready(function() {
