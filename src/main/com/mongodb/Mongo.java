d752ee4b0bb24a7fa71bdda229ffad80785d3fd5
Jeff Yemin <jeff.yemin@10gen.com>
2015-09-08 10:15:01 -0400

    Bumping to 2.13.3

--- src/main/com/mongodb/Mongo.java
+++ src/main/com/mongodb/Mongo.java
@@ -89,7 +89,7 @@ public class Mongo {
     @Deprecated
     public static final int MINOR_VERSION = 13;
 
-    private static final String FULL_VERSION = "2.13.3-SNAPSHOT";
+    private static final String FULL_VERSION = "2.13.3";
 
     static int cleanerIntervalMS;
 
