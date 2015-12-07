f1fd3752d4828caf49c97085c3ffefd48cd63bc9
Jeff Yemin <jeff.yemin@10gen.com>
2015-12-07 14:26:39 -0500

    Bump to 2.14.1-SNAPSHOT

--- src/main/com/mongodb/Mongo.java
+++ src/main/com/mongodb/Mongo.java
@@ -89,7 +89,7 @@ public class Mongo {
     @Deprecated
     public static final int MINOR_VERSION = 14;
 
-    private static final String FULL_VERSION = "2.14.0";
+    private static final String FULL_VERSION = "2.14.1-SNAPSHOT";
 
     static int cleanerIntervalMS;
 
