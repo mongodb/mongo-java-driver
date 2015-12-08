fcd3214f73c254e7c46903ef10b05986a80374ab
Jeff Yemin <jeff.yemin@10gen.com>
2015-12-08 13:22:50 -0500

    JAVA-2052: Ensure that DefaultConnectionPool only used daemon background threads

---
+++ driver-core/src/test/unit/com/mongodb/internal/thread/DaemonThreadFactorySpecification.groovy
@@ -0,0 +1,32 @@
+/*
+ * Copyright (c) 2015 MongoDB, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package com.mongodb.internal.thread
+
+import spock.lang.Specification
+
+class DaemonThreadFactorySpecification extends Specification {
+    def 'should create daemon threads'() {
+        def factory = new DaemonThreadFactory()
+
+        when:
+        def thread = factory.newThread { }
+
+        then:
+        thread.isDaemon()
+    }
+}
