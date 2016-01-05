c59e798d83256b1f57cae68c0e80672e422d3ef3
Jeff Yemin <jeff.yemin@10gen.com>
2016-01-05 10:43:02 -0500

    JAVA-2069: Use both setVersion and electionId to detect a stale primary

--- driver-core/src/test/unit/com/mongodb/connection/TestClusterableServerFactory.java
+++ driver-core/src/test/unit/com/mongodb/connection/TestClusterableServerFactory.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2008-2014 MongoDB, Inc.
+ * Copyright 2008-2016 MongoDB, Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -141,6 +141,7 @@ public class TestClusterableServerFactory implements ClusterableServerFactory {
                                 .hosts(hostsSet)
                                 .passives(passivesSet)
                                 .setName(setName)
-                                .electionId(electionId);
+                                .electionId(electionId)
+                                .setVersion(1);
     }
 }
