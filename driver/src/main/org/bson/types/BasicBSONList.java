c79876f55b1e5e73b6d93d780d4b316f9f6b60f2
Jeff Yemin <jeff.yemin@10gen.com>
2016-02-15 13:47:48 -0500

    JAVA-2147: Optimize BasicBSONList.keySet by making StringRangeSet an optimized implementation of a Set

--- driver/src/main/org/bson/types/BasicBSONList.java
+++ driver/src/main/org/bson/types/BasicBSONList.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2008-2014 MongoDB, Inc.
+ * Copyright 2008-2016 MongoDB, Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -141,7 +141,7 @@ public class BasicBSONList extends ArrayList<Object> implements BSONObject {
 
     @Override
     public Set<String> keySet() {
-        return new StringRangeSet(size()).getSet();
+        return new StringRangeSet(size());
     }
 
     @Override
