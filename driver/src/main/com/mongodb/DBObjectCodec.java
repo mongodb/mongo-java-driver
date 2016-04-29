1397000959462f17207feb84cb08bc982b5f4df0
Andres Oviedo <aoviedo@digitalorigin.com>
2016-04-29 12:56:26 +0200

    Log offensive document when exceeding maxDocumentSize

--- driver/src/main/com/mongodb/DBObjectCodec.java
+++ driver/src/main/com/mongodb/DBObjectCodec.java
@@ -23,6 +23,7 @@ import org.bson.BsonDbPointer;
 import org.bson.BsonDocument;
 import org.bson.BsonDocumentWriter;
 import org.bson.BsonReader;
+import org.bson.BsonSerializationException;
 import org.bson.BsonType;
 import org.bson.BsonValue;
 import org.bson.BsonWriter;
@@ -39,6 +40,9 @@ import org.bson.types.BSONTimestamp;
 import org.bson.types.Binary;
 import org.bson.types.CodeWScope;
 import org.bson.types.Symbol;
+
+import com.mongodb.diagnostics.logging.Logger;
+import com.mongodb.diagnostics.logging.Loggers;
 
 import java.lang.reflect.Array;
 import java.util.ArrayList;
@@ -61,6 +65,8 @@ import static org.bson.BsonBinarySubType.UUID_STANDARD;
  */
 @SuppressWarnings("rawtypes")
 public class DBObjectCodec implements CollectibleCodec<DBObject> {
+	
+	public static final Logger LOGGER = Loggers.getLogger("coder");
 
     private static final BsonTypeClassMap DEFAULT_BSON_TYPE_CLASS_MAP = createDefaultBsonTypeClassMap();
     private static final String ID_FIELD_NAME = "_id";
@@ -119,18 +125,23 @@ public class DBObjectCodec implements CollectibleCodec<DBObject> {
 
     @Override
     public void encode(final BsonWriter writer, final DBObject document, final EncoderContext encoderContext) {
-        writer.writeStartDocument();
+        try {
+			writer.writeStartDocument();
 
-        beforeFields(writer, encoderContext, document);
+			beforeFields(writer, encoderContext, document);
 
-        for (final String key : document.keySet()) {
-            if (skipField(encoderContext, key)) {
-                continue;
-            }
-            writer.writeName(key);
-            writeValue(writer, encoderContext, document.get(key));
-        }
-        writer.writeEndDocument();
+			for (final String key : document.keySet()) {
+			    if (skipField(encoderContext, key)) {
+			        continue;
+			    }
+			    writer.writeName(key);
+			    writeValue(writer, encoderContext, document.get(key));
+			}
+			writer.writeEndDocument();
+		} catch (BsonSerializationException ex) {
+			LOGGER.error("Problem serializing document with id '"+document.get("_id")+"': "+ex.getMessage());
+			throw ex;
+		}
     }
 
     @Override
