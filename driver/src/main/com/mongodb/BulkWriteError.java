6d9c3e030274d98be3b6cd51184f34ca8525f456
Jeff Yemin <jeff.yemin@10gen.com>
2016-08-26 10:56:27 -0400

    JAVA-2267: Add toString method to BulkWriteError, as BulkWriteException relies on it to create its message

--- driver/src/main/com/mongodb/BulkWriteError.java
+++ driver/src/main/com/mongodb/BulkWriteError.java
@@ -117,4 +117,14 @@ public class BulkWriteError {
         result = 31 * result + details.hashCode();
         return result;
     }
+
+    @Override
+    public String toString() {
+        return "BulkWriteError{"
+                       + "index=" + index
+                       + ", code=" + code
+                       + ", message='" + message + '\''
+                       + ", details=" + details
+                       + '}';
+    }
 }
