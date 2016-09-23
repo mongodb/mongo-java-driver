11dcf7a35989bf6a02454856eeecbd244f98ae05
Ross Lawley <ross.lawley@gmail.com>
2016-09-23 11:03:52 +0100

    Remove unneeded import

--- driver-core/src/main/com/mongodb/connection/netty/NettyStream.java
+++ driver-core/src/main/com/mongodb/connection/netty/NettyStream.java
@@ -19,7 +19,6 @@ package com.mongodb.connection.netty;
 import com.mongodb.MongoException;
 import com.mongodb.MongoInternalException;
 import com.mongodb.MongoInterruptedException;
-import com.mongodb.MongoSocketException;
 import com.mongodb.MongoSocketOpenException;
 import com.mongodb.MongoSocketReadTimeoutException;
 import com.mongodb.ServerAddress;
