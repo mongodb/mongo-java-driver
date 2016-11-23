04a11257b48d37ed8f776b77097cfd7d208f1d80
Daniel-Dos <daniel.dias.analistati@gmail.com>
2016-11-23 09:58:58 -0200

    JAVA-2370

--- bson/src/main/org/bson/BsonElement.java
+++ bson/src/main/org/bson/BsonElement.java
@@ -54,4 +54,43 @@ public class BsonElement {
     public BsonValue getValue() {
         return value;
     }
+
+    @Override
+    public int hashCode() {
+	final int prime = 31;
+	int result = 1;
+	result = prime * result + ((name == null) ? 0 : name.hashCode());
+	result = prime * result + ((value == null) ? 0 : value.hashCode());
+	return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+	if (this == obj)
+		return true;
+	if (obj == null)
+		return false;
+	if (getClass() != obj.getClass())
+		return false;
+	BsonElement other = (BsonElement) obj;
+	if (name == null) {
+		if (other.name != null)
+			return false;
+	} else if (!name.equals(other.name))
+		return false;
+	if (value == null) {
+		if (other.value != null)
+			return false;
+	} else if (!value.equals(other.value))
+		return false;
+	return true;
+    }
+
+    @Override
+    public String toString() {
+	return "BsonElement{"
+		+ "name=" + name 
+		+ ", value=" + value 
+		+ "}";
+    }
 }
