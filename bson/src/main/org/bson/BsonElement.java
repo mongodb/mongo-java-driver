d16cd7878ebf11ebf97def38b3f1f0a2076694da
Daniel-Dos <daniel.dias.analistati@gmail.com>
2016-11-23 12:54:30 -0200

    JAVA-2370: Add equals/hashCode/toString to BsonElement

--- bson/src/main/org/bson/BsonElement.java
+++ bson/src/main/org/bson/BsonElement.java
@@ -54,4 +54,50 @@ public class BsonElement {
     public BsonValue getValue() {
         return value;
     }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((name == null) ? 0 : name.hashCode());
+        result = prime * result + ((value == null) ? 0 : value.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(final Object o) {
+        if (this == o) {
+                return true;
+        }
+        if (o == null) {
+                return false;
+        }
+        if (getClass() != o.getClass()) {
+                return false;
+        }
+        BsonElement bosonElement = (BsonElement) o;
+        if (name == null) {
+                if (bosonElement.name != null) {
+                        return false;
+                }
+        } else if (!name.equals(bosonElement.name)) {
+                return false;
+        }
+        if (value == null) {
+                if (bosonElement.value != null) {
+                        return false;
+                }
+        } else if (!value.equals(bosonElement.value)) {
+                return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "BsonElement{"
+                + "name=" + name
+                + ", value=" + value
+                + "}";
+    }
 }
