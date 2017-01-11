eb3353b2b93f6a26d1d4b8c1a3bcc389fe64f9f1
Jeff Yemin <jeff.yemin@10gen.com>
2017-01-11 11:29:25 -0500

    Remove remaining unused functions Remove double negative

--- .evergreen/run-tests.sh
+++ .evergreen/run-tests.sh
@@ -60,7 +60,7 @@ provision_ssl () {
 # Provision the correct connection string and set up SSL if needed
 if [ "$TOPOLOGY" == "sharded_cluster" ]; then
 
-     if [ "$AUTH" != "noauth" ]; then
+     if [ "$AUTH" = "auth" ]; then
        export MONGODB_URI="mongodb://bob:pwd123@localhost:27017/?authSource=admin"
      else
        export MONGODB_URI="mongodb://localhost:27017"
