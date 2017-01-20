798ed8a91d363e9658c72acc17743ce20fa8f0ab
Jeff Yemin <jeff.yemin@10gen.com>
2017-01-20 13:17:56 -0500

    JAVA-2348: Use ORG_GRADLE_PROJECT-prefixed environment variable to pass project properties to Gradle

--- .evergreen/publish.sh
+++ .evergreen/publish.sh
@@ -13,5 +13,12 @@ echo ${RING_FILE_GPG_BASE64} | base64 -d > ${PROJECT_DIRECTORY}/secring.gpg
 echo "Publishing snapshot with jdk8"
 
 export JAVA_HOME="/opt/java/jdk8"
+
+export ORG_GRADLE_PROJECT_nexusUsername=${NEXUS_USERNAME}
+export ORG_GRADLE_PROJECT_nexusPassword=${NEXUS_PASSWORD}
+export ORG_GRADLE_PROJECT_signing_keyId=${SIGNING_KEY_ID}
+export ORG_GRADLE_PROJECT_signing_password=${SIGNING_PASSWORD}
+export ORG_GRADLE_PROJECT_signing_secretKeyRingFile=${PROJECT_DIRECTORY}/secring.gpg
+
 ./gradlew -version
-./gradlew -PnexusUsername=${NEXUS_USER} -PnexusPassword=${NEXUS_PWD} -Psigning.secretKeyRingFile=${PROJECT_DIRECTORY}/secring.gpg -Psigning.password=${SIGNING_PWD} -Psigning.keyId=${SIGNING_KEY_ID} uploadSnapshotArchives --info
+./gradlew uploadSnapshotArchives
