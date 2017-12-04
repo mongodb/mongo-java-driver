41f8cf0b333d7882a96a8bfb4b10f6dcace45606
Qi Liu <qliu@deal.com>
2017-12-04 09:38:13 +0000

    Allow discriminators to be used in a collection.
    Fixes encoding issue, so that actual codecs for interfaces
    or abstract classes are used.
    
    JAVA-2608

--- bson/src/test/unit/org/bson/codecs/pojo/PojoRoundTripTest.java
+++ bson/src/test/unit/org/bson/codecs/pojo/PojoRoundTripTest.java
@@ -54,6 +54,7 @@ import org.bson.codecs.pojo.entities.SimpleGenericsModel;
 import org.bson.codecs.pojo.entities.SimpleModel;
 import org.bson.codecs.pojo.entities.SimpleNestedPojoModel;
 import org.bson.codecs.pojo.entities.UpperBoundsConcreteModel;
+import org.bson.codecs.pojo.entities.conventions.CollectionDiscriminatorModel;
 import org.bson.codecs.pojo.entities.conventions.CreatorAllFinalFieldsModel;
 import org.bson.codecs.pojo.entities.conventions.CreatorConstructorIdModel;
 import org.bson.codecs.pojo.entities.conventions.CreatorConstructorModel;
@@ -61,12 +62,17 @@ import org.bson.codecs.pojo.entities.conventions.CreatorConstructorRenameModel;
 import org.bson.codecs.pojo.entities.conventions.CreatorMethodModel;
 import org.bson.codecs.pojo.entities.conventions.CreatorNoArgsConstructorModel;
 import org.bson.codecs.pojo.entities.conventions.CreatorNoArgsMethodModel;
+import org.bson.codecs.pojo.entities.conventions.Subclass1Model;
+import org.bson.codecs.pojo.entities.conventions.Subclass2Model;
+import org.bson.codecs.pojo.entities.conventions.SuperClassModel;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
 
 import static java.lang.String.format;
@@ -270,6 +276,16 @@ public final class PojoRoundTripTest extends PojoTestCase {
                 new ContainsAlternativeMapAndCollectionModel(BsonDocument.parse("{customList: [1,2,3], customMap: {'field': 'value'}}")),
                 getPojoCodecProviderBuilder(ContainsAlternativeMapAndCollectionModel.class),
                 "{customList: [1,2,3], customMap: {'field': 'value'}}"));
+
+        data.add(new TestData("Collection of discriminators", new CollectionDiscriminatorModel().setList(Arrays
+                .asList(new Subclass1Model().setName("abc").setValue(true),
+                        new Subclass2Model().setInteger(234).setValue(false))).setMap(
+                Collections.singletonMap("key", new Subclass2Model().setInteger(123).setValue(true))),
+                getPojoCodecProviderBuilder(CollectionDiscriminatorModel.class, SuperClassModel.class, Subclass1Model.class,
+                        Subclass2Model.class),
+                "{list: [{_t: 'org.bson.codecs.pojo.entities.conventions.Subclass1Model', value: true, name: 'abc'},"
+                        + "{_t: 'org.bson.codecs.pojo.entities.conventions.Subclass2Model', value: false, integer: 234}],"
+                        + "map: {key: {_t: 'org.bson.codecs.pojo.entities.conventions.Subclass2Model', value: true, integer: 123}}}"));
 
         return data;
     }
