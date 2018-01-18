d7e4b86535dd2cae41e6948f808b6927e5857541
Jeff Yemin <jeff.yemin@10gen.com>
2018-01-18 11:23:02 -0500

    JAVA-2737: Add functional test

--- driver-core/src/test/functional/com/mongodb/connection/SingleServerClusterTest.java
+++ driver-core/src/test/functional/com/mongodb/connection/SingleServerClusterTest.java
@@ -17,41 +17,51 @@
 package com.mongodb.connection;
 
 import com.mongodb.MongoCompressor;
+import com.mongodb.ReadPreference;
+import com.mongodb.ServerAddress;
+import com.mongodb.internal.connection.NoOpSessionContext;
+import com.mongodb.internal.validator.NoOpFieldNameValidator;
 import com.mongodb.selector.ServerSelector;
+import org.bson.BsonDocument;
+import org.bson.BsonDouble;
+import org.bson.BsonString;
+import org.bson.codecs.BsonDocumentCodec;
 import org.junit.After;
-import org.junit.Before;
 import org.junit.Test;
 
 import java.util.Collections;
 import java.util.List;
 
 import static com.mongodb.ClusterFixture.getCredentialList;
+import static com.mongodb.ClusterFixture.getDefaultDatabaseName;
 import static com.mongodb.ClusterFixture.getPrimary;
+import static com.mongodb.ClusterFixture.getSecondary;
 import static com.mongodb.ClusterFixture.getSslSettings;
 import static java.util.Collections.singletonList;
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
 public class SingleServerClusterTest {
     private SingleServerCluster cluster;
 
-    @Before
-    public void setUp() throws Exception {
+
+    private void setUpCluster(final ServerAddress serverAddress) {
         SocketStreamFactory streamFactory = new SocketStreamFactory(SocketSettings.builder().build(),
-                                                                    getSslSettings());
+                getSslSettings());
         ClusterId clusterId = new ClusterId();
         ClusterSettings clusterSettings = ClusterSettings.builder()
-                                               .mode(ClusterConnectionMode.SINGLE)
-                                               .hosts(singletonList(getPrimary()))
-                                               .build();
+                .mode(ClusterConnectionMode.SINGLE)
+                .hosts(singletonList(serverAddress))
+                .build();
         cluster = new SingleServerCluster(clusterId,
-                                          clusterSettings,
-                                          new DefaultClusterableServerFactory(clusterId, clusterSettings, ServerSettings.builder().build(),
-                                                                              ConnectionPoolSettings.builder().maxSize(1).build(),
-                                                                              streamFactory, streamFactory, getCredentialList(),
+                clusterSettings,
+                new DefaultClusterableServerFactory(clusterId, clusterSettings, ServerSettings.builder().build(),
+                        ConnectionPoolSettings.builder().maxSize(1).build(),
+                        streamFactory, streamFactory, getCredentialList(),
 
-                                                                              null, null, null,
-                                                                              Collections.<MongoCompressor>emptyList()));
+                        null, null, null,
+                        Collections.<MongoCompressor>emptyList()));
     }
 
     @After
@@ -61,25 +71,54 @@ public class SingleServerClusterTest {
 
     @Test
     public void shouldGetDescription() {
+        // given
+        setUpCluster(getPrimary());
+
+        // expect
         assertNotNull(cluster.getDescription());
     }
 
     @Test
     public void descriptionShouldIncludeSettings() {
+        // given
+        setUpCluster(getPrimary());
+
+        // expect
         assertNotNull(cluster.getDescription().getClusterSettings());
         assertNotNull(cluster.getDescription().getServerSettings());
     }
 
     @Test
     @SuppressWarnings("deprecation")
-    public void shouldGetServerWithOkDescription() throws InterruptedException {
+    public void shouldGetServerWithOkDescription() {
+        // given
+        setUpCluster(getPrimary());
+
+        // when
         Server server = cluster.selectServer(new ServerSelector() {
             @Override
             public List<ServerDescription> select(final ClusterDescription clusterDescription) {
                 return clusterDescription.getPrimaries();
             }
         });
+
+        // then
         assertTrue(server.getDescription().isOk());
     }
 
+    @Test
+    public void shouldSuccessfullyQueryASecondaryWithPrimaryReadPreference() {
+        // given
+        ServerAddress secondary = getSecondary();
+        setUpCluster(secondary);
+        String collectionName = getClass().getName();
+        Connection connection = cluster.getServer(secondary).getConnection();
+
+        // when
+        BsonDocument result = connection.command(getDefaultDatabaseName(), new BsonDocument("find", new BsonString(collectionName)),
+                new NoOpFieldNameValidator(), ReadPreference.primary(), new BsonDocumentCodec(), NoOpSessionContext.INSTANCE);
+
+        // then
+        assertEquals(new BsonDouble(1.0).intValue(), result.getNumber("ok").intValue());
+    }
 }
