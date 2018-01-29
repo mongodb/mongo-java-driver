0aa2ec20d5215c0ac727602dd2cd891c22c69ba8
Ross Lawley <ross.lawley@gmail.com>
2018-01-29 11:07:45 +0000

    Normalized Copyright notices
    JAVA-2755

--- driver-core/src/main/com/mongodb/internal/connection/NoOpSessionContext.java
+++ driver-core/src/main/com/mongodb/internal/connection/NoOpSessionContext.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2017 MongoDB, Inc.
+ * Copyright 2008-present MongoDB, Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -12,7 +12,6 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *
  */
 
 package com.mongodb.internal.connection;
