3b24e49be83958587101522d6d172da20da5979f
Jeff Yemin <jeff.yemin@10gen.com>
2018-04-24 21:48:59 -0400

    JAVA-2699: Test Atlas connectivity

---
+++ .evergreen/run-connectivity-tests.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Don't trace since the URI contains a password that shouldn't show up in the logs
+set -o errexit  # Exit the script with error if any of the commands fail
+
+# Supported/used environment variables:
+#       JDK                     Set the version of java to be used.  Java versions can be set from the java toolchain /opt/java
+#                               "jdk5", "jdk6", "jdk7", "jdk8"
+# Support arguments:
+#       Pass as many MongoDB URIS as arguments to this script as required
+
+JDK=${JDK:-jdk}
+
+############################################
+#            Main Program                  #
+############################################
+
+echo "Running connectivity tests with ${JDK}"
+
+# We always compile with the latest version of java
+export JAVA_HOME="/opt/java/jdk8"
+
+./gradlew -version
+
+for MONGODB_URI in $@; do
+    ./gradlew -PjdkHome=/opt/java/${JDK} -Dorg.mongodb.test.uri=${MONGODB_URI} --stacktrace --info -Dtest.single=ConnectivityTest --rerun-tasks driver-sync:test
+done
