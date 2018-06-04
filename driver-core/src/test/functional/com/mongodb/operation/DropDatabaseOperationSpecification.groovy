2470feab5537b1071b422d538747ac7f38d519c9
Jeff Yemin <jeff.yemin@10gen.com>
2018-06-04 10:24:44 -0400

    JAVA-2868: Disable drop database write concern tests failing against 4.0 server

--- driver-core/src/test/functional/com/mongodb/operation/DropDatabaseOperationSpecification.groovy
+++ driver-core/src/test/functional/com/mongodb/operation/DropDatabaseOperationSpecification.groovy
@@ -83,7 +83,7 @@ class DropDatabaseOperationSpecification extends OperationFunctionalSpecificatio
         !databaseNameExists(dbName)
     }
 
-    @IgnoreIf({ !serverVersionAtLeast(3, 4) || serverVersionAtLeast(4, 1) || !isDiscoverableReplicaSet() })
+    @IgnoreIf({ !serverVersionAtLeast(3, 4) || serverVersionAtLeast(4, 0) || !isDiscoverableReplicaSet() })
     def 'should throw on write concern error'() {
         given:
         getCollectionHelper().insertDocuments(new DocumentCodec(), new Document('documentTo', 'createTheCollection'))
