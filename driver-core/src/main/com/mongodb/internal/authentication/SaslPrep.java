e70f47b6a42dfd1356e06d00bbda7f124221cf6b
Ross Lawley <ross.lawley@gmail.com>
2018-06-21 12:34:14 +0100

    Reverted copyright changes
    JAVA-2857

--- driver-core/src/main/com/mongodb/internal/authentication/SaslPrep.java
+++ driver-core/src/main/com/mongodb/internal/authentication/SaslPrep.java
@@ -1,5 +1,6 @@
 /*
  * Copyright 2008-present MongoDB, Inc.
+ * Copyright 2017 Tom Bentley
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
