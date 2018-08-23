ff7dff7a1f7538305926586a0e6f7302688f442d
Ross Lawley <ross.lawley@gmail.com>
2018-08-23 12:47:09 +0100

    Added Drop and Other ChangeStream operation types
    JAVA-2939 JAVA-2919

--- driver-core/src/main/com/mongodb/client/model/changestream/OperationType.java
+++ driver-core/src/main/com/mongodb/client/model/changestream/OperationType.java
@@ -16,8 +16,6 @@
 
 package com.mongodb.client.model.changestream;
 
-import static java.lang.String.format;
-
 /**
  * The {@code $changeStream} operation type.
  *
@@ -48,7 +46,24 @@ public enum OperationType {
     /**
      * The invalidate operation type
      */
-    INVALIDATE("invalidate");
+    INVALIDATE("invalidate"),
+
+    /**
+     * The drop operation type
+     *
+     * @since 3.8.2
+     */
+    DROP("drop"),
+
+    /**
+     * The other operation type.
+     *
+     * <p>A placeholder for newer operation types issued by the server.
+     * Users encountering OTHER operation types are advised to update the driver to get the actual operation type.</p>
+     *
+     * @since 3.8.2
+     */
+    OTHER("other");
 
     private final String value;
     OperationType(final String operationTypeName) {
@@ -76,7 +91,7 @@ public enum OperationType {
                 }
             }
         }
-        throw new IllegalArgumentException(format("'%s' is not a valid OperationType", operationTypeName));
+        return OTHER;
     }
 
     @Override
