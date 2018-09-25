d076619cc09a078b01a2d2abf756ac52e82d9e4f
Ross Lawley <ross.lawley@gmail.com>
2018-09-25 10:07:59 +0100

    Build: Packaging for driver-embedded projects
    * Refactored how plugins are applied across the projects.
      As the android plugin can't be used if the java plugin
      is installed.
    * Added driver-embedded-android project to handle building aar.
    * Updated gradle and plugin versions
    
    JAVA-2929 JAVA-2930

--- .evergreen/publish.sh
+++ .evergreen/publish.sh
@@ -12,9 +12,7 @@ echo ${RING_FILE_GPG_BASE64} | base64 -d > ${PROJECT_DIRECTORY}/secring.gpg
 
 trap "rm ${PROJECT_DIRECTORY}/secring.gpg; exit" EXIT HUP
 
-echo "Publishing snapshot with jdk9"
 
-export JAVA_HOME="/opt/java/jdk9"
 
 export ORG_GRADLE_PROJECT_nexusUsername=${NEXUS_USERNAME}
 export ORG_GRADLE_PROJECT_nexusPassword=${NEXUS_PASSWORD}
@@ -22,5 +20,25 @@ export ORG_GRADLE_PROJECT_signing_keyId=${SIGNING_KEY_ID}
 export ORG_GRADLE_PROJECT_signing_password=${SIGNING_PASSWORD}
 export ORG_GRADLE_PROJECT_signing_secretKeyRingFile=${PROJECT_DIRECTORY}/secring.gpg
 
+SDK_ROOT=$PWD/android_sdk
+if [ ! -e  $SDK_ROOT ]; then
+    echo "Installing ANDROID SDK"
+    mkdir $SDK_ROOT
+    (
+        cd $SDK_ROOT
+        export JAVA_HOME="/opt/java/jdk8"
+        SDK_PACKAGE=sdk-tools-linux-4333796.zip
+        curl -O https://dl.google.com/android/repository/$SDK_PACKAGE
+        unzip $SDK_PACKAGE
+        yes | ./tools/bin/sdkmanager --channel=0 \
+            "platforms;android-28"  \
+            "build-tools;28.0.0"
+    )
+fi
+export ANDROID_HOME=${SDK_ROOT}
+
+echo "Publishing snapshot with jdk9"
+export JAVA_HOME="/opt/java/jdk9"
+
 ./gradlew -version
-./gradlew uploadSnapshotArchives
+./gradlew publishSnapshots
