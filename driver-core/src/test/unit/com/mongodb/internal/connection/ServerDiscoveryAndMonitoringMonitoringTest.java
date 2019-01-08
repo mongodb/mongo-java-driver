4f02a9757b7995caff63ba4f26c2121c50d90036
Jeff Yemin <jeff.yemin@10gen.com>
2019-01-08 13:31:55 -0500

    Move StableMultiServerCluster to MultiServerCluster
    Did this in two steps so that we wouldn't lose the history
    on the former MultiServerCluster class (which is now
    called AbstractMultiServerCluster)
    
    JAVA-2927

--- driver-core/src/test/unit/com/mongodb/internal/connection/ServerDiscoveryAndMonitoringMonitoringTest.java
+++ driver-core/src/test/unit/com/mongodb/internal/connection/ServerDiscoveryAndMonitoringMonitoringTest.java
@@ -102,7 +102,7 @@ public class ServerDiscoveryAndMonitoringMonitoringTest extends AbstractServerDi
                 if (newDescription.getString("topologyType").getValue().equals("Single")) {
                     assertEquals(SingleServerCluster.class, getCluster().getClass());
                 } else {
-                    assertEquals(StableMultiServerCluster.class, getCluster().getClass());
+                    assertEquals(MultiServerCluster.class, getCluster().getClass());
                 }
 
             } else if (eventDocument.containsKey("server_opening_event")) {
