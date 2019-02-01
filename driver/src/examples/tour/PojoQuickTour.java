0b010e6065e624f54af28973486748f10f1defcb
Ian Whalen <ian@mongodb.com>
2019-02-01 12:06:32 -0500

    JAVA-3134 Point GitHub links in docs to correct release branches

--- driver/src/examples/tour/PojoQuickTour.java
+++ driver/src/examples/tour/PojoQuickTour.java
@@ -38,7 +38,7 @@ import static org.bson.codecs.configuration.CodecRegistries.fromProviders;
 import static org.bson.codecs.configuration.CodecRegistries.fromRegistries;
 
 /**
- * The POJO QuickTour code example see: https://mongodb.github.io/mongo-java-driver/3.5/getting-started-pojo
+ * The POJO QuickTour code example see: https://mongodb.github.io/mongo-java-driver/3.6/driver/getting-started/quick-start-pojo/
  */
 public class PojoQuickTour {
     /**
