26ab32b71581a5564a73cea6058b7818a0b9165d
Aarjav Patel <aarjav@semandex.net>
2019-03-01 14:59:04 -0500

    JAVA-3220 add copyright and make class package private final for checkstyle

--- bson/src/main/org/bson/codecs/pojo/ConventionSetAllFieldImpl.java
+++ bson/src/main/org/bson/codecs/pojo/ConventionSetAllFieldImpl.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2008-present MongoDB, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.bson.codecs.pojo;
 
 import org.bson.codecs.configuration.CodecConfigurationException;
@@ -5,13 +21,13 @@ import org.bson.codecs.configuration.CodecConfigurationException;
 import static java.lang.String.format;
 import static java.lang.reflect.Modifier.isPublic;
 
-public class ConventionSetAllFieldImpl implements Convention {
+final class ConventionSetAllFieldImpl implements Convention {
     @Override
     public void apply(final ClassModelBuilder<?> classModelBuilder) {
         for (PropertyModelBuilder<?> propertyModelBuilder : classModelBuilder.getPropertyModelBuilders()) {
             if (!(propertyModelBuilder.getPropertyAccessor() instanceof PropertyAccessorImpl)) {
                 throw new CodecConfigurationException(format("The SET_ALL_FIELDS_CONVENTION is not compatible with "
-                                + "propertyModelBuilder instance that have custom implementations of org.bson.codecs.pojo.PropertyAccessor: %s",
+                        + "propertyModelBuilder instance that have custom implementations of org.bson.codecs.pojo.PropertyAccessor: %s",
                         propertyModelBuilder.getPropertyAccessor().getClass().getName()));
             }
             PropertyAccessorImpl<?> defaultAccessor = (PropertyAccessorImpl<?>) propertyModelBuilder.getPropertyAccessor();
