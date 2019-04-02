954b2338161c838a9886f36ba8cedbbd0de711e3
Ross Lawley <ross.lawley@gmail.com>
2019-04-02 15:09:40 +0100

    Ensure srv resolution works for legacy
    JAVA-3251

--- driver-legacy/src/main/com/mongodb/MongoClientURI.java
+++ driver-legacy/src/main/com/mongodb/MongoClientURI.java
@@ -249,6 +249,10 @@ public class MongoClientURI {
         proxied = new ConnectionString(uri);
     }
 
+    ConnectionString getProxied() {
+        return proxied;
+    }
+
     // ---------------------------------
 
     /**
