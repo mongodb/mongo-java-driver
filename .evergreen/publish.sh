1bf7ffe177faac040a7b9e2659c0db0b21466b67
Jeff Yemin <jeff.yemin@10gen.com>
2019-07-18 16:19:08 -0400

    Remove driver-embedded-android
    JAVA-3324

--- .evergreen/publish.sh
+++ .evergreen/publish.sh
@@ -20,23 +20,6 @@ export ORG_GRADLE_PROJECT_signing_keyId=${SIGNING_KEY_ID}
 export ORG_GRADLE_PROJECT_signing_password=${SIGNING_PASSWORD}
 export ORG_GRADLE_PROJECT_signing_secretKeyRingFile=${PROJECT_DIRECTORY}/secring.gpg
 
-SDK_ROOT=$PWD/android_sdk
-if [ ! -e  $SDK_ROOT ]; then
-    echo "Installing ANDROID SDK"
-    mkdir $SDK_ROOT
-    (
-        cd $SDK_ROOT
-        export JAVA_HOME="/opt/java/jdk8"
-        SDK_PACKAGE=sdk-tools-linux-4333796.zip
-        curl -O https://dl.google.com/android/repository/$SDK_PACKAGE
-        unzip $SDK_PACKAGE
-        yes | ./tools/bin/sdkmanager --channel=0 \
-            "platforms;android-28"  \
-            "build-tools;28.0.0"
-    )
-fi
-export ANDROID_HOME=${SDK_ROOT}
-
 echo "Publishing snapshot with jdk9"
 export JAVA_HOME="/opt/java/jdk9"
 
