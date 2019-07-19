c6d95b704393501cf0b626b499a60ef369f60a2a
Kevin Albertson <kevin.albertson@mongodb.com>
2019-07-18 21:19:53 -0400

    Fix FLE tests (#518)

--- driver-sync/src/test/functional/com/mongodb/client/ClientSideEncryptionProseTestSpecification.groovy
+++ driver-sync/src/test/functional/com/mongodb/client/ClientSideEncryptionProseTestSpecification.groovy
@@ -84,7 +84,7 @@ class ClientSideEncryptionProseTestSpecification extends FunctionalSpecification
           "encrypt": {
             "keyId": "/placeholder",
             "bsonType": "string",
-            "algorithm": "AEAD_AES_256_CBC_HMAC_SHA_512-Deterministic"
+            "algorithm": "AEAD_AES_256_CBC_HMAC_SHA_512-Random"
           }
         }
       }
