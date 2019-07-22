e3ddb1a78d3cb975bab247ae45b66860d2e30219
John Stewart <john.stewart@mongodb.com>
2019-07-22 14:13:46 -0400

    Remove unused import from example test

--- driver-sync/src/examples/documentation/ChangeStreamSamples.java
+++ driver-sync/src/examples/documentation/ChangeStreamSamples.java
@@ -20,7 +20,6 @@ import com.mongodb.client.MongoChangeStreamCursor;
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
 import com.mongodb.client.MongoCollection;
-import com.mongodb.client.MongoCursor;
 import com.mongodb.client.MongoDatabase;
 import com.mongodb.client.model.Aggregates;
 import com.mongodb.client.model.Filters;
