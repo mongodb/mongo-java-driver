cc6f52468d9ca415d9ef1c61e21f53ca0773a85c
John Stewart <john.stewart@mongodb.com>
2019-07-24 10:31:40 -0400

    Avoid reordering of _id field in Document and BasicDBObject
    JAVA-3353

--- bson/src/test/unit/org/bson/DocumentTest.java
+++ bson/src/test/unit/org/bson/DocumentTest.java
@@ -79,6 +79,16 @@ public class DocumentTest {
                      document);
     }
 
+    // Test to ensure that toJson does not reorder _id field
+    @Test
+    public void toJsonShouldNotReorderIdField() {
+        // given
+        Document d = new Document().append("x", 1)
+                .append("y", Collections.singletonList("one"))
+                .append("_id", "1");
+        assertEquals(new DocumentCodec().decode(new JsonReader(d.toJson()), DecoderContext.builder().build()), d);
+    }
+
     // Test in Java to make sure none of the casts result in compiler warnings or class cast exceptions
     @Test
     public void shouldGetWithDefaultValue() {
--- driver-core/src/test/unit/com/mongodb/BasicDBObjectTest.java
+++ driver-core/src/test/unit/com/mongodb/BasicDBObjectTest.java
@@ -60,19 +60,19 @@ public class BasicDBObjectTest {
 
     @Test
     public void testToJson() {
-        BasicDBObject document = BasicDBObject.parse("{ '_id' : { '$oid' : '000000000000000000000000' }, 'int' : 1, 'string' : 'abc', "
+        BasicDBObject document = BasicDBObject.parse("{ 'int' : 1, 'string' : 'abc', '_id' : { '$oid' : '000000000000000000000000' }, "
                 + "'dbRef' : { $ref: 'collection', $id: { $oid: '01234567890123456789abcd' }, $db: 'db' } }");
 
-        assertEquals("{\"_id\": {\"$oid\": \"000000000000000000000000\"}, \"int\": 1, \"string\": \"abc\", "
+        assertEquals("{\"int\": 1, \"string\": \"abc\", \"_id\": {\"$oid\": \"000000000000000000000000\"}, "
                 + "\"dbRef\": {\"$ref\": \"collection\", \"$id\": {\"$oid\": \"01234567890123456789abcd\"}, \"$db\": \"db\"}}",
                 document.toJson());
-        assertEquals("{\"_id\": ObjectId(\"000000000000000000000000\"), \"int\": 1, \"string\": \"abc\", "
+        assertEquals("{\"int\": 1, \"string\": \"abc\", \"_id\": ObjectId(\"000000000000000000000000\"), "
                 + "\"dbRef\": {\"$ref\": \"collection\", \"$id\": ObjectId(\"01234567890123456789abcd\"), \"$db\": \"db\"}}",
                 document.toJson(JsonWriterSettings.builder().outputMode(JsonMode.SHELL).build()));
-        assertEquals("{\"_id\": {\"$oid\": \"000000000000000000000000\"}, \"int\": 1, \"string\": \"abc\", "
+        assertEquals("{\"int\": 1, \"string\": \"abc\", \"_id\": {\"$oid\": \"000000000000000000000000\"}, "
                 + "\"dbRef\": {\"$ref\": \"collection\", \"$id\": {\"$oid\": \"01234567890123456789abcd\"}, \"$db\": \"db\"}}",
                 document.toJson(DECODER));
-        assertEquals("{\"_id\": ObjectId(\"000000000000000000000000\"), \"int\": 1, \"string\": \"abc\", "
+        assertEquals("{\"int\": 1, \"string\": \"abc\", \"_id\": ObjectId(\"000000000000000000000000\"), "
                 + "\"dbRef\": {\"$ref\": \"collection\", \"$id\": ObjectId(\"01234567890123456789abcd\"), \"$db\": \"db\"}}",
                 document.toJson(JsonWriterSettings.builder().outputMode(JsonMode.SHELL).build(), DECODER));
     }
