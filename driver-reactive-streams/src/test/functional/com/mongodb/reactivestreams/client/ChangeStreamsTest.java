1bda32bb039d8646be90b8eeaf4ee861b44713b8
Jeff Yemin <jeff.yemin@10gen.com>
2019-08-22 13:12:38 -0400

    Share sync ChangeStreamsTest implementation with reactive streams
    JAVA-3397

--- driver-reactive-streams/src/test/functional/com/mongodb/reactivestreams/client/ChangeStreamsTest.java
+++ driver-reactive-streams/src/test/functional/com/mongodb/reactivestreams/client/ChangeStreamsTest.java
@@ -5,7 +5,7 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *   http://www.apache.org/licenses/LICENSE-2.0
+ *    http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -14,10 +14,14 @@
  * limitations under the License.
  */
 
-package com.mongodb.client;
+
+package com.mongodb.reactivestreams.client;
 
 import com.mongodb.MongoClientSettings;
 import com.mongodb.MongoNamespace;
+import com.mongodb.client.AbstractChangeStreamsTest;
+import com.mongodb.client.MongoClient;
+import com.mongodb.reactivestreams.client.syncadapter.SyncMongoClient;
 import org.bson.BsonDocument;
 
 public class ChangeStreamsTest extends AbstractChangeStreamsTest {
@@ -28,6 +32,6 @@ public class ChangeStreamsTest extends AbstractChangeStreamsTest {
 
     @Override
     protected MongoClient createMongoClient(final MongoClientSettings settings) {
-        return MongoClients.create(settings);
+        return new SyncMongoClient(MongoClients.create(settings));
     }
 }
