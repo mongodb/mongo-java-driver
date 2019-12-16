038fc6f2e3ff53b5661301be9a2e17e00074db0f
Jeff Yemin <jeff.yemin@10gen.com>
2019-12-16 17:56:14 -0500

    Remove assumption that mapReduce always returns statistics
    Starting with MongoDB 4.4, MongoDB will no longer produce execution statistics
    for the mapReduce command.  The driver no longer assumes that they will be in
    the response, and if they are missing will just produce zero values for all
    statistics.
    
    JAVA-3522

--- driver-core/src/main/com/mongodb/internal/operation/MapReduceHelper.java
+++ driver-core/src/main/com/mongodb/internal/operation/MapReduceHelper.java
@@ -17,28 +17,29 @@
 package com.mongodb.internal.operation;
 
 import org.bson.BsonDocument;
+import org.bson.BsonInt32;
 
 final class MapReduceHelper {
 
     static MapReduceStatistics createStatistics(final BsonDocument result) {
         return new MapReduceStatistics(getInputCount(result), getOutputCount(result), getEmitCount(result),
-                                       getDuration(result));
+                getDuration(result));
     }
 
     private static int getInputCount(final BsonDocument result) {
-        return result.getDocument("counts").getNumber("input").intValue();
+        return result.getDocument("counts", new BsonDocument()).getNumber("input", new BsonInt32(0)).intValue();
     }
 
     private static int getOutputCount(final BsonDocument result) {
-        return result.getDocument("counts").getNumber("output").intValue();
+        return result.getDocument("counts", new BsonDocument()).getNumber("output", new BsonInt32(0)).intValue();
     }
 
     private static int getEmitCount(final BsonDocument result) {
-        return result.getDocument("counts").getNumber("emit").intValue();
+        return result.getDocument("counts", new BsonDocument()).getNumber("emit", new BsonInt32(0)).intValue();
     }
 
     private static int getDuration(final BsonDocument result) {
-        return result.getNumber("timeMillis").intValue();
+        return result.getNumber("timeMillis", new BsonInt32(0)).intValue();
     }
 
     private MapReduceHelper() {
