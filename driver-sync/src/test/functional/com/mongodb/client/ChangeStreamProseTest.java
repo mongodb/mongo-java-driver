73394c68871d470ebab3367a6eca228448dd39a0
John Stewart <john.stewart@mongodb.com>
2020-02-28 16:28:38 -0500

    Update disabled change stream tests
    JAVA-3625

--- driver-sync/src/test/functional/com/mongodb/client/ChangeStreamProseTest.java
+++ driver-sync/src/test/functional/com/mongodb/client/ChangeStreamProseTest.java
@@ -123,7 +123,6 @@ public class ChangeStreamProseTest extends DatabaseTestCase {
     //
     @Test
     public void testMissingResumeTokenThrowsException() {
-        assumeTrue(serverVersionLessThan(4, 3));
         boolean exceptionFound = false;
 
         MongoCursor<ChangeStreamDocument<Document>> cursor = collection.watch(asList(Aggregates.project(Document.parse("{ _id : 0 }"))))
