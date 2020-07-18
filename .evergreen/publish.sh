0ed2ad91acced78200c8e5ac16d47be98c675835
jyemin <jeff.yemin@mongodb.com>
2020-07-18 15:38:00 -0400

    Support publishing release builds in publish script used in Evergreen

--- .evergreen/publish.sh
+++ .evergreen/publish.sh
@@ -7,22 +7,28 @@ set -o errexit  # Exit the script with error if any of the commands fail
 ############################################
 #            Main Program                  #
 ############################################
+RELEASE=${RELEASE:false}
 
-echo ${RING_FILE_GPG_BASE64} | base64 -d > ${PROJECT_DIRECTORY}/secring.gpg
+echo ${RING_FILE_GPG_BASE64} | base64 -d > /tmp/secring.gpg
 
-trap "rm ${PROJECT_DIRECTORY}/secring.gpg; exit" EXIT HUP
+trap "rm /tmp/secring.gpg; exit" EXIT HUP
 
-
+git status
 
 export ORG_GRADLE_PROJECT_nexusUsername=${NEXUS_USERNAME}
 export ORG_GRADLE_PROJECT_nexusPassword=${NEXUS_PASSWORD}
 export ORG_GRADLE_PROJECT_signing_keyId=${SIGNING_KEY_ID}
 export ORG_GRADLE_PROJECT_signing_password=${SIGNING_PASSWORD}
-export ORG_GRADLE_PROJECT_signing_secretKeyRingFile=${PROJECT_DIRECTORY}/secring.gpg
+export ORG_GRADLE_PROJECT_signing_secretKeyRingFile=/tmp/secring.gpg
 
 echo "Publishing snapshot with jdk11"
 export JAVA_HOME="/opt/java/jdk11"
 
 ./gradlew -version
-./gradlew publishSnapshots
-./gradlew :bson-scala:publishSnapshots :driver-scala:publishSnapshots -PdefaultScalaVersions=2.11.12,2.12.10
+if [ "$RELEASE" == "true" ]; then
+  ./gradlew --stacktrace --info publishArchives
+  ./gradlew --stacktrace --info :bson-scala:publishArchives :driver-scala:publishArchives -PdefaultScalaVersions=2.11.12,2.12.10
+else
+  ./gradlew publishSnapshots
+  ./gradlew :bson-scala:publishSnapshots :driver-scala:publishSnapshots -PdefaultScalaVersions=2.11.12,2.12.10
+fi
