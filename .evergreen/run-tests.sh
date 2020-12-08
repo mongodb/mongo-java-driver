d3c11fa9926449e2c17228dbfc9949bc8d5f05a3
Jeff Yemin <jeff.yemin@mongodb.com>
2020-12-08 16:46:45 -0500

    Use jdk8 by default in shell scripts invoked by CI

--- .evergreen/run-tests.sh
+++ .evergreen/run-tests.sh
@@ -24,7 +24,7 @@ set -o errexit  # Exit the script with error if any of the commands fail
 AUTH=${AUTH:-noauth}
 SSL=${SSL:-nossl}
 MONGODB_URI=${MONGODB_URI:-}
-JDK=${JDK:-jdk}
+JDK=${JDK:-jdk8}
 TOPOLOGY=${TOPOLOGY:-server}
 COMPRESSOR=${COMPRESSOR:-}
 STREAM_TYPE=${STREAM_TYPE:-nio2}
