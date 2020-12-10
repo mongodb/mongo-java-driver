c943ce3a6a8664604aad7d7d9f069b64704c18a9
Ross Lawley <ross.lawley@gmail.com>
2020-12-10 15:55:22 +0000

    Evergreen: driver-core connectivity test was moved to reactive streams

--- .evergreen/run-connectivity-tests.sh
+++ .evergreen/run-connectivity-tests.sh
@@ -23,7 +23,6 @@ export JAVA_HOME="/opt/java/jdk11"
 
 for MONGODB_URI in $@; do
     ./gradlew -PjdkHome=/opt/java/${JDK} -Dorg.mongodb.test.uri=${MONGODB_URI} --stacktrace --info --rerun-tasks driver-sync:test --tests ConnectivityTest
-    ./gradlew -PjdkHome=/opt/java/${JDK} -Dorg.mongodb.test.uri=${MONGODB_URI} --stacktrace --info --rerun-tasks driver-core:test --tests ConnectivityTest
     ./gradlew -PjdkHome=/opt/java/${JDK} -Dorg.mongodb.test.uri=${MONGODB_URI} --stacktrace --info --rerun-tasks driver-legacy:test --tests ConnectivityTest
     ./gradlew -PjdkHome=/opt/java/${JDK} -Dorg.mongodb.test.uri=${MONGODB_URI} --stacktrace --info --rerun-tasks driver-reactive-streams:test --tests ConnectivityTest
 done
