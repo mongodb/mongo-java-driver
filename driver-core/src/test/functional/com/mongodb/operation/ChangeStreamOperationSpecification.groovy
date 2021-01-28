ed6ea84a471ebcaf693518502cb16b5438120b2e
Jeff Yemin <jeff.yemin@mongodb.com>
2021-01-28 13:56:37 -0500

    Make change stream operation integration test more robust
    JAVA-3942

--- driver-core/src/test/functional/com/mongodb/operation/ChangeStreamOperationSpecification.groovy
+++ driver-core/src/test/functional/com/mongodb/operation/ChangeStreamOperationSpecification.groovy
@@ -466,9 +466,13 @@ class ChangeStreamOperationSpecification extends OperationFunctionalSpecificatio
         when:
         helper.killCursor(helper.getNamespace(), cursor.getWrapped().getServerCursor())
         expected = insertDocuments(helper, [3, 4])
+        def results = nextAndClean(cursor, async)
+        if (results.size() < expected.size()) {
+            results.addAll(nextAndClean(cursor, async))
+        }
 
         then:
-        nextAndClean(cursor, async) == expected
+        results == expected
 
         then:
         tryNextAndClean(cursor, async) == null
@@ -476,9 +480,13 @@ class ChangeStreamOperationSpecification extends OperationFunctionalSpecificatio
         when:
         expected = insertDocuments(helper, [5, 6])
         helper.killCursor(helper.getNamespace(), cursor.getWrapped().getServerCursor())
-
+        results = nextAndClean(cursor, async)
+        if (results.size() < expected.size()) {
+            results.addAll(nextAndClean(cursor, async))
+        }
+        
         then:
-        nextAndClean(cursor, async) == expected
+        results == expected
 
         cleanup:
         cursor?.close()
