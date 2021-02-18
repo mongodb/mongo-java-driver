78d4b0b5a5d83b515c8fe971e7522c87b7513aba
Jeff Yemin <jeff.yemin@mongodb.com>
2021-02-18 18:10:38 -0500

    Add specialized unified test runner for valid-fail tests
    This runner is strange because we actually want the test to pass only if the
    actual unified test runner fails.  Due to the nature of the tests the failure
    can happen either in the setup method or in the actual test, so we have to
    handle both cases.
    
    JAVA-4003

---
+++ driver-sync/src/test/functional/com/mongodb/client/unified/UnifiedTestFailureValidator.java
@@ -0,0 +1,73 @@
+/*
+ * Copyright 2008-present MongoDB, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.mongodb.client.unified;
+
+import com.mongodb.MongoClientSettings;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.lang.Nullable;
+import org.bson.BsonArray;
+import org.bson.BsonDocument;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runners.Parameterized;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.util.Collection;
+
+public class UnifiedTestFailureValidator extends UnifiedTest {
+    private final String fileDescription;
+    private final String testDescription;
+    private Throwable setupException;
+
+    public UnifiedTestFailureValidator(final String fileDescription, final String testDescription, final String schemaVersion,
+                                       @Nullable final BsonArray runOnRequirements, final BsonArray entities, final BsonArray initialData,
+                                       final BsonDocument definition) {
+        super(schemaVersion, runOnRequirements, entities, initialData, definition);
+        this.fileDescription = fileDescription;
+        this.testDescription = testDescription;
+    }
+
+    @Before
+    public void setUp() {
+        try {
+            super.setUp();
+        } catch (Throwable e) {
+            setupException = e;
+        }
+    }
+
+    @Test(expected = Throwable.class)
+    public void shouldPassAllOutcomes() {
+        if (setupException != null) {
+            throw new AssertionError("Setup failed (expected)", setupException);
+        } else {
+            super.shouldPassAllOutcomes();
+        }
+    }
+
+    @Override
+    protected MongoClient createMongoClient(final MongoClientSettings settings) {
+        return MongoClients.create(settings);
+    }
+
+    @Parameterized.Parameters(name = "{0}: {1}")
+    public static Collection<Object[]> data() throws URISyntaxException, IOException {
+        return getTestData("unified-test-format/valid-fail");
+    }
+}
