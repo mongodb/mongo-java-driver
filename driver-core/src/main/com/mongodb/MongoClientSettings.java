6eb7d66115817226b7189610ec08c7877b2db392
Jeff Yemin <jeff.yemin@mongodb.com>
2021-03-02 10:14:26 -0500

    Add JsonObjectCodecProvider to javadoc of getDefaultCodecRegistry
    This provider was added to the default codec registry in the 4.2.0
    release, but not added to the javadoc that lists the providers in
    the default codec registry.
    
    JAVA-4019

--- driver-core/src/main/com/mongodb/MongoClientSettings.java
+++ driver-core/src/main/com/mongodb/MongoClientSettings.java
@@ -111,6 +111,7 @@ public final class MongoClientSettings {
      * <li>{@link com.mongodb.client.model.geojson.codecs.GeoJsonCodecProvider}</li>
      * <li>{@link com.mongodb.client.gridfs.codecs.GridFSFileCodecProvider}</li>
      * <li>{@link org.bson.codecs.jsr310.Jsr310CodecProvider}</li>
+     * <li>{@link org.bson.codecs.JsonObjectCodecProvider}</li>
      * <li>{@link org.bson.codecs.BsonCodecProvider}</li>
      * </ul>
      *
