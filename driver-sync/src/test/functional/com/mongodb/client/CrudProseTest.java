51369e38cf3836d573d3aba865d65592cc0572fa
Jeff Yemin <jeff.yemin@mongodb.com>
2021-06-03 11:07:33 -0400

    Ensure write error is included in exception message
    Implement new CRUD specification prose test for write error details
    
    JAVA-3993

--- driver-sync/src/test/functional/com/mongodb/client/CrudProseTest.java
+++ driver-sync/src/test/functional/com/mongodb/client/CrudProseTest.java
@@ -16,7 +16,12 @@
 
 package com.mongodb.client;
 
+import com.mongodb.MongoBulkWriteException;
 import com.mongodb.MongoWriteConcernException;
+import com.mongodb.MongoWriteException;
+import com.mongodb.client.model.CreateCollectionOptions;
+import com.mongodb.client.model.Filters;
+import com.mongodb.client.model.ValidationOptions;
 import org.bson.BsonArray;
 import org.bson.BsonDocument;
 import org.bson.BsonInt32;
@@ -30,38 +35,80 @@ import static com.mongodb.ClusterFixture.serverVersionAtLeast;
 import static java.lang.String.format;
 import static java.util.Arrays.asList;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 import static org.junit.Assume.assumeTrue;
 
-// See https://github.com/mongodb/specifications/tree/master/source/change-streams/tests/README.rst#prose-tests
-public class WriteConcernProseTest extends DatabaseTestCase {
+/**
+ * See https://github.com/mongodb/specifications/blob/master/source/crud/tests/README.rst#prose-tests
+ */
+public class CrudProseTest extends DatabaseTestCase {
     private BsonDocument failPointDocument;
 
     @Before
     @Override
     public void setUp() {
-        assumeTrue(canRunTests());
         super.setUp();
     }
 
-    // Ensure that the WriteConcernError errInfo object is propagated.
+    /**
+     * 1. WriteConcernError.details exposes writeConcernError.errInfo
+     */
     @Test
     public void testWriteConcernErrInfoIsPropagated() {
+        assumeTrue(isDiscoverableReplicaSet() && serverVersionAtLeast(4, 0));
+
         try {
             setFailPoint();
             collection.insertOne(Document.parse("{ x: 1 }"));
         } catch (MongoWriteConcernException e) {
             assertEquals(e.getWriteConcernError().getCode(), 100);
-            assertTrue(e.getWriteConcernError().getCodeName().equals("UnsatisfiableWriteConcern"));
+            assertEquals("UnsatisfiableWriteConcern", e.getWriteConcernError().getCodeName());
             assertEquals(e.getWriteConcernError().getDetails(), new BsonDocument("writeConcern",
                     new BsonDocument("w", new BsonInt32(2))
                             .append("wtimeout", new BsonInt32(0))
                             .append("provenance", new BsonString("clientSupplied"))));
         } catch (Exception ex) {
-            fail(format("Incorrect exception thrown in test: %s" + ex.getClass()));
+            fail(format("Incorrect exception thrown in test: %s", ex.getClass()));
         } finally {
             disableFailPoint();
+        }
+    }
+
+    /**
+     * 2. WriteError.details exposes writeErrors[].errInfo
+     */
+    @Test
+    public void testWriteErrorDetailsIsPropagated() {
+        assumeTrue(serverVersionAtLeast(3, 2));
+
+        getCollectionHelper().create(getCollectionName(),
+                new CreateCollectionOptions()
+                        .validationOptions(new ValidationOptions()
+                                .validator(Filters.type("x", "string"))));
+
+        try {
+            collection.insertOne(new Document("x", 1));
+            fail("Should throw, as document doesn't match schema");
+        } catch (MongoWriteException e) {
+            // These assertions doesn't do exactly what's required by the specification, but it's simpler to implement and nearly as
+            // effective
+            assertTrue(e.getMessage().contains("Write error"));
+            assertNotNull(e.getError().getDetails());
+            assertFalse(e.getError().getDetails().isEmpty());
+        }
+
+        try {
+            collection.insertMany(asList(new Document("x", 1)));
+            fail("Should throw, as document doesn't match schema");
+        } catch (MongoBulkWriteException e) {
+            // These assertions doesn't do exactly what's required by the specification, but it's simpler to implement and nearly as
+            // effective
+            assertTrue(e.getMessage().contains("Write errors"));
+            assertEquals(1, e.getWriteErrors().size());
+            assertFalse(e.getWriteErrors().get(0).getDetails().isEmpty());
         }
     }
 
@@ -80,9 +127,5 @@ public class WriteConcernProseTest extends DatabaseTestCase {
 
     private void disableFailPoint() {
         getCollectionHelper().runAdminCommand(failPointDocument.append("mode", new BsonString("off")));
-    }
-
-    private boolean canRunTests() {
-        return isDiscoverableReplicaSet() && serverVersionAtLeast(4, 0);
     }
 }
