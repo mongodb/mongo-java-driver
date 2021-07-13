032753b15a6c6d1560a88ab71b20ce4cedac7b19
Jeff Yemin <jeff.yemin@mongodb.com>
2021-07-12 20:59:31 -0400

    Remove MongoDB 2.6 support
    * Remove query-based support for list collections and indexes
    * Remove support for MONGDOB-CR as the default authenticator
    
    JAVA-4296

--- driver-core/src/main/com/mongodb/internal/operation/ListCollectionsOperation.java
+++ driver-core/src/main/com/mongodb/internal/operation/ListCollectionsOperation.java
@@ -18,47 +18,33 @@ package com.mongodb.internal.operation;
 
 import com.mongodb.MongoCommandException;
 import com.mongodb.MongoNamespace;
-import com.mongodb.ReadPreference;
-import com.mongodb.ServerAddress;
-import com.mongodb.ServerCursor;
 import com.mongodb.connection.ConnectionDescription;
 import com.mongodb.connection.ServerDescription;
 import com.mongodb.internal.async.AsyncBatchCursor;
 import com.mongodb.internal.async.SingleResultCallback;
+import com.mongodb.internal.async.function.AsyncCallbackSupplier;
+import com.mongodb.internal.async.function.RetryState;
 import com.mongodb.internal.binding.AsyncConnectionSource;
 import com.mongodb.internal.binding.AsyncReadBinding;
 import com.mongodb.internal.binding.ConnectionSource;
 import com.mongodb.internal.binding.ReadBinding;
 import com.mongodb.internal.connection.AsyncConnection;
 import com.mongodb.internal.connection.Connection;
-import com.mongodb.internal.connection.QueryResult;
-import com.mongodb.internal.async.function.AsyncCallbackSupplier;
 import com.mongodb.internal.operation.CommandOperationHelper.CommandReadTransformer;
 import com.mongodb.internal.operation.CommandOperationHelper.CommandReadTransformerAsync;
-import com.mongodb.internal.async.function.RetryState;
 import com.mongodb.lang.Nullable;
-import org.bson.BsonArray;
 import org.bson.BsonBoolean;
 import org.bson.BsonDocument;
-import org.bson.BsonDocumentReader;
 import org.bson.BsonInt32;
 import org.bson.BsonInt64;
-import org.bson.BsonRegularExpression;
-import org.bson.BsonString;
 import org.bson.BsonValue;
-import org.bson.codecs.BsonDocumentCodec;
 import org.bson.codecs.Codec;
 import org.bson.codecs.Decoder;
-import org.bson.codecs.DecoderContext;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.concurrent.TimeUnit;
 import java.util.function.Supplier;
 
-import static com.mongodb.ReadPreference.primary;
 import static com.mongodb.assertions.Assertions.notNull;
-import static com.mongodb.connection.ServerType.SHARD_ROUTER;
 import static com.mongodb.internal.async.ErrorHandlingResultCallback.errorHandlingCallback;
 import static com.mongodb.internal.operation.CommandOperationHelper.CommandCreator;
 import static com.mongodb.internal.operation.CommandOperationHelper.createReadCommandAndExecute;
@@ -78,9 +64,6 @@ import static com.mongodb.internal.operation.OperationHelper.cursorDocumentToAsy
 import static com.mongodb.internal.operation.OperationHelper.cursorDocumentToBatchCursor;
 import static com.mongodb.internal.operation.OperationHelper.withAsyncSourceAndConnection;
 import static com.mongodb.internal.operation.OperationHelper.withSourceAndConnection;
-import static com.mongodb.internal.operation.ServerVersionHelper.serverIsAtLeastVersionThreeDotZero;
-import static java.lang.String.format;
-import static java.util.Arrays.asList;
 
 /**
  * An operation that provides a cursor allowing iteration through the metadata of all the collections in a database.  This operation
@@ -264,20 +247,12 @@ public class ListCollectionsOperation<T> implements AsyncReadOperation<AsyncBatc
             return withSourceAndConnection(binding::getReadConnectionSource, false, (source, connection) -> {
                 retryState.breakAndThrowIfRetryAnd(() -> !canRetryRead(source.getServerDescription(), connection.getDescription(),
                         binding.getSessionContext()));
-                if (serverIsAtLeastVersionThreeDotZero(connection.getDescription())) {
-                    try {
-                        return createReadCommandAndExecute(retryState, binding, source, databaseName, getCommandCreator(),
-                                createCommandDecoder(), commandTransformer(), connection);
-                    } catch (MongoCommandException e) {
-                        return rethrowIfNotNamespaceError(e, createEmptyBatchCursor(createNamespace(), decoder,
-                                source.getServerDescription().getAddress(), batchSize));
-                    }
-                } else {
-                    retryState.markAsLastAttempt();
-                    return new ProjectingBatchCursor(new QueryBatchCursor<>(connection.query(getNamespace(),
-                            asQueryDocument(connection.getDescription(), binding.getReadPreference()), null, 0, 0, batchSize,
-                            binding.getReadPreference().isSecondaryOk(), false, false, false, false, false,
-                            new BsonDocumentCodec(), binding.getRequestContext()), 0, batchSize, new BsonDocumentCodec(), comment, source));
+                try {
+                    return createReadCommandAndExecute(retryState, binding, source, databaseName, getCommandCreator(),
+                            createCommandDecoder(), commandTransformer(), connection);
+                } catch (MongoCommandException e) {
+                    return rethrowIfNotNamespaceError(e, createEmptyBatchCursor(createNamespace(), decoder,
+                            source.getServerDescription().getAddress(), batchSize));
                 }
             });
         });
@@ -290,43 +265,23 @@ public class ListCollectionsOperation<T> implements AsyncReadOperation<AsyncBatc
         binding.retain();
         AsyncCallbackSupplier<AsyncBatchCursor<T>> asyncRead = CommandOperationHelper.<AsyncBatchCursor<T>>decorateReadWithRetries(
                 retryState, funcCallback -> {
-            logRetryExecute(retryState);
-            withAsyncSourceAndConnection(binding::getReadConnectionSource, false, funcCallback,
-                    (source, connection, releasingCallback) -> {
-                if (retryState.breakAndCompleteIfRetryAnd(() -> !canRetryRead(source.getServerDescription(), connection.getDescription(),
-                        binding.getSessionContext()), releasingCallback)) {
-                    return;
-                }
-                if (serverIsAtLeastVersionThreeDotZero(connection.getDescription())) {
-                    createReadCommandAndExecuteAsync(retryState, binding, source, databaseName, getCommandCreator(), createCommandDecoder(),
-                            asyncTransformer(), connection, (result, t) -> {
-                                if (t != null && !isNamespaceError(t)) {
-                                    releasingCallback.onResult(null, t);
-                                } else {
-                                    releasingCallback.onResult(result != null ? result : emptyAsyncCursor(source), null);
+                    logRetryExecute(retryState);
+                    withAsyncSourceAndConnection(binding::getReadConnectionSource, false, funcCallback,
+                            (source, connection, releasingCallback) -> {
+                                if (retryState.breakAndCompleteIfRetryAnd(() -> !canRetryRead(source.getServerDescription(), connection.getDescription(),
+                                        binding.getSessionContext()), releasingCallback)) {
+                                    return;
                                 }
-                    });
-                } else {
-                    retryState.markAsLastAttempt();
-                    connection.queryAsync(getNamespace(), asQueryDocument(connection.getDescription(), binding.getReadPreference()),
-                            null, 0, 0, batchSize, binding.getReadPreference().isSecondaryOk(), false, false, false, false, false,
-                                new BsonDocumentCodec(), binding.getRequestContext(),
-                                new SingleResultCallback<QueryResult<BsonDocument>>() {
-                                @Override
-                                public void onResult(final QueryResult<BsonDocument> result, final Throwable t) {
-                                    if (t != null) {
-                                        releasingCallback.onResult(null, t);
-                                    } else {
-                                        releasingCallback.onResult(new ProjectingAsyncBatchCursor(
-                                                new AsyncQueryBatchCursor<BsonDocument>(result, 0,
-                                                        batchSize, 0, new BsonDocumentCodec(), comment, source, connection)
-                                        ), null);
-                                    }
-                                }
+                                createReadCommandAndExecuteAsync(retryState, binding, source, databaseName, getCommandCreator(), createCommandDecoder(),
+                                        asyncTransformer(), connection, (result, t) -> {
+                                            if (t != null && !isNamespaceError(t)) {
+                                                releasingCallback.onResult(null, t);
+                                            } else {
+                                                releasingCallback.onResult(result != null ? result : emptyAsyncCursor(source), null);
+                                            }
+                                        });
                             });
-                }
-            });
-        }).whenComplete(binding::release);
+                }).whenComplete(binding::release);
         asyncRead.get(errorHandlingCallback(callback, LOGGER));
     }
 
@@ -357,10 +312,6 @@ public class ListCollectionsOperation<T> implements AsyncReadOperation<AsyncBatc
         };
     }
 
-    private MongoNamespace getNamespace() {
-        return new MongoNamespace(databaseName, "system.namespaces");
-    }
-
     private CommandCreator getCommandCreator() {
         return new CommandCreator() {
             @Override
@@ -386,156 +337,7 @@ public class ListCollectionsOperation<T> implements AsyncReadOperation<AsyncBatc
         return command;
     }
 
-    private BsonDocument asQueryDocument(final ConnectionDescription connectionDescription, final ReadPreference readPreference) {
-        BsonDocument document = new BsonDocument();
-        BsonDocument transformedFilter = null;
-        if (filter != null) {
-            if (filter.containsKey("name")) {
-                if (!filter.isString("name")) {
-                    throw new IllegalArgumentException("When filtering collections on MongoDB versions < 3.0 the name field "
-                                                       + "must be a string");
-                }
-                transformedFilter = new BsonDocument();
-                transformedFilter.putAll(filter);
-                transformedFilter.put("name", new BsonString(format("%s.%s", databaseName, filter.getString("name").getValue())));
-            } else {
-                transformedFilter = filter;
-            }
-        }
-        BsonDocument indexExcludingRegex = new BsonDocument("name", new BsonRegularExpression("^[^$]*$"));
-        BsonDocument query = transformedFilter == null ? indexExcludingRegex
-                                                       : new BsonDocument("$and", new BsonArray(asList(indexExcludingRegex,
-                                                                                                       transformedFilter)));
-
-
-        document.put("$query", query);
-        if (connectionDescription.getServerType() == SHARD_ROUTER && !readPreference.equals(primary())) {
-            document.put("$readPreference", readPreference.toDocument());
-        }
-        if (maxTimeMS > 0) {
-            document.put("$maxTimeMS", new BsonInt64(maxTimeMS));
-        }
-        return document;
-    }
-
     private Codec<BsonDocument> createCommandDecoder() {
         return CommandResultDocumentCodec.create(decoder, "firstBatch");
-    }
-
-    private final class ProjectingBatchCursor implements BatchCursor<T> {
-
-        private final BatchCursor<BsonDocument> delegate;
-
-        private ProjectingBatchCursor(final BatchCursor<BsonDocument> delegate) {
-            this.delegate = delegate;
-        }
-
-        @Override
-        public void remove() {
-            delegate.remove();
-        }
-
-        @Override
-        public void close() {
-            delegate.close();
-        }
-
-        @Override
-        public boolean hasNext() {
-            return delegate.hasNext();
-        }
-
-        @Override
-        public List<T> next() {
-            return projectFromFullNamespaceToCollectionName(delegate.next());
-        }
-
-        @Override
-        public int available() {
-            return delegate.available();
-        }
-
-        @Override
-        public void setBatchSize(final int batchSize) {
-            delegate.setBatchSize(batchSize);
-        }
-
-        @Override
-        public int getBatchSize() {
-            return delegate.getBatchSize();
-        }
-
-        @Override
-        public List<T> tryNext() {
-           return projectFromFullNamespaceToCollectionName(delegate.tryNext());
-        }
-
-        @Override
-        public ServerCursor getServerCursor() {
-            return delegate.getServerCursor();
-        }
-
-        @Override
-        public ServerAddress getServerAddress() {
-            return delegate.getServerAddress();
-        }
-    }
-
-    private final class ProjectingAsyncBatchCursor implements AsyncBatchCursor<T> {
-
-        private final AsyncBatchCursor<BsonDocument> delegate;
-
-        private ProjectingAsyncBatchCursor(final AsyncBatchCursor<BsonDocument> delegate) {
-            this.delegate = delegate;
-        }
-
-        @Override
-        public void next(final SingleResultCallback<List<T>> callback) {
-            delegate.next(new SingleResultCallback<List<BsonDocument>>() {
-                @Override
-                public void onResult(final List<BsonDocument> result, final Throwable t) {
-                    if (t != null) {
-                        callback.onResult(null, t);
-                    } else {
-                        callback.onResult(projectFromFullNamespaceToCollectionName(result), null);
-                    }
-                }
-            });
-        }
-
-        @Override
-        public void setBatchSize(final int batchSize) {
-            delegate.setBatchSize(batchSize);
-        }
-
-        @Override
-        public int getBatchSize() {
-            return delegate.getBatchSize();
-        }
-
-        @Override
-        public boolean isClosed() {
-            return delegate.isClosed();
-        }
-
-        @Override
-        public void close() {
-            delegate.close();
-        }
-    }
-
-    private List<T> projectFromFullNamespaceToCollectionName(final List<BsonDocument> unstripped) {
-        if (unstripped == null) {
-            return null;
-        }
-        List<T> stripped = new ArrayList<T>(unstripped.size());
-        String prefix = databaseName + ".";
-        for (BsonDocument cur : unstripped) {
-            String name = cur.getString("name").getValue();
-            String collectionName = name.substring(prefix.length());
-            cur.put("name", new BsonString(collectionName));
-            stripped.add(decoder.decode(new BsonDocumentReader(cur), DecoderContext.builder().build()));
-        }
-        return stripped;
     }
 }
--- driver-core/src/main/com/mongodb/internal/operation/ServerVersionHelper.java
+++ driver-core/src/main/com/mongodb/internal/operation/ServerVersionHelper.java
@@ -25,7 +25,6 @@ import com.mongodb.connection.ConnectionDescription;
 public final class ServerVersionHelper {
 
     public static final int MIN_WIRE_VERSION = 0;
-    public static final int THREE_DOT_ZERO_WIRE_VERSION = 3;
     public static final int THREE_DOT_TWO_WIRE_VERSION = 4;
     public static final int THREE_DOT_FOUR_WIRE_VERSION = 5;
     public static final int THREE_DOT_SIX_WIRE_VERSION = 6;
@@ -33,10 +32,6 @@ public final class ServerVersionHelper {
     public static final int FOUR_DOT_TWO_WIRE_VERSION = 8;
     public static final int FOUR_DOT_FOUR_WIRE_VERSION = 9;
     public static final int FIVE_DOT_ZERO_WIRE_VERSION = 12;
-
-    public static boolean serverIsAtLeastVersionThreeDotZero(final ConnectionDescription description) {
-        return description.getMaxWireVersion() >= THREE_DOT_ZERO_WIRE_VERSION;
-    }
 
     public static boolean serverIsAtLeastVersionThreeDotTwo(final ConnectionDescription description) {
         return description.getMaxWireVersion() >= THREE_DOT_TWO_WIRE_VERSION;
@@ -64,10 +59,6 @@ public final class ServerVersionHelper {
 
     public static boolean serverIsAtLeastVersionFiveDotZero(final ConnectionDescription description) {
         return description.getMaxWireVersion() >= FIVE_DOT_ZERO_WIRE_VERSION;
-    }
-
-    public static boolean serverIsLessThanVersionThreeDotZero(final ConnectionDescription description) {
-        return description.getMaxWireVersion() < THREE_DOT_ZERO_WIRE_VERSION;
     }
 
     public static boolean serverIsLessThanVersionThreeDotTwo(final ConnectionDescription description) {
