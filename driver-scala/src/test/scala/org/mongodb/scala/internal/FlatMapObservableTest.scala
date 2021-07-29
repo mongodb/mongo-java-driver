e5e2ff243f55c04e6307b57f2ae61aa1fe06c3eb
Ross Lawley <ross.lawley@gmail.com>
2021-07-29 15:24:57 +0100

    Fix FlatMapObservableTest
    Add header and ensure can run in Scala 2.11
    
    JAVA-4241

--- driver-scala/src/test/scala/org/mongodb/scala/internal/FlatMapObservableTest.scala
+++ driver-scala/src/test/scala/org/mongodb/scala/internal/FlatMapObservableTest.scala
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2008-present MongoDB, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.mongodb.scala.internal
 
 import org.mongodb.scala.{ BaseSpec, Observable, Observer }
@@ -13,17 +29,11 @@ class FlatMapObservableTest extends BaseSpec with Futures with Eventually {
     val completedCounter = new AtomicInteger(0)
     Observable(1 to 100)
       .flatMap(
-        x =>
-          (observer: Observer[_ >: Int]) => {
-            Future(()).onComplete(_ => {
-              observer.onNext(x)
-              observer.onComplete()
-            })
-          }
+        x => createObservable(x)
       )
       .subscribe(
         _ => (),
-        p.failure,
+        e => p.failure(e),
         () => {
           completedCounter.incrementAndGet()
           Thread.sleep(100)
@@ -34,5 +44,14 @@ class FlatMapObservableTest extends BaseSpec with Futures with Eventually {
     Thread.sleep(200)
     assert(completedCounter.get() == 1, s"${completedCounter.get()}")
     Thread.sleep(1000)
+  }
+
+  private def createObservable(x: Int): Observable[Int] = new Observable[Int] {
+    override def subscribe(observer: Observer[_ >: Int]): Unit = {
+      Future(()).onComplete(_ => {
+        observer.onNext(x)
+        observer.onComplete()
+      })
+    }
   }
 }
