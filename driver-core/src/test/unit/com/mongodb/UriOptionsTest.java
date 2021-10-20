80bda455c0b4abf0e7fdcdddaaaeb0b2e54ec997
Jeff Yemin <jeff.yemin@mongodb.com>
2021-10-20 14:27:32 -0400

    spec test updates

--- driver-core/src/test/unit/com/mongodb/UriOptionsTest.java
+++ driver-core/src/test/unit/com/mongodb/UriOptionsTest.java
@@ -40,8 +40,7 @@ public class UriOptionsTest extends AbstractConnectionStringTest {
 
     @Test
     public void shouldPassAllOutcomes() {
-        assumeFalse(getDefinition().getBoolean("warning", BsonBoolean.FALSE).getValue()
-                && getDefinition().getBoolean("valid").getValue());
+        assumeFalse(getDefinition().getBoolean("warning", BsonBoolean.FALSE).getValue());
         assumeFalse(getDescription().equals("Arbitrary string readConcernLevel does not cause a warning"));
         // Skip because Java driver does not support the tlsAllowInvalidCertificates option
         assumeFalse(getDescription().startsWith("tlsInsecure and tlsAllowInvalidCertificates both present"));
