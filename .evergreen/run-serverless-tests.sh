94551c74e952af7ce3bb90cfcda33b327a9e65cb
Ross Lawley <ross.lawley@gmail.com>
2021-11-09 09:54:26 +0000

    Evergreen: Fix sourcing javaConfig.bash for serverless
    JAVA-4357

--- .evergreen/run-serverless-tests.sh
+++ .evergreen/run-serverless-tests.sh
@@ -15,6 +15,7 @@ set -o errexit  # Exit the script with error if any of the commands fail
 #            Main Program                  #
 ############################################
 RELATIVE_DIR_PATH="$(dirname "${BASH_SOURCE[0]:-$0}")"
+. "${RELATIVE_DIR_PATH}/javaConfig.bash"
 
 echo "Running serverless tests with Java ${JAVA_VERSION}"
 
