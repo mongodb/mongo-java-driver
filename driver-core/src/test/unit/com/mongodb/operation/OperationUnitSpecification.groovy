86245067d7bddb30922762c54b0098019ea215e6
jyemin <jeff.yemin@mongodb.com>
2022-01-13 10:34:47 -0500

    Bump maximum supported wire version to 15
    This is the maxWireVersion reported by MongoDB 5.2.
    
    JAVA-4441

--- driver-core/src/test/unit/com/mongodb/operation/OperationUnitSpecification.groovy
+++ driver-core/src/test/unit/com/mongodb/operation/OperationUnitSpecification.groovy
@@ -54,7 +54,8 @@ class OperationUnitSpecification extends Specification {
             [4, 2]: 8,
             [4, 4]: 9,
             [5, 0]: 13,
-            [5, 1]: 14
+            [5, 1]: 14,
+            [5, 2]: 15,
     ]
 
     static int getMaxWireVersionForServerVersion(List<Integer> serverVersion) {
