89562a705f5cd401d9e7f67ed11f8158f9895351
Jeff Yemin <jeff.yemin@mongodb.com>
2022-02-10 12:55:47 -0500

    imports...

--- driver-core/src/main/com/mongodb/internal/connection/DefaultServerConnection.java
+++ driver-core/src/main/com/mongodb/internal/connection/DefaultServerConnection.java
@@ -37,7 +37,6 @@ import org.bson.codecs.Decoder;
 
 import java.util.List;
 
-import static com.mongodb.assertions.Assertions.isTrue;
 import static com.mongodb.connection.ServerType.SHARD_ROUTER;
 import static com.mongodb.internal.async.ErrorHandlingResultCallback.errorHandlingCallback;
 
