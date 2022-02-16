26d9fbc29738b954a6e5a0a5cde42d51319cdd6d
Jeff Yemin <jeff.yemin@mongodb.com>
2022-02-16 15:53:21 -0500

    Refactor AwsAuthenticator to extract code that obtains credentials from the environment
    JAVA-4501

--- driver-core/src/main/com/mongodb/internal/connection/AwsAuthenticator.java
+++ driver-core/src/main/com/mongodb/internal/connection/AwsAuthenticator.java
@@ -17,14 +17,12 @@
 package com.mongodb.internal.connection;
 
 import com.mongodb.AuthenticationMechanism;
-import com.mongodb.AwsCredential;
 import com.mongodb.MongoCredential;
 import com.mongodb.MongoException;
-import com.mongodb.MongoInternalException;
 import com.mongodb.ServerAddress;
 import com.mongodb.ServerApi;
 import com.mongodb.connection.ClusterConnectionMode;
-import com.mongodb.lang.NonNull;
+import com.mongodb.internal.authentication.AwsInternalCredential;
 import com.mongodb.lang.Nullable;
 import org.bson.BsonBinary;
 import org.bson.BsonBinaryWriter;
@@ -38,26 +36,15 @@ import org.bson.io.BasicOutputBuffer;
 
 import javax.security.sasl.SaslClient;
 import javax.security.sasl.SaslException;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.nio.charset.StandardCharsets;
 import java.security.SecureRandom;
 import java.time.Instant;
 import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
 import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.function.Supplier;
 
 import static com.mongodb.AuthenticationMechanism.MONGODB_AWS;
 import static com.mongodb.MongoCredential.AWS_CREDENTIAL_PROVIDER_KEY;
 import static com.mongodb.MongoCredential.AWS_SESSION_TOKEN_KEY;
-import static com.mongodb.assertions.Assertions.assertNotNull;
-import static com.mongodb.assertions.Assertions.isTrueArgument;
 import static java.lang.String.format;
 
 public class AwsAuthenticator extends SaslAuthenticator {
@@ -85,24 +72,11 @@ public class AwsAuthenticator extends SaslAuthenticator {
 
     private static class AwsSaslClient implements SaslClient {
         private final MongoCredential credential;
-        @Nullable
-        private final AwsCredential credentialFromSupplier;
         private final byte[] clientNonce = new byte[RANDOM_LENGTH];
         private int step = -1;
-        private String httpResponse;
 
         AwsSaslClient(final MongoCredential credential) {
             this.credential = credential;
-
-            Supplier<AwsCredential> awsCredentialSupplier = credential.getMechanismProperty(AWS_CREDENTIAL_PROVIDER_KEY, null);
-            if (awsCredentialSupplier == null) {
-                credentialFromSupplier = null;
-            } else {
-                credentialFromSupplier = assertNotNull(awsCredentialSupplier.get());
-                isTrueArgument("credential userName is null", credential.getUserName() == null);
-                isTrueArgument("credential password is null", credential.getPassword() == null);
-                isTrueArgument("credential session token is null", credential.getMechanismProperty(AWS_SESSION_TOKEN_KEY, null) == null);
-            }
         }
 
         @Override
@@ -181,11 +155,16 @@ public class AwsAuthenticator extends SaslAuthenticator {
                     .withZone(ZoneId.of("UTC"))
                     .format(Instant.now());
 
-            String token = getSessionToken();
-            final AuthorizationHeader authorizationHeader = AuthorizationHeader.builder()
-                    .setAccessKeyID(getUserName())
-                    .setSecretKey(getPassword())
-                    .setSessionToken(token)
+            AwsInternalCredential awsInternalCredential = AwsInternalCredential.obtainFromEnvironment(new AwsInternalCredential(
+                                credential.getUserName(), getSecretAccessKey(),
+                                credential.getMechanismProperty(AWS_SESSION_TOKEN_KEY, null)),
+                                credential.getMechanismProperty(AWS_CREDENTIAL_PROVIDER_KEY, null));
+
+            String sessionToken = awsInternalCredential.getSessionToken();
+            AuthorizationHeader authorizationHeader = AuthorizationHeader.builder()
+                    .setAccessKeyID(awsInternalCredential.getAccessKeyId())
+                    .setSecretKey(awsInternalCredential.getSecretAccessKey())
+                    .setSessionToken(sessionToken)
                     .setHost(host)
                     .setNonce(serverNonce)
                     .setTimestamp(timestamp)
@@ -194,13 +173,17 @@ public class AwsAuthenticator extends SaslAuthenticator {
             BsonDocument ret = new BsonDocument()
                     .append("a", new BsonString(authorizationHeader.toString()))
                     .append("d", new BsonString(authorizationHeader.getTimestamp()));
-            if (token != null) {
-                ret.append("t", new BsonString(token));
+            if (sessionToken != null) {
+                ret.append("t", new BsonString(sessionToken));
             }
 
             return toBson(ret);
         }
 
+        private String getSecretAccessKey() {
+            char[] password = credential.getPassword();
+            return password == null ? null : new String(password);
+        }
 
         private byte[] toBson(final BsonDocument document) {
             byte[] bytes;
@@ -210,135 +193,5 @@ public class AwsAuthenticator extends SaslAuthenticator {
             System.arraycopy(buffer.getInternalBuffer(), 0, bytes, 0, buffer.getSize());
             return bytes;
         }
-
-        @NonNull
-        String getUserName() {
-            if (credentialFromSupplier != null) {
-                return credentialFromSupplier.getAccessKeyId();
-            }
-            String userName = credential.getUserName();
-            if (userName == null) {
-                userName = System.getenv("AWS_ACCESS_KEY_ID");
-                if (userName == null) {
-                    userName = BsonDocument
-                            .parse(getHttpResponse())
-                            .getString("AccessKeyId")
-                            .getValue();
-                }
-            }
-            return userName;
-        }
-
-        @NonNull
-        private String getPassword() {
-            if (credentialFromSupplier != null) {
-                return credentialFromSupplier.getSecretAccessKey();
-            }
-            char[] password = credential.getPassword();
-            if (password == null) {
-                if (System.getenv("AWS_SECRET_ACCESS_KEY") != null) {
-                    password = System.getenv("AWS_SECRET_ACCESS_KEY").toCharArray();
-                } else {
-                    password = BsonDocument
-                            .parse(getHttpResponse())
-                            .getString("SecretAccessKey")
-                            .getValue()
-                            .toCharArray();
-                }
-            }
-            return new String(password);
-        }
-
-        @Nullable
-        private String getSessionToken() {
-            if (credentialFromSupplier != null) {
-                return credentialFromSupplier.getSessionToken();
-            }
-            String token = credential.getMechanismProperty(AWS_SESSION_TOKEN_KEY, null);
-            if (credential.getUserName() != null) {
-                return token;
-            }
-            if (token != null) {
-                throw new IllegalArgumentException("The connection string contains a session token without credentials");
-            }
-
-            if ((System.getenv("AWS_SECRET_ACCESS_KEY") != null) || (System.getenv("AWS_ACCESS_KEY_ID") != null)
-                    || (System.getenv("AWS_SESSION_TOKEN") != null)) {
-                if (System.getenv("AWS_SECRET_ACCESS_KEY") == null || System.getenv("AWS_ACCESS_KEY_ID") == null) {
-                    throw new IllegalArgumentException("The environment variables 'AWS_ACCESS_KEY_ID' and 'AWS_SECRET_ACCESS_KEY' must "
-                            + "either both be set or both be null");
-                }
-                return System.getenv("AWS_SESSION_TOKEN");
-            }
-
-            return BsonDocument
-                    .parse(getHttpResponse())
-                    .getString("Token")
-                    .getValue();
-        }
-
-
-        @NonNull
-        private String getHttpResponse() {
-            if (httpResponse != null) {
-                return httpResponse;
-            }
-
-            String path = System.getenv("AWS_CONTAINER_CREDENTIALS_RELATIVE_URI");
-            httpResponse = (path == null)
-                    ?  getEc2Response()
-                    :  getHttpContents("GET", "http://169.254.170.2" + path, null);
-            return httpResponse;
-        }
-
-        private String getEc2Response() {
-            final String endpoint = "http://169.254.169.254";
-            final String path = "/latest/meta-data/iam/security-credentials/";
-
-            Map<String, String> header = new HashMap<>();
-            header.put("X-aws-ec2-metadata-token-ttl-seconds", "30");
-            String token = getHttpContents("PUT", endpoint + "/latest/api/token", header);
-
-            header.clear();
-            header.put("X-aws-ec2-metadata-token", token);
-            String role = getHttpContents("GET", endpoint + path, header);
-            return getHttpContents("GET", endpoint + path + role, header);
-        }
-
-        @NonNull
-        private static String getHttpContents(final String method, final String endpoint, final Map<String, String> headers) {
-            StringBuilder content = new StringBuilder();
-            HttpURLConnection conn = null;
-            try {
-                conn = (HttpURLConnection) new URL(endpoint).openConnection();
-                conn.setRequestMethod(method);
-                conn.setReadTimeout(10000);
-                if (headers != null) {
-                    for (Map.Entry<String, String> kvp : headers.entrySet()) {
-                       conn.setRequestProperty(kvp.getKey(), kvp.getValue());
-                    }
-                }
-
-                int status = conn.getResponseCode();
-                if (status != HttpURLConnection.HTTP_OK) {
-                    throw new IOException(String.format("%d %s", status, conn.getResponseMessage()));
-                }
-
-                try (BufferedReader in = new BufferedReader(new InputStreamReader(conn.getInputStream(), StandardCharsets.UTF_8))) {
-                    String inputLine;
-                    while ((inputLine = in.readLine()) != null) {
-                        content.append(inputLine);
-                    }
-                }
-            } catch (IOException e) {
-                throw new MongoInternalException("Unexpected IOException", e);
-            } finally {
-                if (conn != null) {
-                    conn.disconnect();
-                }
-            }
-            return content.toString();
-        }
     }
-
 }
