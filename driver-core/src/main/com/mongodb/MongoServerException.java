497282555539832957e11ee1ff05f2ed0d906556
Jeff Yemin <jeff.yemin@mongodb.com>
2022-04-07 17:00:41 -0400

    Add errorCodeName property to MongoServerException
    Also, ensure that MongoQueryException pushes the property down through the constructor
    
    JAVA-4565

--- driver-core/src/main/com/mongodb/MongoServerException.java
+++ driver-core/src/main/com/mongodb/MongoServerException.java
@@ -16,6 +16,8 @@
 
 package com.mongodb;
 
+import com.mongodb.lang.Nullable;
+
 /**
  * An exception indicating that some error has been raised by a MongoDB server in response to an operation.
  *
@@ -24,6 +26,8 @@ package com.mongodb;
  */
 public abstract class MongoServerException extends MongoException {
     private static final long serialVersionUID = -5213859742051776206L;
+    @Nullable
+    private final String errorCodeName;
     private final ServerAddress serverAddress;
 
     /**
@@ -35,6 +39,7 @@ public abstract class MongoServerException extends MongoException {
     public MongoServerException(final String message, final ServerAddress serverAddress) {
         super(message);
         this.serverAddress = serverAddress;
+        this.errorCodeName = null;
     }
 
     /**
@@ -47,6 +52,23 @@ public abstract class MongoServerException extends MongoException {
     public MongoServerException(final int code, final String message, final ServerAddress serverAddress) {
         super(code, message);
         this.serverAddress = serverAddress;
+        this.errorCodeName = null;
+    }
+
+    /**
+     * Construct a new instance.
+     *
+     * @param code the error code from the server
+     * @param errorCodeName the error code name from the server
+     * @param message the message from the server
+     * @param serverAddress the address of the server
+     * @since 4.6
+     */
+    public MongoServerException(final int code, final @Nullable String errorCodeName, final String message,
+            final ServerAddress serverAddress) {
+        super(code, message);
+        this.errorCodeName = errorCodeName;
+        this.serverAddress = serverAddress;
     }
 
     /**
@@ -56,5 +78,17 @@ public abstract class MongoServerException extends MongoException {
      */
     public ServerAddress getServerAddress() {
         return serverAddress;
+    }
+
+    /**
+     * Get the error code name, which may be null
+     *
+     * @return the error code nam
+     * @mongodb.server.release 3.4
+     * @since 4.6
+     */
+    @Nullable
+    public String getErrorCodeName() {
+        return errorCodeName;
     }
 }
