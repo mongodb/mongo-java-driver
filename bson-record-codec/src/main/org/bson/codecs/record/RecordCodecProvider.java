c469d4f997432bd847cf8fc1b17bfe7cd3da44eb
Ross Lawley <ross.lawley@gmail.com>
2022-04-08 09:33:06 +0100

    Support codec for Java records
    JAVA-3567

---
+++ bson-record-codec/src/main/org/bson/codecs/record/RecordCodecProvider.java
@@ -0,0 +1,39 @@
+/*
+ * Copyright 2008-present MongoDB, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.bson.codecs.record;
+
+import org.bson.codecs.Codec;
+import org.bson.codecs.configuration.CodecProvider;
+import org.bson.codecs.configuration.CodecRegistry;
+
+/**
+ * Provides Codec instances for Java records.
+ *
+ * @since 4.XXXXX
+ * @see Record
+ */
+public final class RecordCodecProvider implements CodecProvider {
+    @SuppressWarnings({"unchecked", "rawtypes"})
+    @Override
+    public <T> Codec<T> get(final Class<T> clazz, final CodecRegistry registry) {
+        if (!clazz.isRecord()) {
+            return null;
+        }
+
+        return (Codec<T>) new RecordCodec(clazz, registry);
+    }
+}
---
+++ bson-record-codec/src/test/unit/org/bson/codecs/record/RecordCodecTest.java
@@ -0,0 +1,108 @@
+/*
+ * Copyright 2008-present MongoDB, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.bson.codecs.record;
+
+import org.bson.BsonArray;
+import org.bson.BsonDocument;
+import org.bson.BsonDocumentReader;
+import org.bson.BsonDocumentWriter;
+import org.bson.BsonInt32;
+import org.bson.BsonObjectId;
+import org.bson.BsonString;
+import org.bson.codecs.DecoderContext;
+import org.bson.codecs.EncoderContext;
+import org.bson.codecs.record.samples.TestRecord;
+import org.bson.conversions.Bson;
+import org.bson.types.ObjectId;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class RecordCodecTest {
+    private final RecordCodec<TestRecord> codec = new RecordCodec<>(TestRecord.class, Bson.DEFAULT_CODEC_REGISTRY);
+
+    @Test
+    public void testSimpleRecord() {
+        var identifier = new ObjectId();
+        var testRecord = new TestRecord("Lucas", 14, List.of("soccer", "basketball"), identifier.toHexString());
+
+        var document = new BsonDocument();
+        var writer = new BsonDocumentWriter(document);
+
+        // when
+        codec.encode(writer, testRecord, EncoderContext.builder().build());
+
+        // then
+        assertEquals(
+                new BsonDocument("_id", new BsonObjectId(identifier))
+                        .append("name", new BsonString("Lucas"))
+                        .append("hobbies", new BsonArray(List.of(new BsonString("soccer"), new BsonString("basketball"))))
+                        .append("a", new BsonInt32(14)),
+                document);
+        assertEquals("_id", document.getFirstKey());
+
+        // when
+        var decoded = codec.decode(new BsonDocumentReader(document), DecoderContext.builder().build());
+
+        // then
+        assertEquals(testRecord, decoded);
+    }
+
+    @Test
+    public void testSimpleRecordWithNulls() {
+        var identifier = new ObjectId();
+        var testRecord = new TestRecord(null, 14, null, identifier.toHexString());
+
+        var document = new BsonDocument();
+        var writer = new BsonDocumentWriter(document);
+
+        // when
+        codec.encode(writer, testRecord, EncoderContext.builder().build());
+
+        // then
+        assertEquals(
+                new BsonDocument("_id", new BsonObjectId(identifier))
+                        .append("a", new BsonInt32(14)),
+                document);
+
+        // when
+        var decoded = codec.decode(new BsonDocumentReader(document), DecoderContext.builder().build());
+
+        // then
+        assertEquals(testRecord, decoded);
+    }
+
+    @Test
+    public void testSimpleRecordWithExtraData() {
+        var identifier = new ObjectId();
+        var testRecord = new TestRecord("Felix", 13, List.of("rugby", "badminton"), identifier.toHexString());
+
+        var document = new BsonDocument("_id", new BsonObjectId(identifier))
+                .append("nationality", new BsonString("British"))
+                .append("name", new BsonString("Felix"))
+                .append("hobbies", new BsonArray(List.of(new BsonString("rugby"), new BsonString("badminton"))))
+                .append("a", new BsonInt32(13));
+
+        // when
+        var decoded = codec.decode(new BsonDocumentReader(document), DecoderContext.builder().build());
+
+        // then
+        assertEquals(testRecord, decoded);
+    }
+}
