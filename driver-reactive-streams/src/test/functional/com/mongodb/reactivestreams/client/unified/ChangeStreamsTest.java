c54aa76c0743aa03bcbb91e92a476581b092417a
jyemin <jeff.yemin@mongodb.com>
2022-04-09 17:56:40 -0400

    Fix or skip new failing unified change stream tests
    * Add support for ignoreExtraEvents in unified test runner
    * Update change stream tests to specify ignoreExtraEvents
    * Update reactive change stream test runner to skip tests
      where extra getMore events occur between required events
    * Update reactive change stream test runner to skip tests
      that require change stream initialization to report an error
    * Enable artificial delay for change stream tests that expect
      an event when change stream is initialized
    
    JAVA-4369

--- driver-reactive-streams/src/test/functional/com/mongodb/reactivestreams/client/unified/ChangeStreamsTest.java
+++ driver-reactive-streams/src/test/functional/com/mongodb/reactivestreams/client/unified/ChangeStreamsTest.java
@@ -19,18 +19,59 @@ package com.mongodb.reactivestreams.client.unified;
 import com.mongodb.lang.Nullable;
 import org.bson.BsonArray;
 import org.bson.BsonDocument;
+import org.junit.After;
 import org.junit.runners.Parameterized;
 
 import java.io.IOException;
 import java.net.URISyntaxException;
+import java.util.Arrays;
 import java.util.Collection;
+import java.util.List;
+
+import static com.mongodb.reactivestreams.client.syncadapter.SyncMongoClient.disableSleep;
+import static com.mongodb.reactivestreams.client.syncadapter.SyncMongoClient.enableSleepAfterCursorOpen;
+import static org.junit.Assume.assumeFalse;
 
 public final class ChangeStreamsTest extends UnifiedReactiveStreamsTest {
+
+    @SuppressWarnings("ArraysAsListWithZeroOrOneArgument")
+    private static final List<String> ERROR_REQUIRED_FROM_CHANGE_STREAM_INITIALIZATION_TESTS =
+            Arrays.asList(
+                    "Test with document comment - pre 4.4"
+            );
+
+    private static final List<String> EVENT_SENSITIVE_TESTS =
+            Arrays.asList(
+                    "Test that comment is set on getMore",
+                    "Test that comment is not set on getMore - pre 4.4"
+            );
+
+    private static final List<String> CURSOR_OPEN_TIMING_SENSITIVE_TESTS =
+            Arrays.asList(
+                    "Test with document comment",
+                    "Test with document comment - pre 4.4",
+                    "Test with string comment",
+                    "Test that comment is set on getMore"
+            );
+
     public ChangeStreamsTest(@SuppressWarnings("unused") final String fileDescription,
                              @SuppressWarnings("unused") final String testDescription,
                              final String schemaVersion, @Nullable final BsonArray runOnRequirements, final BsonArray entities,
                              final BsonArray initialData, final BsonDocument definition) {
         super(schemaVersion, runOnRequirements, entities, initialData, definition);
+
+        assumeFalse(ERROR_REQUIRED_FROM_CHANGE_STREAM_INITIALIZATION_TESTS.contains(testDescription));
+        assumeFalse(EVENT_SENSITIVE_TESTS.contains(testDescription));
+
+        if (CURSOR_OPEN_TIMING_SENSITIVE_TESTS.contains(testDescription)) {
+            enableSleepAfterCursorOpen(256);
+        }
+    }
+
+    @After
+    public void cleanUp() {
+        super.cleanUp();
+        disableSleep();
     }
 
     @Parameterized.Parameters(name = "{0}: {1}")
