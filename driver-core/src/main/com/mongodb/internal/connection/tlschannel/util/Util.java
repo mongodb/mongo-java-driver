cd77a4fe7ed729a4bbe477e16c412ee8d2621d63
Valentin Kovalenko <valentin.kovalenko@mongodb.com>
2022-06-22 01:21:37 -0600

    Fix `tlschannel.util.Util.getJavaMajorVersion`
    This is a manual cherry-pick and squash of the following commits:
    - https://github.com/marianobarrios/tls-channel/commit/3b5f8a0bb592dd918b6641d771fc83d9a5c3d1ca
    - https://github.com/marianobarrios/tls-channel/commit/f4b780e10c42c8a49e39f92d43fcb0179131124d
    
    JAVA-4656

--- driver-core/src/main/com/mongodb/internal/connection/tlschannel/util/Util.java
+++ driver-core/src/main/com/mongodb/internal/connection/tlschannel/util/Util.java
@@ -46,13 +46,16 @@ public class Util {
   public static int getJavaMajorVersion() {
     String version = System.getProperty("java.version");
     if (version.startsWith("1.")) {
-      version = version.substring(2, 3);
-    } else {
-      int dot = version.indexOf(".");
-      if (dot != -1) {
-        version = version.substring(0, dot);
-      }
+      version = version.substring(2);
     }
-    return Integer.parseInt(version);
+    // Allow these formats:
+    // 1.8.0_72-ea
+    // 9-ea
+    // 9
+    // 9.0.1
+    int dotPos = version.indexOf('.');
+    int dashPos = version.indexOf('-');
+    return Integer.parseInt(
+        version.substring(0, dotPos > -1 ? dotPos : dashPos > -1 ? dashPos : 1));
   }
 }
