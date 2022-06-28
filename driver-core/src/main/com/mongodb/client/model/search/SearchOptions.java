559e23fc0935aeb03cef73a6666d22a07c03756a
Valentin Kovalenko <valentin.kovalenko@mongodb.com>
2022-06-27 20:02:31 -0600

    Add missing semicolons in code examples

--- driver-core/src/main/com/mongodb/client/model/search/SearchOptions.java
+++ driver-core/src/main/com/mongodb/client/model/search/SearchOptions.java
@@ -75,8 +75,8 @@ public interface SearchOptions extends Bson {
      * The following code creates two functionally equivalent {@link SearchOptions} objects,
      * though they may not be {@linkplain Object#equals(Object) equal}.
      * <pre>{@code
-     *  SearchOptions options1 = SearchOptions.defaultSearchOptions().index("indexName")
-     *  SearchOptions options2 = SearchOptions.defaultSearchOptions().option("index", "indexName")
+     *  SearchOptions options1 = SearchOptions.defaultSearchOptions().index("indexName");
+     *  SearchOptions options2 = SearchOptions.defaultSearchOptions().option("index", "indexName");
      * }</pre>
      *
      * @param name The option name.
