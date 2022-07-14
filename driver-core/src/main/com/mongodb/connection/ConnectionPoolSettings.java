d696713d8f5167415738027336df25e33842a463
Divyansh <divyansh.24888@gmail.com>
2022-07-14 12:40:05 +0530

    Removing white space

--- driver-core/src/main/com/mongodb/connection/ConnectionPoolSettings.java
+++ driver-core/src/main/com/mongodb/connection/ConnectionPoolSettings.java
@@ -494,18 +494,18 @@ public class ConnectionPoolSettings {
     @Override
     public String toString() {
         return "ConnectionPoolSettings{"
-                + "maxSize=" + maxSize
-                + ", minSize=" + minSize
-                + ", incrementSize=" + incrementSize
-                + ", incrementType='" + incrementType
-                + ", maxWaitQueueSize=" + maxWaitQueueSize
-                + ", maxWaitTimeMS=" + maxWaitTimeMS
-                + ", maxConnectionLifeTimeMS=" + maxConnectionLifeTimeMS
-                + ", maxConnectionIdleTimeMS=" + maxConnectionIdleTimeMS
-                + ", maintenanceInitialDelayMS=" + maintenanceInitialDelayMS
-                + ", maintenanceFrequencyMS=" + maintenanceFrequencyMS
-                + ", connectionPoolListeners=" + connectionPoolListeners
-                + '}';
+               + "maxSize=" + maxSize
+               + ", minSize=" + minSize
+               + ", incrementSize=" + incrementSize
+               + ", incrementType='" + incrementType
+               + ", maxWaitQueueSize=" + maxWaitQueueSize
+               + ", maxWaitTimeMS=" + maxWaitTimeMS
+               + ", maxConnectionLifeTimeMS=" + maxConnectionLifeTimeMS
+               + ", maxConnectionIdleTimeMS=" + maxConnectionIdleTimeMS
+               + ", maintenanceInitialDelayMS=" + maintenanceInitialDelayMS
+               + ", maintenanceFrequencyMS=" + maintenanceFrequencyMS
+               + ", connectionPoolListeners=" + connectionPoolListeners
+               + '}';
     }
 
     ConnectionPoolSettings(final Builder builder) {
