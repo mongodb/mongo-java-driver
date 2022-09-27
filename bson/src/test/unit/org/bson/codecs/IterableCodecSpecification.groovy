4b3af5693948ceaf027fc294df27b9089573d436
Jeff Yemin <jeff.yemin@mongodb.com>
2022-09-27 11:23:57 -0400

    Add unit tests

--- bson/src/test/unit/org/bson/codecs/IterableCodecSpecification.groovy
+++ bson/src/test/unit/org/bson/codecs/IterableCodecSpecification.groovy
@@ -16,12 +16,18 @@
 
 package org.bson.codecs
 
+import org.bson.BsonArray
+import org.bson.BsonDateTime
 import org.bson.BsonDocument
 import org.bson.BsonDocumentReader
 import org.bson.BsonDocumentWriter
+import org.bson.codecs.jsr310.Jsr310CodecProvider
 import org.bson.types.Binary
 import spock.lang.Specification
 import spock.lang.Unroll
+
+import java.lang.reflect.ParameterizedType
+import java.time.Instant
 
 import static org.bson.BsonDocument.parse
 import static org.bson.UuidRepresentation.C_SHARP_LEGACY
@@ -36,7 +42,8 @@ import static org.bson.codecs.configuration.CodecRegistries.fromRegistries
 class IterableCodecSpecification extends Specification {
 
     static final REGISTRY = fromRegistries(fromCodecs(new UuidCodec(JAVA_LEGACY)),
-            fromProviders(new ValueCodecProvider(), new DocumentCodecProvider(), new BsonValueCodecProvider(), new IterableCodecProvider()))
+            fromProviders(new ValueCodecProvider(), new DocumentCodecProvider(), new BsonValueCodecProvider(),
+                    new IterableCodecProvider(), new MapCodecProvider()))
 
     def 'should have Iterable encoding class'() {
         given:
@@ -158,5 +165,45 @@ class IterableCodecSpecification extends Specification {
         C_SHARP_LEGACY | [new Binary((byte) 4, [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16] as byte[])] | '{"array": [{ "$binary" : "AQIDBAUGBwgJCgsMDQ4PEA==", "$type" : "4" }]}'
         PYTHON_LEGACY  | [new Binary((byte) 4, [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16] as byte[])] | '{"array": [{ "$binary" : "AQIDBAUGBwgJCgsMDQ4PEA==", "$type" : "4" }]}'
         UNSPECIFIED    | [new Binary((byte) 4, [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16] as byte[])] | '{"array": [{ "$binary" : "AQIDBAUGBwgJCgsMDQ4PEA==", "$type" : "4" }]}'
+    }
+
+    def 'should parameterize'() {
+        given:
+        def codec = new IterableCodec(REGISTRY, new BsonTypeClassMap())
+        def writer = new BsonDocumentWriter(new BsonDocument())
+        def reader = new BsonDocumentReader(writer.getDocument())
+        def instants = [
+                ['firstMap': [Instant.ofEpochMilli(1), Instant.ofEpochMilli(2)]],
+                ['secondMap': [Instant.ofEpochMilli(3), Instant.ofEpochMilli(4)]]]
+        when:
+        codec = codec.parameterize(fromProviders(new Jsr310CodecProvider(), REGISTRY),
+                Arrays.asList(((ParameterizedType) Container.getMethod('getInstants').genericReturnType).actualTypeArguments))
+        writer.writeStartDocument()
+        writer.writeName('instants')
+        codec.encode(writer, instants, EncoderContext.builder().build())
+        writer.writeEndDocument()
+
+        then:
+        writer.getDocument() == new BsonDocument()
+                .append('instants', new BsonArray(List.of(
+                        new BsonDocument('firstMap', new BsonArray(List.of(new BsonDateTime(1), new BsonDateTime(2)))),
+                        new BsonDocument('secondMap', new BsonArray(List.of(new BsonDateTime(3), new BsonDateTime(4)))))))
+
+        when:
+        reader.readStartDocument()
+        reader.readName('instants')
+        def decodedInstants = codec.decode(reader, DecoderContext.builder().build())
+
+        then:
+        decodedInstants == instants
+    }
+
+    @SuppressWarnings('unused')
+    static class Container {
+        private final List<Map<String, List<Instant>>> instants = []
+
+        List<Map<String, List<Instant>>> getInstants() {
+            instants
+        }
     }
 }
