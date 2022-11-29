6768db0bfe28887787177d933e8537afda3ebd40
Maxim Katcharov <maxim.katcharov@mongodb.com>
2022-11-29 08:30:58 -0700

    Implement arithmetic expressions (#1037)
    Implement arithmetic expressions (from top 50, and others)
    
    JAVA-4803

--- driver-core/src/main/com/mongodb/client/model/expressions/NumberExpression.java
+++ driver-core/src/main/com/mongodb/client/model/expressions/NumberExpression.java
@@ -21,4 +21,37 @@ package com.mongodb.client.model.expressions;
  */
 public interface NumberExpression extends Expression {
 
+    NumberExpression multiply(NumberExpression n);
+
+    default NumberExpression multiply(final Number multiply) {
+        return this.multiply(Expressions.numberToExpression(multiply));
+    }
+
+    NumberExpression divide(NumberExpression n);
+
+    default NumberExpression divide(final Number divide) {
+        return this.divide(Expressions.numberToExpression(divide));
+    }
+
+    NumberExpression add(NumberExpression n);
+
+    default NumberExpression add(final Number add) {
+        return this.add(Expressions.numberToExpression(add));
+    }
+
+    NumberExpression subtract(NumberExpression n);
+
+    default NumberExpression subtract(final Number subtract) {
+        return this.subtract(Expressions.numberToExpression(subtract));
+    }
+
+    NumberExpression max(NumberExpression n);
+
+    NumberExpression min(NumberExpression n);
+
+    IntegerExpression round();
+
+    NumberExpression round(IntegerExpression place);
+
+    NumberExpression abs();
 }
