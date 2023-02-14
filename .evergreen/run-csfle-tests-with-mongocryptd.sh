107651a84de51e13b37e1e03bc9de4efff2a926e
Valentin Kovalenko <valentin.kovalenko@mongodb.com>
2023-02-14 16:51:49 -0700

    JAVA-4763

--- .evergreen/run-csfle-tests-with-mongocryptd.sh
+++ .evergreen/run-csfle-tests-with-mongocryptd.sh
@@ -4,10 +4,7 @@ set -o xtrace   # Write all commands first to stderr
 set -o errexit  # Exit the script with error if any of the commands fail
 
 # Supported/used environment variables:
-#       AUTH                        Set to enable authentication. Values are: "auth" / "noauth" (default)
-#       SSL                         Set to enable SSL. Values are "ssl" / "nossl" (default)
 #       MONGODB_URI                 Set the suggested connection MONGODB_URI (including credentials and topology info)
-#       TOPOLOGY                    Allows you to modify variables and the MONGODB_URI based on test topology
 #                                   Supported values: "server", "replica_set", "sharded_cluster"
 #       COMPRESSOR                  Set to enable compression. Values are "snappy" and "zlib" (default is no compression)
 #       STREAM_TYPE                 Set the stream type.  Values are "nio2" or "netty".  Defaults to "nio2".
@@ -25,10 +22,7 @@ set -o errexit  # Exit the script with error if any of the commands fail
 #       AZUREKMS_KEY_VAULT_ENDPOINT The Azure key vault endpoint for integration tests
 #       AZUREKMS_KEY_NAME           The Azure key name endpoint for integration tests
 
-AUTH=${AUTH:-noauth}
-SSL=${SSL:-nossl}
 MONGODB_URI=${MONGODB_URI:-}
-TOPOLOGY=${TOPOLOGY:-server}
 COMPRESSOR=${COMPRESSOR:-}
 STREAM_TYPE=${STREAM_TYPE:-nio2}
 
@@ -54,41 +48,13 @@ provision_ssl () {
   export GRADLE_EXTRA_VARS="-Pssl.enabled=true -Pssl.keyStoreType=pkcs12 -Pssl.keyStore=`pwd`/client.pkc -Pssl.keyStorePassword=bithere -Pssl.trustStoreType=jks -Pssl.trustStore=`pwd`/mongo-truststore -Pssl.trustStorePassword=changeit"
 }
 
-provision_multi_mongos_uri_for_ssl () {
-  # Arguments for auth + SSL
-  if [ "$AUTH" != "noauth" ] || [ "$TOPOLOGY" == "replica_set" ]; then
-    export MONGODB_URI="${MONGODB_URI}&ssl=true&sslInvalidHostNameAllowed=true"
-    if [ "$SAFE_FOR_MULTI_MONGOS" == "true" ]; then
-        export MULTI_MONGOS_URI="${MULTI_MONGOS_URI}&ssl=true&sslInvalidHostNameAllowed=true"
-    fi
-  else
-    export MONGODB_URI="${MONGODB_URI}/?ssl=true&sslInvalidHostNameAllowed=true"
-    if [ "$SAFE_FOR_MULTI_MONGOS" == "true" ]; then
-        export MULTI_MONGOS_URI="${MULTI_MONGOS_URI}/?ssl=true&sslInvalidHostNameAllowed=true"
-    fi
-  fi
-}
-
 ############################################
 #            Main Program                  #
 ############################################
 
-# Provision the correct connection string and set up SSL if needed
-if [ "$TOPOLOGY" == "sharded_cluster" ]; then
-    if [ "$SAFE_FOR_MULTI_MONGOS" == "true" ]; then
-        if [ "$AUTH" = "auth" ]; then
-            export MULTI_MONGOS_URI="mongodb://bob:pwd123@localhost:27017,localhost:27018/?authSource=admin"
-        else
-            export MULTI_MONGOS_URI="${MONGODB_URI}"
-        fi
-    fi
-
-     if [ "$AUTH" = "auth" ]; then
-       export MONGODB_URI="mongodb://bob:pwd123@localhost:27017/?authSource=admin"
-     else
-       export MONGODB_URI="mongodb://localhost:27017"
-     fi
-fi
+# Provision the correct connection string
+export MONGODB_URI="mongodb://localhost:27017"
+export MULTI_MONGOS_URI="mongodb://bob:pwd123@localhost:27017,localhost:27018/?authSource=admin"
 
 if [ "$COMPRESSOR" != "" ]; then
      if [[ "$MONGODB_URI" == *"?"* ]]; then
@@ -109,10 +75,6 @@ fi
 # Set up keystore/truststore regardless, as they are required for testing KMIP
 provision_ssl
 
-if [ "$SSL" != "nossl" ]; then
-   provision_multi_mongos_uri_for_ssl
-fi
-
 if [ "$SAFE_FOR_MULTI_MONGOS" == "true" ]; then
     export MULTI_MONGOS_URI_SYSTEM_PROPERTY="-Dorg.mongodb.test.multi.mongos.uri=${MULTI_MONGOS_URI}"
 fi
@@ -121,8 +83,6 @@ fi
 if [ ! -z "$REQUIRE_API_VERSION" ]; then
   export API_VERSION="-Dorg.mongodb.test.api.version=1"
 fi
-
-echo "Running $AUTH tests over $SSL for $TOPOLOGY and connecting to $MONGODB_URI"
 
 echo "Running tests with Java ${JAVA_VERSION}"
 ./gradlew -version
