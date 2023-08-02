3e3b52fc3a87543588664994789e9d7f500d3a3d
slav.babanin <frest0512@gmail.com>
2023-08-02 12:20:58 -0700

    Add Scala test methods to SyncMongoCollection.
    JAVA-4983

--- driver-scala/src/integration/scala/org/mongodb/scala/syncadapter/SyncMongoCollection.scala
+++ driver-scala/src/integration/scala/org/mongodb/scala/syncadapter/SyncMongoCollection.scala
@@ -19,14 +19,21 @@ package org.mongodb.scala.syncadapter
 import com.mongodb.bulk.BulkWriteResult
 import com.mongodb.client.model._
 import com.mongodb.client.result.{ DeleteResult, UpdateResult }
-import com.mongodb.client.{ ChangeStreamIterable, ClientSession, MongoCollection => JMongoCollection }
+import com.mongodb.client.{
+  ChangeStreamIterable,
+  ClientSession,
+  ListSearchIndexesIterable,
+  MongoCollection => JMongoCollection
+}
 import com.mongodb.{ MongoNamespace, ReadConcern, ReadPreference, WriteConcern }
+import org.bson.Document
 import org.bson.codecs.configuration.CodecRegistry
 import org.bson.conversions.Bson
 import org.mongodb.scala.MongoCollection
 import org.mongodb.scala.bson.DefaultHelper.DefaultsTo
 import org.mongodb.scala.result.{ InsertManyResult, InsertOneResult }
 
+import java.util
 import scala.collection.JavaConverters._
 import scala.reflect.ClassTag
 
@@ -472,6 +479,31 @@ case class SyncMongoCollection[T](wrapped: MongoCollection[T]) extends JMongoCol
 
   override def drop(clientSession: ClientSession, dropCollectionOptions: DropCollectionOptions): Unit =
     wrapped.drop(unwrap(clientSession), dropCollectionOptions).toFuture().get()
+
+  override def createSearchIndex(indexName: String, definition: Bson) =
+    wrapped.createSearchIndex(indexName, definition).toFuture().get()
+
+  override def createSearchIndex(definition: Bson) =
+    wrapped.createSearchIndex(definition).toFuture().get()
+
+  override def createSearchIndexes(searchIndexModels: util.List[SearchIndexModel]) =
+    throw new UnsupportedOperationException()
+
+  def updateSearchIndex(indexName: String, definition: Bson) =
+    wrapped.updateSearchIndex(indexName, definition).toFuture().get()
+
+  def dropSearchIndex(indexName: String) = wrapped.dropSearchIndex(indexName).toFuture().get()
+
+  override def listSearchIndexes(): ListSearchIndexesIterable[Document] = throw new UnsupportedOperationException()
+
+  override def listSearchIndexes[TResult](resultClass: Class[TResult]) =
+    SyncListSearchIndexesIterable[TResult](
+      wrapped
+        .listSearchIndexes[TResult]()(
+          DefaultsTo.overrideDefault[TResult, org.mongodb.scala.Document],
+          ClassTag(resultClass)
+        )
+    )
 
   override def createIndex(keys: Bson): String = wrapped.createIndex(keys).toFuture().get()
 
