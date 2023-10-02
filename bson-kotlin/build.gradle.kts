434e6ec293d12c76431f32aee0fb0abd55b1edb2
monosoul <Kloz.Klaud@gmail.com>
2023-10-02 10:46:34 +0200

    fix: include Kotlin sources into the sources jar

--- bson-kotlin/build.gradle.kts
+++ bson-kotlin/build.gradle.kts
@@ -143,3 +143,10 @@ tasks.javadocJar.configure {
     archiveClassifier.set("javadoc")
     from(dokkaOutputDir)
 }
+
+// ===========================
+//     Sources publishing configuration
+// ===========================
+tasks.sourcesJar {
+    from(project.sourceSets.main.map { it.kotlin })
+}
