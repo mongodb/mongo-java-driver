03466772d686a443bb0949604c4cc4ba3b340de4
Andrei Nevedomskii <kloz.klaud@gmail.com>
2023-10-03 12:20:34 +0200

    fix: include Kotlin sources into the sources jar (#1210)
    JAVA-5187

--- driver-kotlin-coroutine/build.gradle.kts
+++ driver-kotlin-coroutine/build.gradle.kts
@@ -188,3 +188,10 @@ tasks.javadocJar.configure {
     archiveClassifier.set("javadoc")
     from(dokkaOutputDir)
 }
+
+// ===========================
+//     Sources publishing configuration
+// ===========================
+tasks.sourcesJar {
+    from(project.sourceSets.main.map { it.kotlin })
+}
