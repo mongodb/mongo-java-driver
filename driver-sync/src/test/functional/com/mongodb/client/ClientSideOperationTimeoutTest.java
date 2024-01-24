1873bf7a5838151f499d0eca85d351cc2641b23e
Ross Lawley <ross.lawley@gmail.com>
2024-01-24 10:38:59 +0000

    Temp disable tests

--- driver-sync/src/test/functional/com/mongodb/client/ClientSideOperationTimeoutTest.java
+++ driver-sync/src/test/functional/com/mongodb/client/ClientSideOperationTimeoutTest.java
@@ -42,6 +42,9 @@ public class ClientSideOperationTimeoutTest extends UnifiedSyncTest {
         super(schemaVersion, runOnRequirements, entities, initialData, definition);
         this.testDescription = testDescription;
         checkSkipCSOTTest(fileDescription, testDescription);
+
+        assumeFalse("TODO (CSOT) - JAVA-5104", fileDescription.equals("timeoutMS behaves correctly for non-tailable cursors")
+                &&  testDescription.equals("timeoutMS applied to find if timeoutMode is iteration"));
     }
 
     @Parameterized.Parameters(name = "{0}: {1}")
