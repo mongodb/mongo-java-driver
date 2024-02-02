4d54d224ba039cdf64a0c8f2b9212de7e65c63b6
Jeff Yemin <jeff.yemin@mongodb.com>
2024-02-02 17:30:34 -0500

    JAVA-5311

--- .evergreen/run-csfle-tests-with-mongocryptd.sh
+++ .evergreen/run-csfle-tests-with-mongocryptd.sh
@@ -49,18 +49,30 @@ provision_ssl () {
 provision_ssl
 
 echo "Running tests with Java ${JAVA_VERSION}"
+
+# Append to gradle.properties so that they are not echoed below on the gradlew  command line
+cat <<EOF >> ./gradle.properties
+systemProp.org.mongodb.test.fle.on.demand.credential.test.failure.enabled=true
+systemProp.org.mongodb.test.fle.on.demand.credential.test.azure.keyVaultEndpoint=${AZUREKMS_KEY_VAULT_ENDPOINT}
+systemProp.org.mongodb.test.fle.on.demand.credential.test.azure.keyName=${AZUREKMS_KEY_NAME}
+systemProp.org.mongodb.test.awsAccessKeyId=${AWS_ACCESS_KEY_ID}
+systemProp.org.mongodb.test.awsSecretAccessKey=${AWS_SECRET_ACCESS_KEY}
+systemProp.org.mongodb.test.tmpAwsAccessKeyId=${AWS_TEMP_ACCESS_KEY_ID}
+systemProp.org.mongodb.test.tmpAwsSecretAccessKey=${AWS_TEMP_SECRET_ACCESS_KEY}
+systemProp.org.mongodb.test.tmpAwsSessionToken=${AWS_TEMP_SESSION_TOKEN}
+systemProp.org.mongodb.test.azureTenantId=${AZURE_TENANT_ID}
+systemProp.org.mongodb.test.azureClientId=${AZURE_CLIENT_ID}
+systemProp.org.mongodb.test.azureClientSecret=${AZURE_CLIENT_SECRET}
+systemProp.org.mongodb.test.gcpEmail=${GCP_EMAIL}
+systemProp.org.mongodb.test.gcpPrivateKey=${GCP_PRIVATE_KEY}
+systemProp.org.mongodb.test.crypt.shared.lib.path=${CRYPT_SHARED_LIB_PATH}
+EOF
+
 ./gradlew -version
 
 # By not specifying the path to the `crypt_shared` via the `org.mongodb.test.crypt.shared.lib.path` Java system property,
 # we force the driver to start `mongocryptd` instead of loading and using `crypt_shared`.
 ./gradlew -PjavaVersion=${JAVA_VERSION} -Dorg.mongodb.test.uri=${MONGODB_URI} \
-      -Dorg.mongodb.test.fle.on.demand.credential.test.failure.enabled="true" \
-      -Dorg.mongodb.test.fle.on.demand.credential.test.azure.keyVaultEndpoint="${AZUREKMS_KEY_VAULT_ENDPOINT}" \
-      -Dorg.mongodb.test.fle.on.demand.credential.test.azure.keyName="${AZUREKMS_KEY_NAME}" \
-      -Dorg.mongodb.test.awsAccessKeyId=${AWS_ACCESS_KEY_ID} -Dorg.mongodb.test.awsSecretAccessKey=${AWS_SECRET_ACCESS_KEY} \
-      -Dorg.mongodb.test.tmpAwsAccessKeyId=${AWS_TEMP_ACCESS_KEY_ID} -Dorg.mongodb.test.tmpAwsSecretAccessKey=${AWS_TEMP_SECRET_ACCESS_KEY} -Dorg.mongodb.test.tmpAwsSessionToken=${AWS_TEMP_SESSION_TOKEN} \
-      -Dorg.mongodb.test.azureTenantId=${AZURE_TENANT_ID} -Dorg.mongodb.test.azureClientId=${AZURE_CLIENT_ID} -Dorg.mongodb.test.azureClientSecret=${AZURE_CLIENT_SECRET} \
-      -Dorg.mongodb.test.gcpEmail=${GCP_EMAIL} -Dorg.mongodb.test.gcpPrivateKey=${GCP_PRIVATE_KEY} \
       ${GRADLE_EXTRA_VARS} \
       --stacktrace --info --continue \
       driver-legacy:test \
