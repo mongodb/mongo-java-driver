88f21f3caf444d7d0c8c757ed981350617fab4f2
Ross Lawley <ross.lawley@gmail.com>
2024-02-20 14:25:24 +0000

    Don't set the timeoutMode on change streams as it impedes legacy options

--- driver-core/src/main/com/mongodb/internal/operation/ChangeStreamOperation.java
+++ driver-core/src/main/com/mongodb/internal/operation/ChangeStreamOperation.java
@@ -17,7 +17,6 @@
 package com.mongodb.internal.operation;
 
 import com.mongodb.MongoNamespace;
-import com.mongodb.client.cursor.TimeoutMode;
 import com.mongodb.client.model.Collation;
 import com.mongodb.client.model.changestream.FullDocument;
 import com.mongodb.client.model.changestream.FullDocumentBeforeChange;
@@ -72,7 +71,7 @@ public class ChangeStreamOperation<T> implements AsyncReadOperation<AsyncBatchCu
             final FullDocumentBeforeChange fullDocumentBeforeChange, final List<BsonDocument> pipeline, final Decoder<T> decoder,
             final ChangeStreamLevel changeStreamLevel) {
         this.wrapped = new AggregateOperationImpl<>(namespace, pipeline, RAW_BSON_DOCUMENT_CODEC, getAggregateTarget(),
-                getPipelineCreator()).timeoutMode(TimeoutMode.ITERATION);
+                getPipelineCreator());
         this.fullDocument = notNull("fullDocument", fullDocument);
         this.fullDocumentBeforeChange = notNull("fullDocumentBeforeChange", fullDocumentBeforeChange);
         this.decoder = notNull("decoder", decoder);
