4385af6ee5a7f8d28287b6ddaf14618898e6402f
Rachel Mackintosh <rachel.mackintosh@mongodb.com>
2024-03-01 14:43:27 -0500

    v5.0 api docs

--- 5.0/apidocs/docs/bson-record-codec/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"org.bson.codecs.record","l":"RecordCodecProvider"}];updateSearchResults();
--- 5.0/apidocs/docs/mongodb-driver-kotlin-sync/scripts/sourceset_dependencies.js
+++
@@ -1,1 +0,0 @@
-sourceset_dependencies='{":driver-kotlin-sync:dokkaHtml/main":[]}'
