442fc2fbc143ae48f8f738ee016d3fa5b2a6f388
slav.babanin <slav.babanin@mongodb.com>
2024-03-20 15:05:12 -0700

    Fix tests.
    JAVA-5322

--- driver-core/src/main/com/mongodb/internal/operation/AsyncCommandBatchCursor.java
+++ driver-core/src/main/com/mongodb/internal/operation/AsyncCommandBatchCursor.java
@@ -322,7 +322,7 @@ class AsyncCommandBatchCursor<T> implements AsyncAggregateResponseBatchCursor<T>
                 final AsyncConnection localConnection, final SingleResultCallback<Void> callback) {
             OperationContext operationContext = assertNotNull(getConnectionSource()).getOperationContext();
             TimeoutContext timeoutContext = operationContext.getTimeoutContext();
-            timeoutContext.setMaxTimeSupplier(timeoutContext::getMaxTimeMS);
+            timeoutContext.resetToDefaultMaxTimeSupplier();
 
             localConnection.commandAsync(namespace.getDatabaseName(), getKillCursorsCommand(namespace, localServerCursor),
                     NO_OP_FIELD_NAME_VALIDATOR, ReadPreference.primary(), new BsonDocumentCodec(),
