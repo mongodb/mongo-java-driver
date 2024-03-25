82f4a9f7bcefb036cbffb12fe7315b8333065e7e
Valentin Kovalenko <valentin.kovalenko@mongodb.com>
2024-03-25 11:48:38 -0600

    Test against GraalVM native image technology (#1338)
    JAVA-3580

--- .evergreen/javaConfig.bash
+++ .evergreen/javaConfig.bash
@@ -4,6 +4,9 @@ export JDK8="/opt/java/jdk8"
 export JDK11="/opt/java/jdk11"
 export JDK17="/opt/java/jdk17"
 export JDK21="/opt/java/jdk21"
+# note that `JDK21_GRAALVM` is used in `run-graalvm-native-image-app.sh`
+# by dynamically constructing the variable name
+export JDK21_GRAALVM="/opt/java/jdk21-graalce"
 
 if [ -d "$JDK17" ]; then
   export JAVA_HOME=$JDK17
