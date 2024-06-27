81e4f4dfdb05b8c3b65b1145e6a3f659b096cb66
Valentin Kovalenko <valentin.kovalenko@mongodb.com>
2024-06-27 15:18:15 -0600

    Do not generate a fake Evergreen link for snapshot builds
    JAVA-5500

--- .evergreen/ssdlc-report.sh
+++ .evergreen/ssdlc-report.sh
@@ -45,7 +45,7 @@ elif [ -n "${GIT_BRANCH_PATCH}" ]; then
     declare -r EVERGREEN_PROJECT_NAME_SUFFIX="${PRODUCT_VERSION%.*}"
     declare -r EVERGREEN_BUILD_URL="${EVERGREEN_BUILD_URL_PREFIX}/${EVERGREEN_PROJECT_NAME_PREFIX}_${EVERGREEN_PROJECT_NAME_SUFFIX}_${GIT_COMMIT_HASH}"
 elif [[ "${PRODUCT_NAME}" == *'-snapshot' ]]; then
-    declare -r EVERGREEN_BUILD_URL="https://no-url-for-snapshot-builds"
+    declare -r EVERGREEN_BUILD_URL="cannot-compute-evergreen-url-for-snapshot-builds"
 else
     printf "\nFailed to compute EVERGREEN_BUILD_URL\n"
     exit 1
