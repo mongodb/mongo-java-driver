7683aab5abd50472329d1fe34edcb69ceaf4ca6f
Ross Lawley <ross@mongodb.com>
2024-07-31 17:47:00 +0100

    Kotlin: Updated driver metadata (#1461)
    Added Kotlin version to the driver platform metadata.
    
    JAVA-5539

--- driver-kotlin-coroutine/src/main/kotlin/com/mongodb/kotlin/client/coroutine/MongoClient.kt
+++ driver-kotlin-coroutine/src/main/kotlin/com/mongodb/kotlin/client/coroutine/MongoClient.kt
@@ -92,7 +92,9 @@ public class MongoClient(private val wrapped: JMongoClient) : Closeable {
             val builder =
                 if (mongoDriverInformation == null) MongoDriverInformation.builder()
                 else MongoDriverInformation.builder(mongoDriverInformation)
-            return MongoClient(JMongoClients.create(settings, builder.driverName("kotlin").build()))
+            return MongoClient(
+                JMongoClients.create(
+                    settings, builder.driverName("kotlin").driverPlatform("kotlin/${KotlinVersion.CURRENT}").build()))
         }
     }
 
