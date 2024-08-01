3fc18bf4e4e6adae7ef2b75d7d1d61e27c68e10a
Ross Lawley <ross.lawley@gmail.com>
2024-08-01 08:58:47 +0100

    Ensure Sink.contextView is propagated
    Port Flux/Mono improvements to the logic from master
    Context view is propagated via the subscriber, so any
    nested subscribe calls need to have the context passed
    through.
    
    JAVA-5345

--- driver-reactive-streams/src/main/com/mongodb/reactivestreams/client/internal/BatchCursorFlux.java
+++ driver-reactive-streams/src/main/com/mongodb/reactivestreams/client/internal/BatchCursorFlux.java
@@ -18,11 +18,9 @@ package com.mongodb.reactivestreams.client.internal;
 
 import org.reactivestreams.Publisher;
 import org.reactivestreams.Subscriber;
-import reactor.core.CoreSubscriber;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.FluxSink;
 import reactor.core.publisher.Mono;
-import reactor.util.context.Context;
 
 import java.util.Objects;
 import java.util.concurrent.atomic.AtomicBoolean;
@@ -48,19 +46,19 @@ class BatchCursorFlux<T> implements Publisher<T> {
                 if (calculateDemand(demand) > 0 && inProgress.compareAndSet(false, true)) {
                     if (batchCursor == null) {
                         int batchSize = calculateBatchSize(sink.requestedFromDownstream());
-                        Context initialContext =  subscriber instanceof CoreSubscriber<?>
-                                ? ((CoreSubscriber<?>) subscriber).currentContext() : null;
-                        batchCursorPublisher.batchCursor(batchSize).subscribe(bc -> {
-                            batchCursor = bc;
-                            inProgress.set(false);
+                        batchCursorPublisher.batchCursor(batchSize)
+                                .contextWrite(sink.contextView())
+                                .subscribe(bc -> {
+                                    batchCursor = bc;
+                                    inProgress.set(false);
 
-                            // Handle any cancelled subscriptions that happen during the time it takes to get the batchCursor
-                            if (sink.isCancelled()) {
-                                closeCursor();
-                            } else {
-                                recurseCursor();
-                            }
-                        }, sink::error, null,  initialContext);
+                                    // Handle any cancelled subscriptions that happen during the time it takes to get the batchCursor
+                                    if (sink.isCancelled()) {
+                                        closeCursor();
+                                    } else {
+                                        recurseCursor();
+                                    }
+                                }, sink::error);
                     } else {
                         inProgress.set(false);
                         recurseCursor();
@@ -86,6 +84,7 @@ class BatchCursorFlux<T> implements Publisher<T> {
             } else {
                 batchCursor.setBatchSize(calculateBatchSize(sink.requestedFromDownstream()));
                 Mono.from(batchCursor.next(() -> sink.isCancelled()))
+                        .contextWrite(sink.contextView())
                         .doOnCancel(this::closeCursor)
                         .doOnError((e) -> {
                             try {
