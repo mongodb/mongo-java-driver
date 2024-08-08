eeeb88f1f0ea89edb3889a398820c7739cd1e9db
Ross Lawley <ross@mongodb.com>
2024-08-08 16:03:15 +0100

    Checkstyle fix ConnectionStringUnitTest (#1475)

--- driver-core/src/test/unit/com/mongodb/ConnectionStringUnitTest.java
+++ driver-core/src/test/unit/com/mongodb/ConnectionStringUnitTest.java
@@ -19,7 +19,6 @@ import com.mongodb.assertions.Assertions;
 import com.mongodb.connection.ServerMonitoringMode;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import java.io.UnsupportedEncodingException;
