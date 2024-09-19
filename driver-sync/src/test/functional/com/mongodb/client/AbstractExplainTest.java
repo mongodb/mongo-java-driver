d3c8a18a24ffa5a9e71341a4985f581c554a1e36
slav.babanin <slav.babanin@mongodb.com>
2024-09-18 18:10:18 -0700

    Fix static checks.
    JAVA-5580

--- driver-sync/src/test/functional/com/mongodb/client/AbstractExplainTest.java
+++ driver-sync/src/test/functional/com/mongodb/client/AbstractExplainTest.java
@@ -39,7 +39,6 @@ import static java.util.Collections.singletonList;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertThrows;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assume.assumeTrue;
 
