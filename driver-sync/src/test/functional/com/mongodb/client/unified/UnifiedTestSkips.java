2689fa6b2a24836d2b7258bd1a3e8bf67a41ffa8
Maxim Katcharov <maxim.katcharov@mongodb.com>
2024-10-31 10:12:29 -0600

    Fix static checks

--- driver-sync/src/test/functional/com/mongodb/client/unified/UnifiedTestSkips.java
+++ driver-sync/src/test/functional/com/mongodb/client/unified/UnifiedTestSkips.java
@@ -1,16 +1,34 @@
+/*
+ * Copyright 2008-present MongoDB, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.mongodb.client.unified;
 
 import com.mongodb.assertions.Assertions;
 
 import static org.junit.jupiter.api.Assumptions.assumeFalse;
 
-public class UnifiedTestSkips {
+public final class UnifiedTestSkips {
+
+    private UnifiedTestSkips() {}
 
     public static TestDef testDef(final String dir, final String file, final String test) {
         return new TestDef(dir, file, test);
     }
 
-    public static class TestDef {
+    public static final class TestDef {
         private final String dir;
         private final String file;
         private final String test;
@@ -52,7 +70,7 @@ public class UnifiedTestSkips {
         }
     }
 
-    public static class Skip {
+    public static final class Skip {
         private final TestDef testDef;
         private final String reason;
 
