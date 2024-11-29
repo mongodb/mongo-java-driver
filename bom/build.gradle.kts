5f7b73141b7243acef5ca9f7f5b134252ee5ecc2
slav.babanin <slav.babanin@mongodb.com>
2024-11-28 19:14:44 -0800

    - Remove Kotlin and Scala BOM modules. - Add BOM validation.
    JAVA-3808

--- bom/build.gradle.kts
+++ bom/build.gradle.kts
@@ -1,5 +1,4 @@
 group = "org.mongodb"
-base.archivesName.set("mongodb-driver-bom")
 description = "This Bill of Materials POM simplifies dependency management when referencing multiple" +
         " MongoDB Java Driver artifacts in projects using Gradle or Maven."
 
@@ -7,14 +6,19 @@ dependencies {
     constraints {
         api(project(":mongodb-crypt"))
         api(project(":driver-core"))
-        api(project(":driver-sync"))
-        api(project(":driver-reactive-streams"))
         api(project(":bson"))
         api(project(":bson-record-codec"))
+
+        api(project(":driver-sync"))
+        api(project(":driver-reactive-streams"))
+
+        api(project(":bson-kotlin"))
+        api(project(":bson-kotlinx"))
+        api(project(":driver-kotlin-coroutine"))
+        api(project(":driver-kotlin-sync"))
+
+        api(project(":bson-scala"))
+        api(project(":driver-scala"))
+
     }
-
-}
-
-javaPlatform {
-    allowDependencies()
 }
