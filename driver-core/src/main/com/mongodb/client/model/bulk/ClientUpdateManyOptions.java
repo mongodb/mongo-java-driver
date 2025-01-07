f0f7ed37ce837b70f3d0cd67150f20498116f9a3
slav.babanin <slav.babanin@mongodb.com>
2025-01-06 23:24:13 -0800

    Fix return type.
    JAVA-5737

--- driver-core/src/main/com/mongodb/client/model/bulk/ClientUpdateManyOptions.java
+++ driver-core/src/main/com/mongodb/client/model/bulk/ClientUpdateManyOptions.java
@@ -45,7 +45,7 @@ public interface ClientUpdateManyOptions extends BaseClientUpdateOptions {
      * @return {@code this}.
      * @see Filters
      */
-    BaseClientUpdateOptions arrayFilters(@Nullable Iterable<? extends Bson> arrayFilters);
+    ClientUpdateManyOptions arrayFilters(@Nullable Iterable<? extends Bson> arrayFilters);
 
     /**
      * Sets the collation.
