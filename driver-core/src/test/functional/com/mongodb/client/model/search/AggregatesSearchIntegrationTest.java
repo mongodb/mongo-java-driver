6d2ffe428242a8bdd1073c71d38464f4fa576352
Joy Kim <joy.kim@mongodb.com>
2025-01-09 11:36:54 -0500

    evergreen test

--- driver-core/src/test/functional/com/mongodb/client/model/search/AggregatesSearchIntegrationTest.java
+++ driver-core/src/test/functional/com/mongodb/client/model/search/AggregatesSearchIntegrationTest.java
@@ -610,8 +610,10 @@ final class AggregatesSearchIntegrationTest {
                                                 .lte(Instant.ofEpochMilli(1)),
                                         near(0, 1.5, fieldPath("fieldName7"), fieldPath("fieldName8")),
                                         near(Instant.ofEpochMilli(1), Duration.ofMillis(3), fieldPath("fieldName9")),
+                                        wildcard("term9", fieldPath("fieldName1")),
                                         wildcard("term9", fieldPath("fieldName13"))
-                                                .score(function(logExpression(constantExpression(3)))),
+                                                .score(function(logExpression(constantExpression(3))))
+                                                .allowAnalyzedField(true),
                                         wildcard(asList("term10", "term11"), asList(wildcardPath("wildc*rd"), fieldPath("fieldName14")))
                                                 .allowAnalyzedField()
                                 ))
