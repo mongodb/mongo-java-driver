927c73217d63d52131386fe9b9d68227880b26d1
joykim1005 <81274936+joykim1005@users.noreply.github.com>
2025-01-17 12:09:25 -0500

    Fix Int Test for Regex and Wildcard Operator (#1611)
    Fix int test for regex and wildcard operator
    
    The merged PRs for the regex and wildcard causes the AggregatesSearchIntegrationTest to fail. This PR fixes the integration test, so it passes.
    
    JAVA-5726

--- driver-core/src/test/functional/com/mongodb/client/model/search/AggregatesSearchIntegrationTest.java
+++ driver-core/src/test/functional/com/mongodb/client/model/search/AggregatesSearchIntegrationTest.java
@@ -614,9 +614,10 @@ final class AggregatesSearchIntegrationTest {
                                         near(0, 1.5, fieldPath("fieldName7"), fieldPath("fieldName8")),
                                         near(Instant.ofEpochMilli(1), Duration.ofMillis(3), fieldPath("fieldName9")),
                                         phrase(fieldPath("fieldName10"), "term6"),
-                                        regex(fieldPath("fieldName11"), "term7"),
+                                        regex(fieldPath("title").multi("keyword"), "term7"),
                                         queryString(fieldPath("fieldName12"), "term8"),
-                                        wildcard(asList("term10", "term11"), asList(wildcardPath("wildc*rd"), fieldPath("fieldName14")))
+                                        wildcard(asList("term10", "term11"), asList(wildcardPath("wildc*rd"), fieldPath("title").multi(
+                                                "keyword")))
                                 ))
                                 .minimumShouldMatch(1)
                                 .mustNot(singleton(
