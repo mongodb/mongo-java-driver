5e14e04880b15074d7a5fdfe5a8b552a1da8a5a0
Jeff Yemin <jeff.yemin@mongodb.com>
2025-03-28 17:52:12 -0400

    Fixed copy pasta

--- driver-core/src/main/com/mongodb/internal/operation/ServerVersionHelper.java
+++ driver-core/src/main/com/mongodb/internal/operation/ServerVersionHelper.java
@@ -30,7 +30,7 @@ public final class ServerVersionHelper {
     public static final int FIVE_DOT_ZERO_WIRE_VERSION = 12;
     public static final int SIX_DOT_ZERO_WIRE_VERSION = 17;
     public static final int SEVEN_DOT_ZERO_WIRE_VERSION = 21;
-    public static final int EIGHT_DOT_ZERO_WIRE_VERSION = 21;
+    public static final int EIGHT_DOT_ZERO_WIRE_VERSION = 25;
     public static final int EARLIEST_WIRE_VERSION = FOUR_DOT_TWO_WIRE_VERSION;
     public static final int LATEST_WIRE_VERSION = EIGHT_DOT_ZERO_WIRE_VERSION;
 
