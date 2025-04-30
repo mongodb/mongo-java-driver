8a5ce2d78999afc3d1fe3e4beede6ab21b2655d4
Viacheslav Babanin <slav.babanin@mongodb.com>
2025-04-30 15:16:19 -0700

    Update bson/src/main/org/bson/io/ByteBufferBsonInput.java
    Co-authored-by: Valentin Kovalenko <valentin.male.kovalenko@gmail.com>

--- bson/src/main/org/bson/io/ByteBufferBsonInput.java
+++ bson/src/main/org/bson/io/ByteBufferBsonInput.java
@@ -205,7 +205,7 @@ public class ByteBufferBsonInput implements BsonInput {
             /*
               mask will only have the most significant bit set iff it was a 0x00 byte (0x00 becomes 0xFF because of the borrow).
               ~chunk will have bits that were originally 0 set to 1.
-              mask & ~chunk will have the most significant bit set iff original byte was 0x00.
+              mask & ~chunk will have the most significant bit in each byte set iff original byte was 0x00.
              */
             mask &= ~chunk;
             /*
