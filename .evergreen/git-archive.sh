dd782f6bd85d2a9f23497263ca2cb9cf1d83dd42
Ross Lawley <ross.lawley@gmail.com>
2025-05-27 16:34:10 +0100

    Update to a neater way of doing things

--- .evergreen/git-archive.sh
+++ .evergreen/git-archive.sh
@@ -6,24 +6,15 @@ set -o errexit
 # Returns the path to the root archive file which includes all git submodules.
 
 echo "Creating root archive"
-export GIT_ARCHIVE_FILE="/tmp/mongo-java-driver"
+export GIT_ARCHIVE_FILE="/tmp/mongo-java-driver.tar"
 
 # create root archive
-git archive --output "$GIT_ARCHIVE_FILE.tar" HEAD
+git archive --output $GIT_ARCHIVE_FILE HEAD
 
 echo "Appending submodule archives"
-# for each of git submodules append to the root archive
-git submodule foreach --recursive 'git archive --prefix=$path/ --output "$GIT_ARCHIVE_FILE-sub-$sha1.tar" $sha1'
-
-if [[ $(ls $GIT_ARCHIVE_FILE-sub*.tar | wc -l) != 0  ]]; then
-  echo "Combining all submodule archives into one tar"
-  tar -cf $GIT_ARCHIVE_FILE.tar $GIT_ARCHIVE_FILE-sub*.tar
-
-  echo "Removing submodule archives"
-  rm -rf $GIT_ARCHIVE_FILE-sub*.tar
-fi
+git submodule status --recursive | awk '{ print $2 }' | xargs tar -rf $GIT_ARCHIVE_FILE
 
 echo "Appending .git directory to the root archive"
-tar -rf $GIT_ARCHIVE_FILE.tar .git
+tar -rf $GIT_ARCHIVE_FILE .git
 
-echo "$GIT_ARCHIVE_FILE.tar"
+echo "$GIT_ARCHIVE_FILE"
