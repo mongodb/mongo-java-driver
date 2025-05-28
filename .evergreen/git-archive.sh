9f03047842e07a01e4cc8fdb79af0b2529ffa0e5
Ross Lawley <ross.lawley@gmail.com>
2025-05-28 16:15:24 +0100

    Updated git-archive logic
    Removed old file if exists
    Cleaned up assigning in evergreen
    Updated evergreen copy pasta tgz file name
    Ensure all task groups have their setup and tear down config in the same place
    
    JAVA-5886

--- .evergreen/git-archive.sh
+++ .evergreen/git-archive.sh
@@ -3,10 +3,14 @@
 # Exit the script with error if any of the commands fail
 set -o errexit
 
-# Returns the path to the root archive file which includes all git submodules.
+# Creates the root archive file which includes all git submodules.
+export GIT_ARCHIVE_FILE=${1:-"/tmp/mongo-java-driver.tar"}
 
-echo "Creating root archive"
-export GIT_ARCHIVE_FILE="/tmp/mongo-java-driver.tar"
+echo "Creating archive: $GIT_ARCHIVE_FILE"
+if [ -f "$GIT_ARCHIVE_FILE" ]; then
+  echo "Removing existing archive file: $GIT_ARCHIVE_FILE"
+  rm $GIT_ARCHIVE_FILE
+fi
 
 # create root archive
 git archive --output $GIT_ARCHIVE_FILE HEAD
@@ -17,4 +21,4 @@ git submodule status --recursive | awk '{ print $2 }' | xargs tar -rf $GIT_ARCHI
 echo "Appending .git directory to the root archive"
 tar -rf $GIT_ARCHIVE_FILE .git
 
-echo "$GIT_ARCHIVE_FILE"
+echo "Created root archive $GIT_ARCHIVE_FILE"
