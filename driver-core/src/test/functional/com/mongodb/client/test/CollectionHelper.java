350af710ea8593479309757895c1bbaff77d21cc
Ross Lawley <ross.lawley@gmail.com>
2025-08-12 11:44:23 +0100

    Support CSFLE unified tests
    - Added scala UnifiedTest support.
    - Added scala unified tests for ClientEncryption and Crud
    
    JAVA-5674

--- driver-core/src/test/functional/com/mongodb/client/test/CollectionHelper.java
+++ driver-core/src/test/functional/com/mongodb/client/test/CollectionHelper.java
@@ -179,6 +179,11 @@ public final class CollectionHelper<T> {
                 case "encryptedFields":
                     createCollectionOptions.encryptedFields(createOptions.getDocument("encryptedFields"));
                     break;
+                case "validator":
+                    ValidationOptions validationOptions = new ValidationOptions();
+                    validationOptions.validator(createOptions.getDocument("validator"));
+                    createCollectionOptions.validationOptions(validationOptions);
+                    break;
                 default:
                     throw new UnsupportedOperationException("Unsupported create collection option: " + option);
             }
@@ -197,6 +202,10 @@ public final class CollectionHelper<T> {
         IndexOptionDefaults indexOptionDefaults = options.getIndexOptionDefaults();
         if (indexOptionDefaults.getStorageEngine() != null) {
             operation.indexOptionDefaults(new BsonDocument("storageEngine", toBsonDocument(indexOptionDefaults.getStorageEngine())));
+        }
+        Bson encryptedFields = options.getEncryptedFields();
+        if (encryptedFields != null) {
+            operation.encryptedFields(encryptedFields.toBsonDocument());
         }
         ValidationOptions validationOptions = options.getValidationOptions();
         if (validationOptions.getValidator() != null) {
