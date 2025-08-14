66c7e57e2174c218720e154a9411bf72dd3dbb55
Ross Lawley <ross@mongodb.com>
2025-08-14 16:39:46 +0100

    Added ClientEncryption methods to scala (#1779)
    * Added ClientEncryption methods to scala
    
    - Added explicit public constructor for `RewrapManyDataKeyOptions`
    - Added missing methods to scala's `ClientEncryption`
    - Added type alias and builder objects for `RewrapManyDataKeyResult`
    - Added type alias and builder objects for `RewrapManyDataKeyOptions`
    - Updated logic for API tests - checking all public methods
    - Fixed the `ClientEncryptionSpec` to test for scala class methods as opposed to scala object methods.
    
    JAVA-5933
    
    Co-authored-by: Viacheslav Babanin <frest0512@gmail.com>

--- driver-scala/src/test/scala/org/mongodb/scala/gridfs/GridFSUploadPublisherSpec.scala
+++ driver-scala/src/test/scala/org/mongodb/scala/gridfs/GridFSUploadPublisherSpec.scala
@@ -31,7 +31,7 @@ class GridFSUploadPublisherSpec extends BaseSpec with MockitoSugar {
 
     wrapped.foreach((name: String) => {
       val cleanedName = name.stripPrefix("get")
-      assert(local.contains(name) | local.contains(cleanedName.head.toLower + cleanedName.tail), s"Missing: $name")
+      assert(local.contains(name) || local.contains(cleanedName.head.toLower + cleanedName.tail), s"Missing: $name")
     })
   }
 
