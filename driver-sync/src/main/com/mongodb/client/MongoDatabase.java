7572c56fd261be475266e0b7fe1a50fea26f8fb4
slav.babanin <slav.babanin@mongodb.com>
2025-08-14 18:03:01 -0700

    Add Javadoc to specify that CSOT does not limit socket writes in synchronous API.
    JAVA-5569

--- driver-sync/src/main/com/mongodb/client/MongoDatabase.java
+++ driver-sync/src/main/com/mongodb/client/MongoDatabase.java
@@ -160,6 +160,9 @@ public interface MongoDatabase {
      *    <li>{@code > 0} The time limit to use for the full execution of an operation.</li>
      * </ul>
      *
+     *  <p><strong>NOTE:</strong> This timeout does not limit socket writes, therefore there is a possibility that the
+     *  operation might not be timed out when expected.
+     *
      * @param timeout the timeout, which must be greater than or equal to 0
      * @param timeUnit the time unit
      * @return a new MongoDatabase instance with the set time limit for the full execution of an operation.
