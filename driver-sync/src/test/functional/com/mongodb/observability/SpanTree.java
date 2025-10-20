90e4b1c3556464ebea19cb38cebb310eb177250b
Ross Lawley <ross.lawley@gmail.com>
2025-10-20 11:40:40 +0100

    Observability changes
    Fix javadoc
    Move tracing tests to the appropriate package

--- driver-sync/src/test/functional/com/mongodb/observability/SpanTree.java
+++ driver-sync/src/test/functional/com/mongodb/observability/SpanTree.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package com.mongodb.client.tracing;
+package com.mongodb.observability;
 
 import com.mongodb.lang.Nullable;
 import io.micrometer.tracing.exporter.FinishedSpan;
