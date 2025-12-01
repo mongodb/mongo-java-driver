81e8548768d19207b2a832b1de92530d4eb60ce8
Ross Lawley <ross@mongodb.com>
2025-12-01 08:58:52 +0000

    Update driver-core/src/test/unit/com/mongodb/internal/connection/CompositeByteBufTest.java
    Co-authored-by: Valentin Kovalenko <valentin.male.kovalenko@gmail.com>

--- driver-core/src/test/unit/com/mongodb/internal/connection/CompositeByteBufTest.java
+++ driver-core/src/test/unit/com/mongodb/internal/connection/CompositeByteBufTest.java
@@ -43,7 +43,7 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 
-public final class CompositeByteBufTest {
+final class CompositeByteBufTest {
 
     @Test
     @SuppressWarnings("ConstantConditions")
