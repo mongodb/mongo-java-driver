973b9e1561137869afb23b7b6fb0f548ab10994c
Viacheslav Babanin <slav.babanin@mongodb.com>
2026-02-22 23:14:18 -0800

    Make NettyByteBuf share parent reference count (#1897)
    JAVA-6107

--- driver-core/src/test/functional/com/mongodb/internal/connection/CommandHelperSpecification.groovy
+++ driver-core/src/test/functional/com/mongodb/internal/connection/CommandHelperSpecification.groovy
@@ -25,7 +25,6 @@ import com.mongodb.connection.SocketSettings
 import com.mongodb.internal.connection.netty.NettyStreamFactory
 import org.bson.BsonDocument
 import org.bson.BsonInt32
-import spock.lang.Ignore
 import spock.lang.Specification
 
 import java.util.concurrent.CountDownLatch
@@ -57,7 +56,6 @@ class CommandHelperSpecification extends Specification {
         connection?.close()
     }
 
-    @Ignore("JAVA-5982")
     def 'should execute command asynchronously'() {
         when:
         BsonDocument receivedDocument = null
@@ -84,5 +82,4 @@ class CommandHelperSpecification extends Specification {
         !receivedDocument
         receivedException instanceof MongoCommandException
     }
-
 }
