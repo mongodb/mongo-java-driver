69630b08fbef5f2e89cece2fe4a36ae4845d3e90
Nabil Hachicha <nabil.hachicha@gmail.com>
2026-03-31 13:27:33 +0100

    Update driver-core/src/test/unit/com/mongodb/internal/time/ExponentialBackoffTest.java
    Co-authored-by: Valentin Kovalenko <valentin.male.kovalenko@gmail.com>

--- driver-core/src/test/unit/com/mongodb/internal/time/ExponentialBackoffTest.java
+++ driver-core/src/test/unit/com/mongodb/internal/time/ExponentialBackoffTest.java
@@ -26,7 +26,7 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 class ExponentialBackoffTest {
     /**
      * Expected {@linkplain ExponentialBackoff#calculateTransactionBackoffMs(int) backoffs} with 1.0 as
-     * {@link ExponentialBackoff#setTestJitterSupplier(DoubleSupplier)  jitter}.
+     * {@link ExponentialBackoff#setTestJitterSupplier(DoubleSupplier) jitter}.
      */
     private static final double[] EXPECTED_BACKOFFS_MAX_VALUES = {5.0, 7.5, 11.25, 16.875, 25.3125, 37.96875, 56.953125, 85.4296875, 128.14453125,
             192.21679688, 288.32519531, 432.48779297, 500.0};
