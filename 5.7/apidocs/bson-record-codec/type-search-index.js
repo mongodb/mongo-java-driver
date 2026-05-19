b0f1e517b19fdda96848747f4acbf35ae6a82ff6
Michael Morisi <michael.morisi@mongodb.com>
2026-05-19 11:21:35 -0400

    5.7 docs (#1976)

---
+++ 5.7/apidocs/bson-record-codec/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"org.bson.codecs.record","l":"RecordCodecProvider"}];updateSearchResults();
---
+++ 5.7/apidocs/mongodb-crypt/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.mongodb.crypt.capi"}];updateSearchResults();
