42c14abda58690540d953c23f620147fe33b2018
Andre Masella <andre.masella@mongodb.com>
2026-09-08 15:31:02 -0400

    fixup

--- driver-reactive-streams/src/main/com/mongodb/reactivestreams/client/internal/ClientSessionBinding.java
+++ driver-reactive-streams/src/main/com/mongodb/reactivestreams/client/internal/ClientSessionBinding.java
@@ -241,7 +241,8 @@ public class ClientSessionBinding extends AbstractReferenceCounted implements As
                 return assertNotNull(clientSession.getTransactionOptions().getReadConcern());
             } else if (isSnapshot()) {
                 return ReadConcern.SNAPSHOT;
-            } else if (!clientSession.getServerSession().isClosed()
+            } else if (inheritedReadConcern == null
+                    && !clientSession.getServerSession().isClosed()
                     && clientSession.isCausallyConsistent()
                     && clientSession.getOperationTime() != null) {
                 return new ReadConcern(clientSession.getOperationTime());
--- driver-sync/src/main/com/mongodb/client/internal/ClientSessionBinding.java
+++ driver-sync/src/main/com/mongodb/client/internal/ClientSessionBinding.java
@@ -222,7 +222,8 @@ public class ClientSessionBinding extends AbstractReferenceCounted implements Re
                 return assertNotNull(clientSession.getTransactionOptions().getReadConcern());
             } else if (isSnapshot()) {
                 return ReadConcern.SNAPSHOT;
-            } else if (!clientSession.getServerSession().isClosed()
+            } else if (inheritedReadConcern == null
+                    && !clientSession.getServerSession().isClosed()
                     && clientSession.isCausallyConsistent()
                     && clientSession.getOperationTime() != null) {
                 return new ReadConcern(clientSession.getOperationTime());
