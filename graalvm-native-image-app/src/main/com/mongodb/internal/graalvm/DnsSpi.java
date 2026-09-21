46d077780d123ed18533566f4a0c287b4cf48cfa
Andre Masella <andre.masella@mongodb.com>
2026-09-21 15:10:05 -0400

    fixup

--- graalvm-native-image-app/src/main/com/mongodb/internal/graalvm/DnsSpi.java
+++ graalvm-native-image-app/src/main/com/mongodb/internal/graalvm/DnsSpi.java
@@ -44,6 +44,7 @@ final class DnsSpi {
         try (MongoClient client = MongoClients.create(MongoClientSettings.builder()
                 .applyToClusterSettings(builder -> builder
                         .srvHost("a.b.c")
+                        .srvAllowedHostsSuffix("b.c")
                         // `MongoClient` uses `CustomDnsClientProvider` asynchronously,
                         // and by waiting for server selection that cannot succeed due to `a.b.c` not resolving to an IP address,
                         // we give `MongoClient` enough time to use `CustomDnsClientProvider`.
